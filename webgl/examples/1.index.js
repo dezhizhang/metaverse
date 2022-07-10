021921024f743bd8050058394338952591cf2aac
dezhizhang <1541609448@qq.com>
2022-07-10 14:55:55 +0800

    feat:update

--- webgl/examples/1.index.js
+++ webgl/examples/1.index.js
@@ -1,10 +1,11 @@
 
+
 function init() {
     const canvas = document.getElementById('canvas');
     const webgl = canvas.getContext('webgl');
     webgl.viewport(0,0,canvas.clientWidth,canvas.clientHeight);
-    webgl.clearColor(0,0,0,1)
-    webgl.clear(webgl.COLOR_BUFFER_BIT)
+    webgl.clearColor(106/255,121/255,162/255,1.0);
+    webgl.clear(webgl.COLOR_BUFFER_BIT);
 }
 
 init();
