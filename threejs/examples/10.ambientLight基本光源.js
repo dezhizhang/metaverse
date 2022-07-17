c1ca3d78e4ae08051619da64384f832f461c0fbc
dezhizhang <1541609448@qq.com>
2022-07-18 07:52:35 +0800

    feat:基本光源

--- threejs/examples/10.ambientLight基本光源.js
+++ threejs/examples/10.ambientLight基本光源.js
@@ -1,13 +1,3 @@
-/*
- * :file description: ;
-
- * :name: /threejs/src/index.js
- * :author: 张德志
- * :copyright: (c) 2022, Tungee
- * :date created: 2022-07-12 07:44:44
- * :last editor: 张德志
- * :date last edited: 2022-07-18 07:50:53
- */
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
