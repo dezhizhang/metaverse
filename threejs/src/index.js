30c87977eb3e036d27faf4ec465201b30eecc031
dezhizhang <1541609448@qq.com>
2022-07-23 07:37:36 +0800

    feat:基础几何体

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-12 07:44:44
  * :last editor: 张德志
- * :date last edited: 2022-07-23 07:16:43
+ * :date last edited: 2022-07-23 07:36:05
  */
 
 // import * as THREE from 'three';
@@ -16,6 +16,8 @@ import * as THREE from 'three';
 import * as dat from 'dat.gui';
 
 const gui = new dat.GUI();
+
+let step = 0;
 
 // 创建场影
 const scene = new THREE.Scene();
@@ -32,12 +34,55 @@ renderer.shadowMap = {
 }
 
 // 创建平面
-const planeGeometry = new THREE.PlaneGeometry(100,100,4,4);
+const planeGeom = new THREE.PlaneGeometry(100,100,4,4);
 const planeMaterial = new THREE.MeshBasicMaterial({color:0x777777});
-const planeMesh = new THREE.Mesh(planeGeometry,planeMaterial)
+const planeMesh = new THREE.Mesh(planeGeom,planeMaterial)
 planeMesh.rotation.x = -Math.PI / 2;
 planeMesh.position.y = -20;
 scene.add(planeMesh);
+
+
+// 创建圆形几何体
+const sphereGeometry = new THREE.SphereGeometry(14,20,20);
+const cubeGeometry = new THREE.BoxGeometry(15,15,15);
+const planeGeometry = new THREE.PlaneGeometry(14,14,4,4);
+
+const meshMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff});
+const sphere = new THREE.Mesh(sphereGeometry,meshMaterial);
+const cube = new THREE.Mesh(cubeGeometry,meshMaterial);
+const plane = new THREE.Mesh(planeGeometry,meshMaterial);
+
+sphere.position.x = 0;
+sphere.position.y = 3;
+sphere.position.z = 2;
+
+cube.position.x = 0;
+cube.position.y = 3;
+cube.position.z = 2;
+
+plane.position.x = 0;
+plane.position.y = 3;
+plane.position.z = 2;
+
+scene.add(cube);
+
+
+// 设置相机的位置
+camera.position.x = -20;
+camera.position.y = 50;
+camera.position.z = 40;
+
+camera.lookAt(new THREE.Vector3(10,0,0));
+
+// 设置光源
+const ambientLight = new THREE.AmbientLight(0x0c0c0c);
+scene.add(ambientLight);
+
+// 设置点光源
+const spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(-40,60,-10);
+spotLight.castShadow = true;
+scene.add(spotLight);
 
 
 
@@ -45,63 +90,16 @@ document.body.appendChild(renderer.domElement);
 
 
 function render() {
+    cube.rotation.y = step += 0.01;
+    console.log('cube',cube);
+            // plane.rotation.y = step;
+            // sphere.rotation.y = step;
     requestAnimationFrame(render)
     renderer.render(scene,camera);
 }
 
 render();
 
-// function init() {
-
-
-//     var groundGeom = new THREE.PlaneGeometry(100, 100, 4, 4);
-//     var groundMesh = new THREE.Mesh(groundGeom, new THREE.MeshBasicMaterial({color: 0x777777}));
-//     groundMesh.rotation.x = -Math.PI / 2;
-//     groundMesh.position.y = -20;
-//     scene.add(groundMesh);
-
-//     var sphereGeometry = new THREE.SphereGeometry(14, 20, 20);
-//     var cubeGeometry = new THREE.BoxGeometry(15, 15, 15);
-//     var planeGeometry = new THREE.PlaneGeometry(14, 14, 4, 4);
-
-
-//     var meshMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff});
-
-//     var sphere = new THREE.Mesh(sphereGeometry, meshMaterial);
-//     var cube = new THREE.Mesh(cubeGeometry, meshMaterial);
-//     var plane = new THREE.Mesh(planeGeometry, meshMaterial);
-
-//     // position the sphere
-//     sphere.position.x = 0;
-//     sphere.position.y = 3;
-//     sphere.position.z = 2;
-
-
-//     cube.position = sphere.position;
-//     plane.position = sphere.position;
-
-
-//     // add the sphere to the scene
-//     scene.add(cube);
-
-//     // position and point the camera to the center of the scene
-//     camera.position.x = -20;
-//     camera.position.y = 50;
-//     camera.position.z = 40;
-//     camera.lookAt(new THREE.Vector3(10, 0, 0));
-
-//     // add subtle ambient lighting
-//     var ambientLight = new THREE.AmbientLight(0x0c0c0c);
-//     scene.add(ambientLight);
-
-//     // add spotlight for the shadows
-//     var spotLight = new THREE.SpotLight(0xffffff);
-//     spotLight.position.set(-40, 60, -10);
-//     spotLight.castShadow = true;
-//     scene.add(spotLight);
-
-//     // add the output of the renderer to the html element
-//     document.getElementById("WebGL-output").appendChild(renderer.domElement);
 
 //     // call the render function
 //     var step = 0;
