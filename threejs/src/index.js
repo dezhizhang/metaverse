70a654b79788195fb1e0f06c803a813f9a9533f8
dezhizhang <1541609448@qq.com>
2022-07-26 07:51:10 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,17 +5,17 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-12 07:44:44
  * :last editor: 张德志
- * :date last edited: 2022-07-25 07:31:01
+ * :date last edited: 2022-07-26 07:50:42
  */
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
 import { createMultiMaterialObject } from 'three/examples/jsm/utils/SceneUtils';
 
-
-let  step = 0;
-// 创建场景
+let step = 0;
+let spGroup;
+let latheMesh;
+// 创建场影
 const scene = new THREE.Scene();
-
 // 创建相机
 const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,0.1,1000);
 
@@ -24,59 +24,59 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setClearColor(new THREE.Color(0xEEEEEE));
 renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.shadowMap = {
-    enabled:true
+    enabled:true 
 }
-
-function createMesh(geom) {
-    const meshMaterial = new THREE.MeshNormalMaterial();
-    meshMaterial.side = THREE.DoubleSide;
-    const wireFrameMat = new THREE.MeshBasicMaterial();
-    wireFrameMat.wireframe = true;
-
-    const mesh = createMultiMaterialObject(geom,[meshMaterial,wireFrameMat]);
-    return mesh;
-    
-}
-
-const cylinder = createMesh(new THREE.CylinderGeometry(20,20,20));
-scene.add(cylinder);
 
 // 设置相机位置
-camera.position.x =  -30;
+camera.position.x = -30;
 camera.position.y = 40;
 camera.position.z = 50;
 camera.lookAt(new THREE.Vector3(10,0,0));
 
-
 document.body.append(renderer.domElement);
 
-const controls = new function() {
-    this.radiusTop = 20;
-    this.radiusBottom = 20;
-    this.height = 20;
 
-    this.radialSegments = 8;
-    this.heightSegments = 8;
+generatePoints(12, 2, 2 * Math.PI);
 
-    this.openEnded = false;
-    this.redraw = function() {
-        scene.remove(cylinder);
-        cylinder = createMesh(new THREE.CylinderGeometry(this.radiusTop,this.radiusBottom,this.height,this.radialSegments,this.heightSegments,this.openEnded))
-       scene.add(cylinder);
+function generatePoints(segments,phiStart,phiLength) {
+    let points = [];
+    let height = 5;
+    let count = 30;
+    for(let i=0;i < count;i++) {
+        points.push(new THREE.Vector3((Math.sin(i * 0.2) + Math.cos(i * 0.3)) * height + 12, 0, ( i - count ) + count / 2));
     }
+    spGroup = new  THREE.Object3D();
+    let material = new THREE.MeshBasicMaterial({color:0xff0000,transparent:false});
+    points.forEach((point) => {
+        let spGeom = new  THREE.SphereGeometry(0.2);
+        let spMesh = new THREE.Mesh(spGeom,material);
+        spMesh.position.copy(point);
+        spGroup.add(spMesh);
+    });
+    var latheGeometry = new THREE.LatheGeometry(points, segments, phiStart, phiLength);
+    latheMesh = createMesh(latheGeometry);
+    scene.add(spGroup);
+
 
 }
 
-const gui = new dat.GUI();
-gui.add(controls,'radiusTop',-40,40)
+function createMesh(geom) {
+    let meshMaterial = new THREE.MeshNormalMaterial();
+    meshMaterial.side = THREE.DoubleSide;
+    let wireFrameMat = new THREE.MeshBasicMaterial();
+    wireFrameMat.wireframe = true;
+
+    let mesh = createMultiMaterialObject(geom,[meshMaterial,wireFrameMat]);
+    return mesh;
+}
 
 function render() {
-    cylinder.rotation.y = step += 0.01;
-    requestAnimationFrame(render);
+    spGroup.rotation.x = step;
+    latheMesh.rotation.x = step += 0.01;
+    requestAnimationFrame(render)
     renderer.render(scene,camera);
 }
 
+
 render();
-
-
 
