6df1b51493f67bbf50e8f0abf4e43ab82ccc8212
dezhizhang <1541609448@qq.com>
2022-08-04 08:06:52 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,124 +5,144 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-12 07:44:44
  * :last editor: 张德志
- * :date last edited: 2022-08-04 07:46:55
+ * :date last edited: 2022-08-04 08:05:20
  */
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
+import * as TWEEN from '@tweenjs/tween.js'
 
-// 创建场影
-const scene = new THREE.Scene();
+function init() {
+    
+    // create a scene, that will hold all our elements such as objects, cameras and lights.
+    var scene = new THREE.Scene();
 
-// 创建相机
-const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,0.1,1000);
+    // create a camera, which defines where we're looking at.
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-// 创建渲染器
-const renderer = new THREE.WebGLRenderer();
-renderer.setClearColor(new THREE.Color(0xEEEEEE));
-renderer.setSize(window.innerWidth,window.innerHeight);
+    // create a render and set the size
+    var webGLRenderer = new THREE.WebGLRenderer();
+    webGLRenderer.setClearColor(new THREE.Color(0x000, 1.0));
+    webGLRenderer.setSize(window.innerWidth, window.innerHeight);
+    webGLRenderer.shadowMapEnabled = true;
 
-// 创建平面
-const planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
-const planeMaterial = new THREE.MeshLambertMaterial({color:0xffffff});
-const plane = new THREE.Mesh(planeGeometry,planeMaterial);
+    // position and point the camera to the center of the scene
+    camera.position.x = 10;
+    camera.position.y = 10;
+    camera.position.z = 10;
+    camera.lookAt(new THREE.Vector3(0, -2, 0));
 
-// 设置平面的位置
-plane.rotation.x = -0.5 * Math.PI;
-plane.position.x = 15;
-plane.position.y = 0;
-plane.position.z = 0;
+    // add spotlight for the shadows
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(20, 20, 20);
+    scene.add(spotLight);
 
-// 添加到场影中
-scene.add(plane);
+    // add the output of the renderer to the html element
+    document.body.appendChild(webGLRenderer.domElement);
 
-// 创建几何体
-const cubeGeometry = new THREE.BoxGeometry(4,4,4);
-const cubeMaterial = new THREE.MeshLambertMaterial({color:0xff0000});
-const cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
-
-// 设置几何体位置
-cube.position.x = -9;
-cube.position.y = 3;
-cube.position.z = 0;
-scene.add(cube);
-
-// 创建圆形几何体
-const sphereGeometry = new THREE.SphereGeometry(4,20,20);
-const sphereMaterial = new THREE.MeshLambertMaterial({color:0x7777ff});
-const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
-
-// 设置园形的位置
-sphere.position.x = 20;
-sphere.position.y = 0;
-sphere.position.z = 2;
-
-// 添加到场影中
-scene.add(sphere);
-
-// 创建圆柱体
-const cylinderGeometry = new THREE.CylinderGeometry(2,2,20);
-const cylinderMaterial = new THREE.MeshLambertMaterial({color:0x77ff77});
-const cylinder = new THREE.Mesh(cylinderGeometry,cylinderMaterial);
-
-cylinder.position.set(0,0,1);
-scene.add(cylinder);
-
-// 设置相机的位置
-camera.position.x = -30;
-camera.position.y = 40;
-camera.position.z = 30;
-camera.lookAt(scene.position);
-
-// 设置光源
-const ambientLight = new THREE.AmbientLight(0x0c0c0c);
-scene.add(ambientLight);
-
-// 设置点光源
-const spotLight = new THREE.SpotLight(0xffffff);
-spotLight.position.set(-40,60,-10);
-scene.add(spotLight);
-
-document.body.append(renderer.domElement);
-
-let step = 0;
-let scalingStep = 0;
-
-let controls = new function() {
-    this.rotationSpeed = 0.02;
-    this.bouncingSpeed = 0.03;
-    this.scalingSpeed = 0.03;
-} 
-
-let gui = new dat.GUI();
-gui.add(controls,'rotationSpeed',0,0.5);
-gui.add(controls,'bouncingSpeed',0,0.5);
-gui.add(controls,'scalingSpeed',0,0.5);
+    // call the render function
+    var step = 0;
 
 
+    // setup the control gui
+    var controls = new function () {
+        // we need the first child, since it's a multimaterial
 
-function render() {
-    cube.rotation.x += controls.rotationSpeed;
-    cube.rotation.y += controls.rotationSpeed;
-    cube.rotation.z += controls.rotationSpeed;
 
-    // bounce the sphere up and down
-    step += controls.bouncingSpeed;
-    sphere.position.x = 20 + ( 10 * (Math.cos(step)));
-    sphere.position.y = 2 + ( 10 * Math.abs(Math.sin(step)));
+    };
 
-    // scale the cylinder
+    var pointCloud = new THREE.Object3D();
+    var loadedGeometry;
 
-    scalingStep += controls.scalingSpeed;
-    var scaleX = Math.abs(Math.sin(scalingStep / 4));
-    var scaleY = Math.abs(Math.cos(scalingStep / 5));
-    var scaleZ = Math.abs(Math.sin(scalingStep / 7));
-    cylinder.scale.set(scaleX, scaleY, scaleZ);
 
-    // render using requestAnimationFrame
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
+    // create a tween
+    // http://sole.github.io/tween.js/examples/03_graphs.html
+    var posSrc = {pos: 1};
+    var tween = new TWEEN.Tween(posSrc).to({pos: 0}, 5000);
+    tween.easing(TWEEN.Easing.Sinusoidal.InOut);
+
+    var tweenBack = new TWEEN.Tween(posSrc).to({pos: 1}, 5000);
+    tweenBack.easing(TWEEN.Easing.Sinusoidal.InOut);
+
+    tween.chain(tweenBack);
+    tweenBack.chain(tween);
+
+
+    var onUpdate = function () {
+        var count = 0;
+        var pos = this.pos;
+
+        loadedGeometry.vertices.forEach(function (e) {
+            var newY = ((e.y + 3.22544) * pos) - 3.22544;
+            pointCloud.geometry.vertices[count++].set(e.x, newY, e.z);
+        });
+
+        pointCloud.sortParticles = true;
+    };
+
+    tween.onUpdate(onUpdate);
+    tweenBack.onUpdate(onUpdate);
+
+
+    var gui = new dat.GUI();
+
+
+    // var loader = new THREE.PLYLoader();
+
+    // loader.load("../assets/models/test.ply", function (geometry) {
+    //     loadedGeometry = geometry.clone();
+
+    //     var material = new THREE.PointCloudMaterial({
+    //         color: 0xffffff,
+    //         size: 0.4,
+    //         opacity: 0.6,
+    //         transparent: true,
+    //         blending: THREE.AdditiveBlending,
+    //         map: generateSprite()
+    //     });
+
+    //     pointCloud = new THREE.PointCloud(geometry, material);
+    //     pointCloud.sortParticles = true;
+
+    //     tween.start();
+    //     scene.add(pointCloud);
+    // });
+
+
+    render();
+
+    generateSprite();
+    
+
+    // from THREE.js examples
+    function generateSprite() {
+
+        var canvas = document.createElement('canvas');
+        canvas.width = 16;
+        canvas.height = 16;
+
+        var context = canvas.getContext('2d');
+        var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
+        gradient.addColorStop(0, 'rgba(255,255,255,1)');
+        gradient.addColorStop(0.2, 'rgba(0,255,255,1)');
+        gradient.addColorStop(0.4, 'rgba(0,0,64,1)');
+        gradient.addColorStop(1, 'rgba(0,0,0,1)');
+
+        context.fillStyle = gradient;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        var texture = new THREE.Texture(canvas);
+        texture.needsUpdate = true;
+        return texture;
+
+    }
+
+    function render() {
+       
+        TWEEN.update();
+        requestAnimationFrame(render);
+        webGLRenderer.render(scene, camera);
+    }
+
+ 
 }
-
-render();
-
-
+window.onload = init;
