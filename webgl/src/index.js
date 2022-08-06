3f75033ce63777a1037bdb874cba1fb869e1881f
dezhizhang <1541609448@qq.com>
2022-08-06 09:20:48 +0800

    feat:webgl

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -1,3 +1,12 @@
+/*
+ * :file description: 
+ * :name: /webgl/src/index.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-07-10 11:12:55
+ * :last editor: 张德志
+ * :date last edited: 2022-08-06 09:20:20
+ */
 
 function init() {
     const canvas = document.getElementById('canvas');
@@ -6,5 +15,4 @@ function init() {
     webgl.clearColor(0,0,0,1)
     webgl.clear(webgl.COLOR_BUFFER_BIT)
 }
-
 init();
