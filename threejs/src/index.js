a28b7fff7d2d138d802d08bcb555f10ffccff213
dezhizhang <1541609448@qq.com>
2022-08-06 09:44:43 +0800

    feat:财质

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,144 +5,106 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-12 07:44:44
  * :last editor: 张德志
- * :date last edited: 2022-08-04 08:05:20
+ * :date last edited: 2022-08-06 09:21:59
  */
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
-import * as TWEEN from '@tweenjs/tween.js'
 
-function init() {
+import stone from '../assets/textures/general/stone.jpg';
+import lm1 from '../assets/textures/lightmap/lm-1.png'
+import floorwood from '../assets/textures/general/floor-wood.jpg';
+
+const scene = new THREE.Scene();
+
+
+// function init() {
+
+
+//     // create a scene, that will hold all our elements such as objects, cameras and lights.
+//     var scene = new THREE.Scene();
+
+//     // create a camera, which defines where we're looking at.
+//     var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//     // create a render and set the size
+//     var renderer;
+//     var webGLRenderer = new THREE.WebGLRenderer();
+//     webGLRenderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
+//     webGLRenderer.setSize(window.innerWidth, window.innerHeight);
+//     webGLRenderer.shadowMapEnabled = true;
+
+//     renderer = webGLRenderer;
+
+//     var groundGeom = new THREE.PlaneGeometry(95, 95, 1, 1);
+//     var lm = THREE.ImageUtils.loadTexture(lm1);
+//     var wood = THREE.ImageUtils.loadTexture(floorwood);
+//     var groundMaterial = new THREE.MeshBasicMaterial(
+//             {
+//                 color: 0x777777,
+//                 lightMap: lm,
+//                 map: wood
+//             });
+
+
+// //        console.log(groundGeom.faceVertexUvs[1]);
+//     // http://stackoverflow.com/questions/15137695/three-js-lightmap-causes-an-error-webglrenderingcontext-gl-error-gl-invalid-op
+//     // https://github.com/mrdoob/three.js/pull/2372
+//     // lightmaps use own mapping of uvs (faceVertexUvs[1])
+//     // so need to create those. Reason is explained
+//     // here. So we can use a low res map for lightmap
+//     // and a high res map for textures.
+//     groundGeom.faceVertexUvs[1] = groundGeom.faceVertexUvs[0];
+// //        console.log(groundGeom.faceVertexUvs[1]);
+
+
+//     var groundMesh = new THREE.Mesh(groundGeom, groundMaterial);
+
+//     groundMesh.rotation.x = -Math.PI / 2;
+//     groundMesh.position.y = 0;
+//     scene.add(groundMesh);
+
+
+//     var cubeGeometry = new THREE.BoxGeometry(12, 12, 12);
+//     var cubeGeometry2 = new THREE.BoxGeometry(6, 6, 6);
+
+
+//     var meshMaterial = new THREE.MeshBasicMaterial();
+//     meshMaterial.map = THREE.ImageUtils.loadTexture(stone);
+
+
+//     var cube = new THREE.Mesh(cubeGeometry, meshMaterial);
+//     var cube2 = new THREE.Mesh(cubeGeometry2, meshMaterial);
+//     cube.position.set(0.9, 6, -12);
+//     cube2.position.set(-13.2, 3, -6);
+
+
+//     // add the sphere to the scene
+//     scene.add(cube);
+//     scene.add(cube2);
+
+//     // position and point the camera to the center of the scene
+//     camera.position.x = -20;
+//     camera.position.y = 20;
+//     camera.position.z = 30;
+//     camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+//     // add subtle ambient lighting
+//     var ambientLight = new THREE.AmbientLight(0x0c0c0c);
+//     scene.add(ambientLight);
+
+
+//     // add the output of the renderer to the html element
+//     document.body.appendChild(renderer.domElement);
+
+
+//     render();
+
+//     function render() {
+//         // render using requestAnimationFrame
+//         requestAnimationFrame(render);
+//         renderer.render(scene, camera);
+//     }
+
     
-    // create a scene, that will hold all our elements such as objects, cameras and lights.
-    var scene = new THREE.Scene();
-
-    // create a camera, which defines where we're looking at.
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    // create a render and set the size
-    var webGLRenderer = new THREE.WebGLRenderer();
-    webGLRenderer.setClearColor(new THREE.Color(0x000, 1.0));
-    webGLRenderer.setSize(window.innerWidth, window.innerHeight);
-    webGLRenderer.shadowMapEnabled = true;
-
-    // position and point the camera to the center of the scene
-    camera.position.x = 10;
-    camera.position.y = 10;
-    camera.position.z = 10;
-    camera.lookAt(new THREE.Vector3(0, -2, 0));
-
-    // add spotlight for the shadows
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(20, 20, 20);
-    scene.add(spotLight);
-
-    // add the output of the renderer to the html element
-    document.body.appendChild(webGLRenderer.domElement);
-
-    // call the render function
-    var step = 0;
-
-
-    // setup the control gui
-    var controls = new function () {
-        // we need the first child, since it's a multimaterial
-
-
-    };
-
-    var pointCloud = new THREE.Object3D();
-    var loadedGeometry;
-
-
-    // create a tween
-    // http://sole.github.io/tween.js/examples/03_graphs.html
-    var posSrc = {pos: 1};
-    var tween = new TWEEN.Tween(posSrc).to({pos: 0}, 5000);
-    tween.easing(TWEEN.Easing.Sinusoidal.InOut);
-
-    var tweenBack = new TWEEN.Tween(posSrc).to({pos: 1}, 5000);
-    tweenBack.easing(TWEEN.Easing.Sinusoidal.InOut);
-
-    tween.chain(tweenBack);
-    tweenBack.chain(tween);
-
-
-    var onUpdate = function () {
-        var count = 0;
-        var pos = this.pos;
-
-        loadedGeometry.vertices.forEach(function (e) {
-            var newY = ((e.y + 3.22544) * pos) - 3.22544;
-            pointCloud.geometry.vertices[count++].set(e.x, newY, e.z);
-        });
-
-        pointCloud.sortParticles = true;
-    };
-
-    tween.onUpdate(onUpdate);
-    tweenBack.onUpdate(onUpdate);
-
-
-    var gui = new dat.GUI();
-
-
-    // var loader = new THREE.PLYLoader();
-
-    // loader.load("../assets/models/test.ply", function (geometry) {
-    //     loadedGeometry = geometry.clone();
-
-    //     var material = new THREE.PointCloudMaterial({
-    //         color: 0xffffff,
-    //         size: 0.4,
-    //         opacity: 0.6,
-    //         transparent: true,
-    //         blending: THREE.AdditiveBlending,
-    //         map: generateSprite()
-    //     });
-
-    //     pointCloud = new THREE.PointCloud(geometry, material);
-    //     pointCloud.sortParticles = true;
-
-    //     tween.start();
-    //     scene.add(pointCloud);
-    // });
-
-
-    render();
-
-    generateSprite();
-    
-
-    // from THREE.js examples
-    function generateSprite() {
-
-        var canvas = document.createElement('canvas');
-        canvas.width = 16;
-        canvas.height = 16;
-
-        var context = canvas.getContext('2d');
-        var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
-        gradient.addColorStop(0, 'rgba(255,255,255,1)');
-        gradient.addColorStop(0.2, 'rgba(0,255,255,1)');
-        gradient.addColorStop(0.4, 'rgba(0,0,64,1)');
-        gradient.addColorStop(1, 'rgba(0,0,0,1)');
-
-        context.fillStyle = gradient;
-        context.fillRect(0, 0, canvas.width, canvas.height);
-
-        var texture = new THREE.Texture(canvas);
-        texture.needsUpdate = true;
-        return texture;
-
-    }
-
-    function render() {
-       
-        TWEEN.update();
-        requestAnimationFrame(render);
-        webGLRenderer.render(scene, camera);
-    }
-
- 
-}
-window.onload = init;
+// }
+// window.onload = init;
