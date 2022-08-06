bab339514b53a048757618e72d77569aace51c79
dezhizhang <1541609448@qq.com>
2022-08-06 22:44:32 +0800

    feat:提交

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-12 07:44:44
  * :last editor: 张德志
- * :date last edited: 2022-08-06 09:21:59
+ * :date last edited: 2022-08-06 09:42:53
  */
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
@@ -14,97 +14,70 @@ import stone from '../assets/textures/general/stone.jpg';
 import lm1 from '../assets/textures/lightmap/lm-1.png'
 import floorwood from '../assets/textures/general/floor-wood.jpg';
 
+// 创建场影
 const scene = new THREE.Scene();
 
+// 创建相机
+const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,0.1,1000);
 
-// function init() {
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer();
+renderer.clearColor(new THREE.Color(0xEEEEEE));
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.shadowMapEnabled = true;
 
+const groundGeom = new THREE.PlaneGeometry(95,95,1,1);
+const lm = new THREE.ImageUtils.loadTexture(lm1);
+const wood = new THREE.ImageUtils.loadTexture(floorwood);
+const groundMaterial = new THREE.MeshBasicMaterial({
+    color:0x777777,
+    lightMap:lm,
+    map:wood
+})
 
-//     // create a scene, that will hold all our elements such as objects, cameras and lights.
-//     var scene = new THREE.Scene();
+groundGeom.faceVertexUvs[1] = groundGeom.faceVertexUvs[0];
 
-//     // create a camera, which defines where we're looking at.
-//     var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+const groundMesh = new THREE.Mesh(groundGeom,groundMaterial);
+groundMesh.rotation.x = -Math.PI / 2;
+groundMesh.rotation.y = 0;
+scene.add(groundMesh);
 
-//     // create a render and set the size
-//     var renderer;
-//     var webGLRenderer = new THREE.WebGLRenderer();
-//     webGLRenderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
-//     webGLRenderer.setSize(window.innerWidth, window.innerHeight);
-//     webGLRenderer.shadowMapEnabled = true;
+// 创建正方形几何体
+const cubeGeometry = new THREE.BoxGeometry(12,12,12);
+const cubeGeometry2 = new THREE.BoxGeometry(6,6,6);
 
-//     renderer = webGLRenderer;
+const meshMaterial = new THREE.MeshBasicMaterial();
+meshMaterial.map = THREE.ImageUtils.loadTexture(stone);
 
-//     var groundGeom = new THREE.PlaneGeometry(95, 95, 1, 1);
-//     var lm = THREE.ImageUtils.loadTexture(lm1);
-//     var wood = THREE.ImageUtils.loadTexture(floorwood);
-//     var groundMaterial = new THREE.MeshBasicMaterial(
-//             {
-//                 color: 0x777777,
-//                 lightMap: lm,
-//                 map: wood
-//             });
+const cube = new THREE.Mesh(cubeGeometry,meshMaterial);
+const cube2 = new THREE.Mesh(cubeGeometry2,meshMaterial);
+cube.position.set(0.9,6,-12);
+cube2.position.set(-13.2,3,-6);
 
+// 添加到场景中
+scene.add(cube);
+scene.add(cube2);
 
-// //        console.log(groundGeom.faceVertexUvs[1]);
-//     // http://stackoverflow.com/questions/15137695/three-js-lightmap-causes-an-error-webglrenderingcontext-gl-error-gl-invalid-op
-//     // https://github.com/mrdoob/three.js/pull/2372
-//     // lightmaps use own mapping of uvs (faceVertexUvs[1])
-//     // so need to create those. Reason is explained
-//     // here. So we can use a low res map for lightmap
-//     // and a high res map for textures.
-//     groundGeom.faceVertexUvs[1] = groundGeom.faceVertexUvs[0];
-// //        console.log(groundGeom.faceVertexUvs[1]);
-
-
-//     var groundMesh = new THREE.Mesh(groundGeom, groundMaterial);
-
-//     groundMesh.rotation.x = -Math.PI / 2;
-//     groundMesh.position.y = 0;
-//     scene.add(groundMesh);
-
-
-//     var cubeGeometry = new THREE.BoxGeometry(12, 12, 12);
-//     var cubeGeometry2 = new THREE.BoxGeometry(6, 6, 6);
+// 设置相机的位置
+camera.position.x = -20;
+camera.position.y = 20;
+camera.position.z = 30;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 
-//     var meshMaterial = new THREE.MeshBasicMaterial();
-//     meshMaterial.map = THREE.ImageUtils.loadTexture(stone);
+// 创建平行光
+const ambientLight = new THREE.AmbientLight(0x0c0c0c);
+scene.add(ambientLight);
 
 
-//     var cube = new THREE.Mesh(cubeGeometry, meshMaterial);
-//     var cube2 = new THREE.Mesh(cubeGeometry2, meshMaterial);
-//     cube.position.set(0.9, 6, -12);
-//     cube2.position.set(-13.2, 3, -6);
+document.body.append(renderer.domElement);
 
 
-//     // add the sphere to the scene
-//     scene.add(cube);
-//     scene.add(cube2);
+function render(){
+    requestAnimationFrame(render);
+    renderer.render(scene,camera);
+}
 
-//     // position and point the camera to the center of the scene
-//     camera.position.x = -20;
-//     camera.position.y = 20;
-//     camera.position.z = 30;
-//     camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-//     // add subtle ambient lighting
-//     var ambientLight = new THREE.AmbientLight(0x0c0c0c);
-//     scene.add(ambientLight);
+render();
 
 
-//     // add the output of the renderer to the html element
-//     document.body.appendChild(renderer.domElement);
-
-
-//     render();
-
-//     function render() {
-//         // render using requestAnimationFrame
-//         requestAnimationFrame(render);
-//         renderer.render(scene, camera);
-//     }
-
-    
-// }
-// window.onload = init;
