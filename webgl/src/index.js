a26142c92bfd309302456d8afab84c3d9f155752
dezhizhang <1541609448@qq.com>
2022-08-13 19:42:50 +0800

    feat:update

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-10 11:12:55
  * :last editor: 张德志
- * :date last edited: 2022-08-13 19:29:11
+ * :date last edited: 2022-08-13 19:40:47
  */
 
 
@@ -31,15 +31,14 @@ const vertex = gl.createShader(gl.VERTEX_SHADER);
 const frag = gl.createShader(gl.FRAGMENT_SHADER);
 
 gl.shaderSource(vertex,VERTEX_SHADER);
-gl.shaderSource(frag,FRAG_SHADER)
-
+gl.shaderSource(frag,FRAG_SHADER);
 
 // 编译
 gl.compileShader(vertex);
 gl.compileShader(frag);
 
-//创建对像
-const  program = gl.createProgram();
+
+const program = gl.createProgram();
 gl.attachShader(program,vertex);
 gl.attachShader(program,frag);
 
