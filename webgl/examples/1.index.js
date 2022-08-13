30ee92d80c8e08ecf64a4d1e245b9bcb6db652ba
dezhizhang <1541609448@qq.com>
2022-08-13 15:29:51 +0800

    feat:清除画布

--- webgl/examples/1.index.js
+++ webgl/examples/1.index.js
@@ -1,11 +1,17 @@
+/*
+ * :file description: 
+ * :name: /webgl/examples/1.index.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-07-10 14:44:58
+ * :last editor: 张德志
+ * :date last edited: 2022-08-13 15:29:18
+ */
 
 
-function init() {
-    const canvas = document.getElementById('canvas');
-    const webgl = canvas.getContext('webgl');
-    webgl.viewport(0,0,canvas.clientWidth,canvas.clientHeight);
-    webgl.clearColor(106/255,121/255,162/255,1.0);
-    webgl.clear(webgl.COLOR_BUFFER_BIT);
-}
+const canvas = document.getElementById('canvas');
+const gl = canvas.getContext('webgl');
+gl.clearColor(0.0,1.0,1.0,1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
 
-init();
+
