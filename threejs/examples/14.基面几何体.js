1593e42e3818c76c347df5d4cbdaf157b2bd50c9
dezhizhang <1541609448@qq.com>
2022-08-21 11:27:02 +0800

    feat:update

--- threejs/examples/14.基面几何体.js
+++ threejs/examples/14.基面几何体.js
@@ -1,13 +1,12 @@
 /*
  * :file description: 
- * :name: /threejs/src/index.js
+ * :name: /threejs/examples/14.基面几何体.js
  * :author: 张德志
  * :copyright: (c) 2022, Tungee
- * :date created: 2022-07-12 07:44:44
+ * :date created: 2022-08-21 10:57:07
  * :last editor: 张德志
- * :date last edited: 2022-08-21 11:12:40
+ * :date last edited: 2022-08-21 11:00:51
  */
-
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
@@ -19,20 +18,24 @@ camera.position.y = 40;
 camera.position.z = 40;
 camera.lookAt(scene.position);
 
+
 const renderer = new THREE.WebGL1Renderer();
 renderer.setClearColor(new THREE.Color(0xEEEEEEE));
 renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.shadowMap.enabled = true;
 
-// 创建平面
+
+// // 创建平面
 const planeGeometry = new THREE.PlaneGeometry(70,50,1,1);
-const planeMaterial = new THREE.MeshLambertMaterial({color:new THREE.Color(0xEEEEEE)});
+const planeMaterial = new THREE.MeshLambertMaterial({Color:new THREE.Color(0xEEEEEE)});
 const plane = new THREE.Mesh(planeGeometry,planeMaterial);
 plane.rotation.x = -0.5 * Math.PI;
-plane.position.X = 0;
 plane.position.y = 0;
+plane.position.x = 0;
 plane.position.z = 0;
+
 scene.add(plane);
+
 
 // 创建点光源
 const spotLight = new THREE.SpotLight(0xffffff);
@@ -42,16 +45,17 @@ scene.add(spotLight);
 
 document.body.appendChild(renderer.domElement);
 
-// 创建几何体
+
 const geometry = new THREE.BoxGeometry(4,4,4);
-const material = new THREE.MeshLambertMaterial({color:new THREE.Color(0X00cc00)});
-const cube = new THREE.Mesh(geometry,material);
+const masterial = new THREE.MeshLambertMaterial({color:new THREE.Color(0X00cc00)});
+const mesh = new THREE.Mesh(geometry,masterial);
 
-cube.position.x = -10;
-cube.position.y = 10;
-cube.position.z = -1;
+mesh.position.y = 10;
+mesh.position.x = -10;
+mesh.position.z = -1
 
-scene.add(cube);
+scene.add(mesh);
+
 
 function render() {
 	
@@ -60,11 +64,3 @@ function render() {
 }
 
 render();
-
-
-
-
-
-
-
-
