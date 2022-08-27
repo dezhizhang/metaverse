dbdaf91650f36612b09b95a2ca7eadb8a8d37bf4
dezhizhang <1541609448@qq.com>
2022-08-27 16:39:34 +0800

    feat:update

--- cesium/src/main.jsx
+++ cesium/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+/*
+ * :file description: 
+ * :name: /cesium/src/main.jsx
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-08-27 16:29:41
+ * :last editor: 张德志
+ * :date last edited: 2022-08-27 16:37:31
+ */
+import * as Cesium from 'cesium';
+const viewer = new Cesium.Viewer('root');
--- cesium/vite.config.js
+++ cesium/vite.config.js
@@ -1,7 +1,17 @@
+/*
+ * :file description: 
+ * :name: /cesium/vite.config.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-08-27 16:29:41
+ * :last editor: 张德志
+ * :date last edited: 2022-08-27 16:36:24
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cesium from 'vite-plugin-cesium';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(),cesium()]
 })
