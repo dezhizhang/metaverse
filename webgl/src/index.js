635ee88941198f85a92dab3178bac2ee867f2136
dezhizhang <1541609448@qq.com>
2022-08-29 22:19:55 +0800

    feat:着色器

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -5,9 +5,8 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-13 21:36:57
  * :last editor: 张德志
- * :date last edited: 2022-08-28 16:32:21
+ * :date last edited: 2022-08-29 22:18:42
  */
-import * as THREE from 'three';
 
 const canvas = document.createElement('canvas');
 canvas.width = 500;
@@ -17,11 +16,39 @@ document.body.appendChild(canvas);
 
 const gl = canvas.getContext('webgl');
 
-const color = new THREE.Color(`rgba(255,0,0,1)`);
 
-gl.clearColor(color.r,color.g,color.b,1);
+const VERTEX_SHADER =
+    'void main() {\n\
+    gl_Position = vec4(-0.5, 0, 0, 1);\n\
+    gl_PointSize = 10.0;\n\
+}';
+
+const FRAG_SHADER =
+    'void main() {\n\
+    gl_FragColor = vec4(1, 0, 0, 1);\n\
+}';
+
+
+const vertex = gl.createShader(gl.VERTEX_SHADER);
+const frag = gl.createShader(gl.FRAGMENT_SHADER);
+
+gl.shaderSource(vertex,VERTEX_SHADER);
+gl.shaderSource(frag,FRAG_SHADER);
+
+
+gl.compileShader(vertex);
+gl.compileShader(frag);
+
+const program = gl.createProgram();
+gl.attachShader(program,vertex);
+gl.attachShader(program,frag);
+
+gl.linkProgram(program);
+gl.useProgram(program);
+
+gl.clearColor(0,0,0,1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-
+gl.drawArrays(gl.POINTS,0,1);
 
 
