6f34cbfbfedc3e82f4d9614177c11e6122391836
dezhizhang <1541609448@qq.com>
2022-08-30 07:05:44 +0800

    feat:缓冲区对像

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-13 21:36:57
  * :last editor: 张德志
- * :date last edited: 2022-08-29 23:11:28
+ * :date last edited: 2022-08-30 07:04:06
  */
 
 const canvas = document.createElement('canvas');
@@ -15,8 +15,6 @@ canvas.height = 500;
 document.body.appendChild(canvas);
 
 const gl = canvas.getContext('webgl');
-
-
 
 const VERTEX_SHADER =
     `
@@ -31,11 +29,13 @@ const FRAG_SHADER =
     gl_FragColor = vec4(1, 0, 0, 1);\n\
 }';
 
+
 const vertex = gl.createShader(gl.VERTEX_SHADER);
 const frag = gl.createShader(gl.FRAGMENT_SHADER);
 
 gl.shaderSource(vertex,VERTEX_SHADER);
 gl.shaderSource(frag,FRAG_SHADER);
+
 
 gl.compileShader(vertex);
 gl.compileShader(frag);
@@ -48,12 +48,31 @@ gl.linkProgram(program);
 gl.useProgram(program);
 
 
+const dataVetices = new Float32Array([
+    0.0,0.0,
+    0.5,0.5,
+    0.5,-0.5,
+    -0.5,-0.5,
+    -0.5,-0.5,
+]);
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
+gl.bufferData(gl.ARRAY_BUFFER,dataVetices,gl.STATIC_DRAW);
+
+
+
+
 const a_pos = gl.getAttribLocation(program,'a_pos');
-gl.vertexAttrib4f(a_pos,0,0,1,1);
+gl.vertexAttribPointer(a_pos,2,gl.FLOAT,false,0,0)
+gl.enableVertexAttribArray(a_pos);
+
 
 gl.clearColor(0,0,0,1);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
 
-gl.drawArrays(gl.POINTS,0,1);
+
+
+gl.drawArrays(gl.POINTS,0,5);
 
