1f605d74b08e190cb8180b00ae54df58a8dcac42
dezhizhang <1541609448@qq.com>
2022-08-29 22:21:23 +0800

    feat:update

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -5,50 +5,50 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-13 21:36:57
  * :last editor: 张德志
- * :date last edited: 2022-08-29 22:18:42
+ * :date last edited: 2022-08-29 22:21:03
  */
 
-const canvas = document.createElement('canvas');
-canvas.width = 500;
-canvas.height = 500;
+// const canvas = document.createElement('canvas');
+// canvas.width = 500;
+// canvas.height = 500;
 
-document.body.appendChild(canvas);
+// document.body.appendChild(canvas);
 
-const gl = canvas.getContext('webgl');
-
-
-const VERTEX_SHADER =
-    'void main() {\n\
-    gl_Position = vec4(-0.5, 0, 0, 1);\n\
-    gl_PointSize = 10.0;\n\
-}';
-
-const FRAG_SHADER =
-    'void main() {\n\
-    gl_FragColor = vec4(1, 0, 0, 1);\n\
-}';
+// const gl = canvas.getContext('webgl');
 
 
-const vertex = gl.createShader(gl.VERTEX_SHADER);
-const frag = gl.createShader(gl.FRAGMENT_SHADER);
+// const VERTEX_SHADER =
+//     'void main() {\n\
+//     gl_Position = vec4(-0.5, 0, 0, 1);\n\
+//     gl_PointSize = 10.0;\n\
+// }';
 
-gl.shaderSource(vertex,VERTEX_SHADER);
-gl.shaderSource(frag,FRAG_SHADER);
+// const FRAG_SHADER =
+//     'void main() {\n\
+//     gl_FragColor = vec4(1, 0, 0, 1);\n\
+// }';
 
 
-gl.compileShader(vertex);
-gl.compileShader(frag);
+// const vertex = gl.createShader(gl.VERTEX_SHADER);
+// const frag = gl.createShader(gl.FRAGMENT_SHADER);
 
-const program = gl.createProgram();
-gl.attachShader(program,vertex);
-gl.attachShader(program,frag);
+// gl.shaderSource(vertex,VERTEX_SHADER);
+// gl.shaderSource(frag,FRAG_SHADER);
 
-gl.linkProgram(program);
-gl.useProgram(program);
 
-gl.clearColor(0,0,0,1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
+// gl.compileShader(vertex);
+// gl.compileShader(frag);
 
-gl.drawArrays(gl.POINTS,0,1);
+// const program = gl.createProgram();
+// gl.attachShader(program,vertex);
+// gl.attachShader(program,frag);
+
+// gl.linkProgram(program);
+// gl.useProgram(program);
+
+// gl.clearColor(0,0,0,1.0);
+// gl.clear(gl.COLOR_BUFFER_BIT);
+
+// gl.drawArrays(gl.POINTS,0,1);
 
 
