09ea0729e7ef8ce6a6d1eb61cab865a816328f74
dezhizhang <1541609448@qq.com>
2022-09-01 07:05:36 +0800

    feat:缩放

--- webgl/examples/23.旋转操作.js
+++ webgl/examples/23.旋转操作.js
@@ -1,73 +1,12 @@
-# webgl
-### 第一个webgl程序
-```js 
-// 创建canvas
-const canvas = document.createElement('canvas');
-canvas.width = 500;
-canvas.height = 500;
-
-// 添加到body
-document.body.append(canvas);
-
-// 获取绘图环境
-const gl = canvas.getContext('webgl');
-
-// 设置清空颜色
-gl.clearColor(0,0,0,1);
-
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-```
-### 偏元着色器
-```js
-const canvas = document.createElement('canvas');
-canvas.width = 500;
-canvas.height = 500;
-
-document.body.appendChild(canvas);
-
-const gl = canvas.getContext('webgl');
-
-const VERTEX_SHADER =
-    'void main() {\n\
-    gl_Position = vec4(-0.5, 0, 0, 1);\n\
-    gl_PointSize = 10.0;\n\
-}';
-
-const FRAG_SHADER =
-    'void main() {\n\
-    gl_FragColor = vec4(1, 0, 0, 1);\n\
-}';
-
-
-const vertex = gl.createShader(gl.VERTEX_SHADER);
-const frag = gl.createShader(gl.FRAGMENT_SHADER);
-
-gl.shaderSource(vertex,VERTEX_SHADER);
-gl.shaderSource(frag,FRAG_SHADER);
-
-
-gl.compileShader(vertex);
-gl.compileShader(frag);
-
-const program = gl.createProgram();
-gl.attachShader(program,vertex);
-gl.attachShader(program,frag);
-
-gl.linkProgram(program);
-gl.useProgram(program);
-
-gl.linkProgram(program);
-gl.useProgram(program);
-
-gl.clearColor(0,0,0,1);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-gl.drawArrays(gl.POINTS,0,1);
-
-```
-### 旋转操作
-```js
+/*
+ * :file description: 
+ * :name: /webgl/examples/23.旋转操作.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-07-10 11:12:55
+ * :last editor: 张德志
+ * :date last edited: 2022-08-31 06:58:22
+ */
 const canvas = document.createElement('canvas');
 canvas.width = 500;
 canvas.height = 500;
@@ -147,4 +86,3 @@ gl.clear(gl.COLOR_BUFFER_BIT);
 
 
 gl.drawArrays(gl.TRIANGLES,0,3);
-```
--- webgl/src/index.js
+++ webgl/src/index.js
@@ -3,91 +3,87 @@
  * :name: /webgl/src/index.js
  * :author: 张德志
  * :copyright: (c) 2022, Tungee
- * :date created: 2022-08-13 21:36:57
+ * :date created: 2022-07-10 11:12:55
  * :last editor: 张德志
- * :date last edited: 2022-08-30 23:45:09
+ * :date last edited: 2022-08-31 07:05:21
  */
 
-const canvas = document.createElement('canvas');
-canvas.width = 500;
-canvas.height = 500;
+// const canvas = document.createElement('canvas');
+// canvas.width = 500;
+// canvas.height = 500;
 
-document.body.appendChild(canvas);
+// document.body.appendChild(canvas);
 
-const VERTEX_SHADER =
-    `
-    attribute vec4 a_pos;\n
-    uniform vec4 u_change;\n
-    void main() {\n\
-    gl_Position = a_pos + u_change ;\n\
-    gl_PointSize = 25.0;\n\
-}`;
+// const gl = canvas.getContext('webgl');
 
+// const VERTEX_SHADER =
+//     `
+//     attribute vec4 a_pos;\n\
+//     uniform float u_sinB,u_cosB;\n\
+//     void main() {\n\
+//     gl_Position.x = a_pos.x * u_cosB - a_pos.y * u_sinB;\n
+//     gl_Position.y = a_pos.y * u_cosB + a_pos.y * u_cosB;\n
+//     gl_Position.z = a_pos.z;\n
+//     gl_Position.w = a_pos.w;\n
+// }`;
 
-const FRAG_SHADER =
-    'void main() {\n\
-    gl_FragColor = vec4(1, 0, 0, 1);\n\
-}';
-
-const vertex = gl.createShader(gl.VERTEX_SHADER);
-const frag = gl.createShader(gl.FRAGMENT_SHADER);
-
-gl.shaderSource(vertex,VERTEX_SHADER);
-gl.shaderSource(frag,FRAG_SHADER);
-
-gl.compileShader(vertex);
-gl.compileShader(frag);
-
-const program = gl.createProgram();
-gl.attachShader(program,vertex);
-gl.attachShader(program.frag);
+// const FRAG_SHADER =
+//     `void main() {\n\
+//     gl_FragColor = vec4(1, 0, 0, 1);\n\
+// }`;
 
 
-gl.linkProgram(program);
-gl.useProgram(program);
+// const vertex = gl.createShader(gl.VERTEX_SHADER);
+// const frag = gl.createShader(gl.FRAGMENT_SHADER);
 
-const dataVetices = new Float32Array([
-    0.0,0.0,
-    0.0,0.5,
-    0.3,0.0,
-])
+// gl.shaderSource(vertex,VERTEX_SHADER);
+// gl.shaderSource(frag,FRAG_SHADER);
 
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
-gl.bufferData(gl.ARRAY_BUFFER,dataVetices,gl.STATIC_DRAW);
+// // 编译
+// gl.compileShader(vertex);
+// gl.compileShader(frag);
 
-const a_pos = gl.getAttribLocation(program,'a_pos');
-gl.vertexAttribPointer(a_pos,2,gl.FLOAT,false,0,0);
-gl.enableVertexAttribArray(a_pos);
+// //创建几何体
+// const program = gl.createProgram();
+// gl.attachShader(program,vertex);
+// gl.attachShader(program,frag);
+
+// // 链接几何体
+// gl.linkProgram(program);
+// gl.useProgram(program);
+
+// // 创建顶点
+// const dataVertices = new Float32Array([
+//     0.0,0.0,
+//     0.3,0.3,
+//     0.6,0.0, 
+// ]);
+
+// const buffer = gl.createBuffer();
+// gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
+
+// gl.bufferData(gl.ARRAY_BUFFER,dataVertices,gl.STATIC_DRAW);
 
 
-const u_change = gl.getUniformLocation(program,'u_change');
-let x = 0.0;
-let y = 0.0;
+// const a_pos = gl.getAttribLocation(program,'a_pos');
+// gl.vertexAttribPointer(a_pos,2,gl.FLOAT,false,0,0);
+// gl.enableVertexAttribArray(a_pos);
 
-window.addEventListener('keydown',function(ev){
-    console.log(ev.code);
 
-    if(ev.code == 'ArrowDown') {
-        y -= 0.1;
-    }else if(ev.code == 'ArrowUp') {
-        y += 0.1;
-    }else if(ev.code == 'ArrowLeft') {
-        x-= 0.1;
-    }else if(ev.code == 'ArrowRight') {
-        x += 0.1;
-    }
-    run();
+// const angle = 30;
+// const sinB = Math.sin(angle / 180 * Math.PI);
+// const cosB = Math.cos(angle / 180 * Math.PI);
 
-},false)
+// const u_sinB = gl.getUniformLocation(program,'u_sinB');
+// const u_cosB = gl.getUniformLocation(program,'u_cosB');
 
-function run() {
-    console.log(x,y);
 
-    gl.uniform4f(u_change,x,y,0.0,0.0);
-    gl.clearColor(0,0,0,1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES,0,3);
-}
+// gl.uniform1f(u_sinB,sinB);
+// gl.uniform1f(u_cosB,cosB);
 
-run();
+// // 清屏操作
+// gl.clearColor(0.0,0.0,0.0,1.0);
+// gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+// gl.drawArrays(gl.TRIANGLES,0,3);
