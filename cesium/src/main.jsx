b6dde9237ff59bfdd6117007db3d7708c2c49c7a
dezhizhang <1541609448@qq.com>
2022-09-10 21:33:59 +0800

    feat:设置视角

--- cesium/src/main.jsx
+++ cesium/src/main.jsx
@@ -5,7 +5,26 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-27 16:29:41
  * :last editor: 张德志
- * :date last edited: 2022-08-27 16:37:31
+ * :date last edited: 2022-09-10 21:31:09
  */
 import * as Cesium from 'cesium';
-const viewer = new Cesium.Viewer('root');
+import './index.css';
+
+
+const atLayer = new Cesium.UrlTemplateImageryProvider({
+    url: "https://webst02.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}",
+    minimumLevel: 3,
+    maximumLevel: 18
+})
+
+
+const viewer = new Cesium.Viewer('root',{
+    baseLayerPicker:false,
+});
+viewer.imageryLayers.addImageryProvider(atLayer);
+
+
+// viewer.scene.globe.show = true;
+// viewer.scene.camera.setView({
+//     destination:Cesium.Cartesian3.fromDegrees(116.39,39.9,1500)
+// })
