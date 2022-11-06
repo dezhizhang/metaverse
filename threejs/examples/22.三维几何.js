63a040c25643b8419724727837e43c9a714e4ad0
dezhizhang <1541609448@qq.com>
2022-11-07 07:10:54 +0800

    feat:渲染器

---
+++ threejs/examples/22.三维几何.js
@@ -0,0 +1,13 @@
+/*
+ * :file description: 
+ * :name: /threejs/examples/22.三维几何.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-08-27 17:06:49
+ * :last editor: 张德志
+ * :date last edited: 2022-08-27 17:09:16
+ */
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-21 15:01:07
  * :last editor: 张德志
- * :date last edited: 2022-08-27 15:48:55
+ * :date last edited: 2022-11-07 07:09:53
  */
 /*
  * :file description: 
@@ -18,24 +18,29 @@
  */
 
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 // 创建场影
 const scene = new THREE.Scene();
 
 // 创建相机
 const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,0.1,1000);
-camera.position.set(-20,40,50);
-camera.lookAt(new THREE.Vector3(10,0,0));
+camera.position.set(0,0,10);
 
+// 创建几何体
+const cubeGeometry = new THREE.BoxGeometry(1,1,1);
+const cubeMaterial = new THREE.MeshBasicMaterial({color:0xffff00});
+const cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
+scene.add(cube);
 
 // 创建渲染器
 const renderer = new THREE.WebGL1Renderer();
-renderer.setClearColor(new THREE.Color(0xeeeeff));
+renderer.setClearColor(new THREE.Color(0x00000));
 renderer.setSize(window.innerWidth,window.innerHeight);
-renderer.shadowMapEnabled = true;
-
 
 document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera,renderer.domElement);
 
 
 function render() {
