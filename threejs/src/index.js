36c7683c782d7790436f39f40344e1eaee3d73f5
dezhizhang <1541609448@qq.com>
2022-11-10 06:25:46 +0800

    featLupdate

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,16 +5,18 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-12 07:44:44
  * :last editor: 张德志
- * :date last edited: 2022-11-07 07:38:55
+ * :date last edited: 2022-11-10 06:18:05
  */
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
+// 创建场影
 const scene = new THREE.Scene();
 
 // 创建相机
-const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,0.1,1000);
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
 camera.position.set(0,0,10);
+scene.add(camera);
 
 // 创建几何体
 const cubeGeometry = new THREE.BoxGeometry(1,1,1);
@@ -22,24 +24,27 @@ const cubeMaterial = new THREE.MeshBasicMaterial({color:0xffff00});
 const cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
 scene.add(cube);
 
-// 创建渲染器
+// 创建渲染器 
 const renderer = new THREE.WebGL1Renderer();
-renderer.setClearColor(new THREE.Color(0x00000));
 renderer.setSize(window.innerWidth,window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-
+// 创建控制器
 const controls = new OrbitControls(camera,renderer.domElement);
 
-const axesHelper = new THREE.AxesHelper(5);
+// 创建坐标系
+const axesHelper = new THREE.AxesHelper(10);
 scene.add(axesHelper);
 
-
 function render() {
-    requestAnimationFrame(render);
+    cube.scale.set(3,2,1)
     renderer.render(scene,camera);
+    requestAnimationFrame(render);
 }
 
 render();
+
+
+
 
