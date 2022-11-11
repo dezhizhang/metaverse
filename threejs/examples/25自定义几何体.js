df38bfed1de7ee23d7bdd6a17505b68cd0a45421
dezhizhang <1541609448@qq.com>
2022-11-12 07:28:49 +0800

    feat:update

--- threejs/examples/25自定义几何体.js
+++ threejs/examples/25自定义几何体.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-12 07:44:44
  * :last editor: 张德志
- * :date last edited: 2022-11-12 07:28:21
+ * :date last edited: 2022-11-12 07:20:09
  */
 import * as THREE from 'three';
 import gsap from "gsap";
@@ -24,14 +24,14 @@ camera.position.set(0,0,10);
 scene.add(camera);
 
 // 创建几何体
-for(let i=0;i < 50;i++) {
+for(let i = 0;i < 50;i++) {
     const geometry = new THREE.BufferGeometry();
     const positionArray = new Float32Array(9);
     for(let j = 0;j < 9;j++) {
         positionArray[j] = Math.random() * 5;
     }
     geometry.setAttribute('position',new THREE.BufferAttribute(positionArray,3));
-    const colors = new THREE.Color(Math.random(),Math.random(),Math.random());
+    const colors = new THREE.Color(Math.random(),Math.random(),Math.random())
     const material = new THREE.MeshBasicMaterial({color:colors});
     const mesh = new THREE.Mesh(geometry,material);
     scene.add(mesh);
@@ -54,6 +54,20 @@ scene.add(axesHelper);
 
 const clock = new THREE.Clock();
 
+
+
+
+// gui.add(cube.position,'x').min(0).max(5).step(0.01).name("x坐标").onChange((value) => {
+//     console.log('value',value);
+// }).onFinishChange((value) => {
+//     console.log('完全停下来')
+// });
+
+// gui.add({color:'#ffff00'},'color').onChange((value) => {
+//     cube.material.color.set(value);
+// });
+
+// gui.add(cube,'visible').name('是否显示');
 
 function render() {
     renderer.render(scene,camera);
