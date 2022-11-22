72f6107977b52d4d6a75b3663ae0bbab7d680d76
dezhizhang <1541609448@qq.com>
2022-11-23 06:57:32 +0800

    feat:更新样式

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -5,57 +5,48 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-13 21:36:57
  * :last editor: 张德志
- * :date last edited: 2022-11-23 06:30:01
+ * :date last edited: 2022-11-23 06:56:53
  */
-// import VERTEX_SHADER from './vertex.glsl';
-// import FRAG_SHADER from './fragment.glsl';
 
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-
 const gl = canvas.getContext('webgl');
 
-gl.clearColor(0,0,0,1);
 
 const VERTEX_SHADER =
-    `
-    attribute vec4 a_pos;\n\
-    void main() {\n\
-    gl_Position = a_pos;\n\
-    gl_PointSize = 25.0;\n\
-}`;
+    'void main() {\n\
+    gl_Position = vec4(-0.5, 0, 0, 1);\n\
+    gl_PointSize = 10.0;\n\
+}';
 
 const FRAG_SHADER =
-    `void main() {\n\
+    'void main() {\n\
     gl_FragColor = vec4(1, 0, 0, 1);\n\
-}`;
+}';
 
 const vertex = gl.createShader(gl.VERTEX_SHADER);
-const fragment = gl.createShader(gl.FRAGMENT_SHADER);
+const frag = gl.createShader(gl.FRAGMENT_SHADER);
 
 gl.shaderSource(vertex,VERTEX_SHADER);
-gl.shaderSource(fragment,FRAG_SHADER);
-
+gl.shaderSource(frag,FRAG_SHADER);
 
 // 编译
 gl.compileShader(vertex);
-gl.compileShader(fragment);
+gl.compileShader(frag);
 
 const program = gl.createProgram();
 gl.attachShader(program,vertex);
-gl.attachShader(program,fragment);
+gl.attachShader(program,frag);
 
 gl.linkProgram(program);
 gl.useProgram(program);
 
-gl.clearColor(0.0,0.0,0.0,1.0);
+gl.clearColor(0,0,0,1);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
+
 gl.drawArrays(gl.POINTS,0,1);
-
-
 
 document.body.appendChild(canvas);
 
