027f4d9d1fd609485233b6866d952db6a4fbc258
dezhizhang <1541609448@qq.com>
2022-11-27 20:31:03 +0800

    feat:旋转

---
+++ webgl/examples/35旋转.js
@@ -0,0 +1,9 @@
+/*
+ * :file description: 
+ * :name: /webgl/examples/35旋转.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-11-27 20:30:34
+ * :last editor: 张德志
+ * :date last edited: 2022-11-27 20:30:34
+ */
