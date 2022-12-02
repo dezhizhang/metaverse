d15ecc80017b67b1e78a70ed543e46b2d379f58e
dezhizhang <1541609448@qq.com>
2022-12-03 06:15:00 +0800

    feat:变量

--- webgl/examples/39uniformt和attribute.js
+++ webgl/examples/39uniformt和attribute.js
@@ -1,11 +1,11 @@
 /*
  * :file description: 
- * :name: /webgl/src/index.js
+ * :name: /webgl/examples/39uniformt和attribute.js
  * :author: 张德志
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-13 15:47:48
  * :last editor: 张德志
- * :date last edited: 2022-12-03 06:12:55
+ * :date last edited: 2022-12-03 06:03:32
  */
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
@@ -29,13 +29,11 @@ const FRAG_SHADER = `
     }
 `;
 
-
 const vertex = gl.createShader(gl.VERTEX_SHADER);
 const frag = gl.createShader(gl.FRAGMENT_SHADER);
 
 gl.shaderSource(vertex,VERTEX_SHADER);
 gl.shaderSource(frag,FRAG_SHADER);
-
 
 gl.compileShader(vertex);
 gl.compileShader(frag);
@@ -47,9 +45,10 @@ gl.attachShader(program,frag);
 gl.linkProgram(program);
 gl.useProgram(program);
 
-const a_position = gl.getAttribLocation(program,'a_position');
+
+const a_postion = gl.getAttribLocation(program,'a_position');
 const u_color = gl.getUniformLocation(program,'u_color');
-gl.vertexAttrib4f(a_position,-0.5,0.0,0.0,1.0);
+gl.vertexAttrib4f(a_postion,-0.5,0.0,0.0,1.0);
 gl.uniform4f(u_color,1.0,1.0,0.0,1.0);
 
 gl.clearColor(0,0,0,1);
