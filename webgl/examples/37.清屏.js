02207117a650b315417882d17d7c12f71bae3dc4
dezhizhang <1541609448@qq.com>
2022-12-02 22:49:45 +0800

    feat:清屏功能

--- webgl/examples/37.清屏.js
+++ webgl/examples/37.清屏.js
@@ -1,13 +1,12 @@
 /*
  * :file description: 
- * :name: /webgl/src/index.js
+ * :name: /webgl/examples/37.清屏.js
  * :author: 张德志
  * :copyright: (c) 2022, Tungee
- * :date created: 2022-07-10 11:12:55
+ * :date created: 2022-12-02 22:49:06
  * :last editor: 张德志
- * :date last edited: 2022-12-02 22:48:11
+ * :date last edited: 2022-12-02 22:49:07
  */
-
 
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
@@ -18,6 +17,5 @@ gl.clearColor(0.0,1.0,1.0,1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
 document.body.appendChild(canvas);
-
 
 
