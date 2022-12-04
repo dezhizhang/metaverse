656be30a3685c3f0ecfe441a68bfedb24a72891f
dezhizhang <1541609448@qq.com>
2022-12-04 15:31:49 +0800

    feat:平移

--- webgl/examples/41平移操作.js
+++ webgl/examples/41平移操作.js
@@ -1,15 +1,16 @@
 /*
  * :file description: 
- * :name: /webgl/src/index.js
+ * :name: /webgl/examples/41平移操作.js
  * :author: 张德志
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-10 11:12:55
  * :last editor: 张德志
- * :date last edited: 2022-12-04 15:09:28
+ * :date last edited: 2022-12-04 15:30:14
  */
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
 const gl = canvas.getContext('webgl');
 
 
@@ -29,30 +30,28 @@ const FRAG_SHADER =
 const vertex = gl.createShader(gl.VERTEX_SHADER);
 const frag = gl.createShader(gl.FRAGMENT_SHADER);
 
-gl.shaderSource(vertex,VERTEX_SHADER);
-gl.shaderSource(frag,FRAG_SHADER);
-
+gl.attachShader(vertex,VERTEX_SHADER);
+gl.attachShader(frag,FRAG_SHADER);
 
 // 编译
 gl.compileShader(vertex);
 gl.compileShader(frag);
-
 
 // 创建几何体
 const program = gl.createProgram();
 gl.attachShader(program,vertex);
 gl.attachShader(program,frag);
 
-// 连接几何体
+// 链接几何体
 gl.linkProgram(program);
 gl.useProgram(program);
 
-// 创建顶点
 const dataVertices = new Float32Array([
     0.0,0.0,
     0.5,0.5,
     0.5,-0.5,
-])
+
+]);
 
 const buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
@@ -64,12 +63,10 @@ gl.vertexAttribPointer(a_pos,2,gl.FLOAT,false,0,0);
 gl.enableVertexAttribArray(a_pos);
 
 const u_change = gl.getUniformLocation(program,'u_change');
-
 gl.uniform4f(u_change,1.0,0.0,0.0,0.0);
-// 清屏操作
+
 gl.clearColor(0.0,0.0,0.0,1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
-
 
 gl.drawArrays(gl.TRIANGLES,0,3);
 
