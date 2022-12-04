2659626ede10ea918377b43728e6ecdc86f065b3
dezhizhang <1541609448@qq.com>
2022-12-04 16:08:44 +0800

    feat:旋转

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -5,20 +5,23 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-10 11:12:55
  * :last editor: 张德志
- * :date last edited: 2022-12-04 15:09:28
+ * :date last edited: 2022-12-04 16:06:54
  */
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const gl = canvas.getContext('webgl');
 
+const gl = canvas.getContext('webgl');
 
 const VERTEX_SHADER =
     `
     attribute vec4 a_pos;\n\
+    uniform float u_sinB,u_cosB;\n\
     void main() {\n\
-    gl_Position = a_pos;\n\
-    gl_PointSize = 25.0;\n\
+    gl_Position.x = a_pos.x * u_cosB - a_pos.y * u_sinB;\n
+    gl_Position.y = a_pos.y * u_cosB + a_pos.y * u_cosB;\n
+    gl_Position.z = a_pos.z;\n
+    gl_Position.w = a_pos.w;\n
 }`;
 
 const FRAG_SHADER =
@@ -26,50 +29,58 @@ const FRAG_SHADER =
     gl_FragColor = vec4(1, 0, 0, 1);\n\
 }`;
 
+
 const vertex = gl.createShader(gl.VERTEX_SHADER);
 const frag = gl.createShader(gl.FRAGMENT_SHADER);
 
 gl.shaderSource(vertex,VERTEX_SHADER);
 gl.shaderSource(frag,FRAG_SHADER);
 
-
 // 编译
 gl.compileShader(vertex);
 gl.compileShader(frag);
 
-
-// 创建几何体
+// 创建链接
 const program = gl.createProgram();
 gl.attachShader(program,vertex);
 gl.attachShader(program,frag);
 
-// 连接几何体
+
+// 创建链接
 gl.linkProgram(program);
 gl.useProgram(program);
+
 
 // 创建顶点
 const dataVertices = new Float32Array([
     0.0,0.0,
-    0.5,0.5,
-    0.5,-0.5,
-])
+    0.3,0.3,
+    0.6,0.0
+]);
 
 const buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
-
 gl.bufferData(gl.ARRAY_BUFFER,dataVertices,gl.STATIC_DRAW);
 
 const a_pos = gl.getAttribLocation(program,'a_pos');
 gl.vertexAttribPointer(a_pos,2,gl.FLOAT,false,0,0);
 gl.enableVertexAttribArray(a_pos);
 
-const u_change = gl.getUniformLocation(program,'u_change');
+const angle = 30;
+const sinB = Math.sin(angle / 180 * Math.PI);
+const cosB = Math.cos(angle / 180 * Math.PI);
 
-gl.uniform4f(u_change,1.0,0.0,0.0,0.0);
+const u_sinB = gl.getUniformLocation(program,'u_sinB');
+const u_cosB = gl.getUniformLocation(program,'u_cosB');
+
+
+gl.uniform1f(u_sinB,sinB);
+gl.uniform1f(u_cosB,cosB);
+
+
 // 清屏操作
 gl.clearColor(0.0,0.0,0.0,1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
-
 
 gl.drawArrays(gl.TRIANGLES,0,3);
 
