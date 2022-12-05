2e899408482392ed5124b5c2c032b96edfb99f82
dezhizhang <1541609448@qq.com>
2022-12-05 21:18:24 +0800

    feat:更新

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-10 11:12:55
  * :last editor: 张德志
- * :date last edited: 2022-12-05 21:05:33
+ * :date last edited: 2022-12-05 21:17:14
  */
 // ClickedPints.js (c) 2012 matsuda
 // Vertex shader program
@@ -13,81 +13,84 @@ import '../lib/webgl-utils';
 import '../lib/webgl-debug';
 import {getWebGLContext,initShaders} from  '../lib/cuon-utils'
 
+const VSHADER_SOURCE = `
+    attribute vec4 a_Position;\n
+    void main() {
+        gl_Position = a_Position;\n
+        gl_PointSize = 10.0;\n
+    }
+`;
 
-var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-  'void main() {\n' +
-  '  gl_Position = a_Position;\n' +
-  '  gl_PointSize = 10.0;\n' +
-  '}\n';
-
-// Fragment shader program
-var FSHADER_SOURCE =
-  'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
-  '}\n';
+const FSHADER_SOURCE = `
+    void main() {
+        gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n
+    }
+`;
 
 function main() {
-  // Retrieve <canvas> element
-  var canvas = document.createElement('canvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  
+    const canvas = document.createElement('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-  // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
-  }
+    const gl = getWebGLContext(canvas);
+    if(!gl) {
+        console.log(`Failed to get the rendering context for WebGL`);
+        return;
+    }
 
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
 
-  // // Get the storage location of a_Position
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return;
-  }
+    const a_Position = gl.getAttribLocation(gl.program,'a_Position');
+    if(a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
 
-  // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position); };
+    canvas.onmousedown = function(ev) {click(ev, gl, canvas, a_Position);}
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    document.body.appendChild(canvas);
+      // // Get the storage location of a_Position
+//   var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+//   if (a_Position < 0) {
+//     console.log('Failed to get the storage location of a_Position');
+//     return;
+//   }
 
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+//   // Register function (event handler) to be called on a mouse press
+//   canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position); };
 
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  document.body.appendChild(canvas);
+//   // Specify the color for clearing <canvas>
+//   gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+//   // Clear <canvas>
+//   gl.clear(gl.COLOR_BUFFER_BIT);
+//   document.body.appendChild(canvas);
+
 }
 
-var g_points = []; // The array for the position of a mouse press
-function click(ev, gl, canvas, a_Position) {
-  var x = ev.clientX; // x coordinate of a mouse pointer
-  var y = ev.clientY; // y coordinate of a mouse pointer
-  var rect = ev.target.getBoundingClientRect() ;
 
-  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
-  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
-  // Store the coordinates to g_points array
-  g_points.push(x); g_points.push(y);
-
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  var len = g_points.length;
-  for(var i = 0; i < len; i += 2) {
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
-
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
-  }
- 
+const g_points = [];
+function click(ev,gl,canvas,a_Position) {
+    let x = ev.clientX; // x coordinate of a mouse pointer
+    let y = ev.clientY; // y coordinate of a mouse pointer
+    var rect = ev.target.getBoundingClientRect();
+    x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+    y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+    g_points.push(x); g_points.push(y);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    const len = g_points.length;
+    for(var i = 0; i < len; i += 2) {
+        // Pass the position of a point to a_Position variable
+        gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+    
+        // Draw
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
 }
 
 main();
