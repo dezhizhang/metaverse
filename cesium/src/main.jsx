d49020523149fbd3522ad82c3655126fbb6603df
dezhizhang <1541609448@qq.com>
2022-12-24 20:49:22 +0800

    feat:更新

--- cesium/src/main.jsx
+++ cesium/src/main.jsx
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-27 16:29:41
  * :last editor: 张德志
- * :date last edited: 2022-12-24 19:18:22
+ * :date last edited: 2022-12-24 20:01:08
  */
 import * as Cesium from 'cesium';
 import './index.css';
@@ -19,13 +19,17 @@ import './index.css';
 
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NWVlNTI2MC00YTViLTQzZjYtOGMxNy1lYTAxMDVkMTMwNTQiLCJpZCI6MTA3NjIxLCJpYXQiOjE2NjI3OTY2ODR9.9Amu-saGmeaPMMt9LE5MjF0FQcoC3toDrxCo_J4ItAg';
 // An example of using a b3dm tileset to classify another b3dm tileset.
-const viewer = new Cesium.Viewer("root");
+const viewer = new Cesium.Viewer("root",{
+  terrainProvider:Cesium.createWorldTerrain({
+    
+  })
+});
 
 viewer.camera.setView({
   destination:Cesium.Cartesian3.fromDegrees(113.318977,23.114155,2000),
   orientation:{
     heading:0.60,
-  }
+  },
 })
   
 
