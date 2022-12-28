0b2e666587203a7901cd691312a83d48263f0d62
dezhizhang <1541609448@qq.com>
2022-12-28 20:53:56 +0800

    feat:调整样式

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-12 07:44:44
  * :last editor: 张德志
- * :date last edited: 2022-12-28 12:54:16
+ * :date last edited: 2022-12-28 20:51:08
  */
 import * as THREE from 'three';
 
@@ -50,97 +50,55 @@ function init() {
   group = new THREE.Group();
   scene.add(group);
   
+  let dodecahedronGeometry = new THREE.DodecahedronGeometry(10);
+  dodecahedronGeometry.deleteAttribute('normal');
+  dodecahedronGeometry.deleteAttribute('uv');
+
+  dodecahedronGeometry = BufferGeometryUtils.mergeVertices(dodecahedronGeometry);
+  
+  const vertices = [];
+  const positionAttribute = dodecahedronGeometry.getAttribute('position');
+
+  for(let i=0;i < positionAttribute.count;i++) {
+    const vertex = new THREE.Vector3();
+    vertex.fromBufferAttribute(positionAttribute,i);
+    vertices.push(vertex); 
+  }
+
+  const pointsMaterial = new THREE.PointsMaterial({
+    color:0x0080ff,
+    map:texture,
+    size:1,
+    alphaTest:0.5,
+  });
+
+  const pointsGeometry = new THREE.BufferGeometry().setFromPoints(vertices);
+  
+  const points = new THREE.Points(pointsGeometry,pointsMaterial);
+  group.add(points);
+
+  const meshMaterial = new THREE.MeshLambertMaterial({
+    color:0xffffff,
+    opacity: 0.5,
+    transparent: true,
+  });
+
+  const meshGeometry = new ConvexGeometry(vertices);
+
+  const mesh1 = new THREE.Mesh(meshGeometry, meshMaterial);
+  mesh1.material.side = THREE.BackSide; // back faces
+  mesh1.renderOrder = 0;
+  group.add(mesh1);
+
+  const mesh2 = new THREE.Mesh(meshGeometry, meshMaterial.clone());
+  mesh2.material.side = THREE.FrontSide; // front faces
+  mesh2.renderOrder = 1;
+  group.add(mesh2);
+
+  window.addEventListener('resize', onWindowResize);
   
 }
 
-// function init() {
-
-
-//   // controls
-
-//   const controls = new OrbitControls(camera, renderer.domElement);
-//   controls.minDistance = 20;
-//   controls.maxDistance = 50;
-//   controls.maxPolarAngle = Math.PI / 2;
-
-//   // ambient light
-
-//   scene.add(new THREE.AmbientLight(0x222222));
-
-//   // point light
-
-//   const light = new THREE.PointLight(0xffffff, 1);
-//   camera.add(light);
-
-//   // helper
-
-//   scene.add(new THREE.AxesHelper(20));
-
-//   // textures
-
-//   const loader = new THREE.TextureLoader();
-//   const texture = loader.load('textures/sprites/disc.png');
-
-//   group = new THREE.Group();
-//   scene.add(group);
-
-//   // points
-
-//   let dodecahedronGeometry = new THREE.DodecahedronGeometry(10);
-
-//   // if normal and uv attributes are not removed, mergeVertices() can't consolidate indentical vertices with different normal/uv data
-
-//   dodecahedronGeometry.deleteAttribute('normal');
-//   dodecahedronGeometry.deleteAttribute('uv');
-
-//   dodecahedronGeometry =
-//     BufferGeometryUtils.mergeVertices(dodecahedronGeometry);
-
-//   const vertices = [];
-//   const positionAttribute = dodecahedronGeometry.getAttribute('position');
-
-//   for (let i = 0; i < positionAttribute.count; i++) {
-//     const vertex = new THREE.Vector3();
-//     vertex.fromBufferAttribute(positionAttribute, i);
-//     vertices.push(vertex);
-//   }
-
-//   const pointsMaterial = new THREE.PointsMaterial({
-//     color: 0x0080ff,
-//     map: texture,
-//     size: 1,
-//     alphaTest: 0.5,
-//   });
-
-//   const pointsGeometry = new THREE.BufferGeometry().setFromPoints(vertices);
-
-//   const points = new THREE.Points(pointsGeometry, pointsMaterial);
-//   group.add(points);
-
-//   // convex hull
-
-//   const meshMaterial = new THREE.MeshLambertMaterial({
-//     color: 0xffffff,
-//     opacity: 0.5,
-//     transparent: true,
-//   });
-
-//   const meshGeometry = new ConvexGeometry(vertices);
-
-//   const mesh1 = new THREE.Mesh(meshGeometry, meshMaterial);
-//   mesh1.material.side = THREE.BackSide; // back faces
-//   mesh1.renderOrder = 0;
-//   group.add(mesh1);
-
-//   const mesh2 = new THREE.Mesh(meshGeometry, meshMaterial.clone());
-//   mesh2.material.side = THREE.FrontSide; // front faces
-//   mesh2.renderOrder = 1;
-//   group.add(mesh2);
-
-//   //
-
-//   window.addEventListener('resize', onWindowResize);
-// }
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
