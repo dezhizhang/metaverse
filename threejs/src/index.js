37aee521642f985a15e4fa1d23997e5799a3c1a0
dezhizhang <1541609448@qq.com>
2022-12-29 23:13:40 +0800

    feat:更新

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -14,23 +14,48 @@ function init() {
   scene.add(cube);
 
   axesHelper = new THREE.AxesHelper(100);
-
   scene.add(axesHelper);
 
-
-
-  // 
   camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
   camera.position.z = 5;
   camera.position.x = 2;
   camera.position.y = 1;
 
-
-  renderer = new THREE.WebGL1Renderer();
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth,window.innerHeight);
+
   new OrbitControls(camera,renderer.domElement);
+
   document.body.appendChild(renderer.domElement);
+
+
 }
+
+
+// function init() {
+//   scene = new THREE.Scene();
+//   const geometry = new THREE.BoxGeometry(1,1,1);
+//   const material = new THREE.MeshBasicMaterial({color:0xffff00});
+//   cube = new THREE.Mesh(geometry,material);
+
+//   scene.add(cube);
+
+//   axesHelper = new THREE.AxesHelper(100);
+
+//   scene.add(axesHelper);
+
+//   // 
+//   camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+//   camera.position.z = 5;
+//   camera.position.x = 2;
+//   camera.position.y = 1;
+
+
+//   renderer = new THREE.WebGL1Renderer();
+//   renderer.setSize(window.innerWidth,window.innerHeight);
+//   new OrbitControls(camera,renderer.domElement);
+//   document.body.appendChild(renderer.domElement);
+// }
 
 
 function render() {
