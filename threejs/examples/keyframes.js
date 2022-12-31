173f53bbc1535dec0dedc51109b2c356f32d3b43
dezhizhang <1541609448@qq.com>
2022-12-31 16:59:14 +0800

    feat:模型

---
+++ threejs/examples/keyframes.js
@@ -0,0 +1,87 @@
+import * as THREE from 'three';
+import Stats from 'stats.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+
+let mixer;
+
+const clock = new THREE.Clock();
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
+document.body.appendChild(renderer.domElement);
+
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xbfe3dd);
+scene.environment = pmremGenerator.fromScene(
+  new RoomEnvironment(),
+  0.04,
+).texture;
+
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  1,
+  100,
+);
+camera.position.set(5, 2, 8);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0.5, 0);
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('https://threejs.org/examples/jsm/libs/draco/gltf/');
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+loader.load(
+  'https://threejs.org/examples/models/gltf/LittlestTokyo.glb',
+  function (gltf) {
+    const model = gltf.scene;
+    model.position.set(1, 1, 0);
+    model.scale.set(0.01, 0.01, 0.01);
+    scene.add(model);
+
+    mixer = new THREE.AnimationMixer(model);
+    mixer.clipAction(gltf.animations[0]).play();
+
+    animate();
+  },
+  undefined,
+  function (e) {
+    console.error(e);
+  },
+);
+
+window.onresize = function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  const delta = clock.getDelta();
+
+  mixer.update(delta);
+
+  controls.update();
+
+  stats.update();
+
+  renderer.render(scene, camera);
+}
--- threejs/examples/skinningBlending.js
+++ threejs/examples/skinningBlending.js
@@ -5,5 +5,133 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-12-31 15:36:52
  * :last editor: 张德志
- * :date last edited: 2022-12-31 15:36:52
+ * :date last edited: 2022-12-31 16:48:12
  */
+import * as THREE from 'three';
+import Stats from 'stats.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; 
+
+let scene, renderer, camera, stats;
+let model, skeleton, mixer, clock;
+
+
+let singleStepMode = false;
+let sizeOfNextStep = 0;
+
+init();
+
+function init() {
+  camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,1,1000);
+  camera.position.set(1,2,-3);
+  camera.lookAt(0,1,0);
+
+  clock = new THREE.Clock();
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xa0a0a0);
+  scene.fog = new THREE.Fog(0xa0a0a0,10,50);
+
+  const hemiLight = new THREE.HemisphereLight(0xffffff,0x444444);
+  hemiLight.position.set(0,20,0);
+  scene.add(hemiLight);
+
+  const dirLight = new THREE.DirectionalLight(0xffffff);
+  dirLight.position.set(-3,10,-10);
+  dirLight.castShadow = true;
+  dirLight.shadow.camera.top = 2;
+  dirLight.shadow.camera.bottom = -2;
+  dirLight.shadow.camera.left = -2;
+  dirLight.shadow.camera.right = 2;
+  dirLight.shadow.camera.near = 0.1;
+  dirLight.shadow.camera.far = 40;
+  scene.add(dirLight);
+
+  const mesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(100,100),
+    new THREE.MeshPhongMaterial({color:0x999999,depthWrite:false})
+  )
+  mesh.rotation.x = -Math.PI / 2;
+  mesh.receiveShadow = true;
+  scene.add(mesh);
+
+  const loader = new GLTFLoader();
+  loader.load('https://tugua.oss-cn-hangzhou.aliyuncs.com/model/china.glb', function (gltf) {
+    model = gltf.scene;
+    console.log('model',model);
+
+    scene.add(model);
+
+    model.traverse(function (object) {
+      if (object.isMesh) object.castShadow = true;
+    });
+
+    skeleton = new THREE.SkeletonHelper(model);
+    skeleton.visible = false;
+    scene.add(skeleton);
+
+    const animations = gltf.animations;
+
+    mixer = new THREE.AnimationMixer(model);
+
+    //mixer.clipAction(animations[0]).play();
+ 
+
+
+    animate();
+  });
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.shadowMap.enabled = true;
+
+  const controls = new OrbitControls(camera,renderer.domElement);
+  controls.target.y = 0.5;
+  controls.update();
+
+  document.body.appendChild(renderer.domElement);
+
+  stats = new Stats();
+  document.body.appendChild(stats.dom);
+
+  window.addEventListener('resize', onWindowResize);
+
+}
+
+
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  // Render loop
+
+  requestAnimationFrame(animate);
+
+
+
+  // Get the time elapsed since the last frame, used for mixer update (if not in single step mode)
+
+  let mixerUpdateDelta = clock.getDelta();
+
+  // If in single step mode, make one step and then do nothing (until the user clicks again)
+
+  if (singleStepMode) {
+    mixerUpdateDelta = sizeOfNextStep;
+    sizeOfNextStep = 0;
+  }
+
+  // Update the animation mixer, the stats panel, and render this frame
+
+  mixer.update(mixerUpdateDelta);
+
+  stats.update();
+
+  renderer.render(scene, camera);
+}
