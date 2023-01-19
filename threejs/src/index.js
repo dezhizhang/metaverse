57d66afbed43bc544f55c1d3851453f9aafdbc69
dezhizhang <1541609448@qq.com>
2023-01-19 22:26:54 +0800

    feat:提交

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,13 +1,9 @@
+// 现在浏览器支持ES6语法，自然包括import方式引入js文件
 import * as THREE from 'three';
-import {
-  OrbitControls
-} from 'three/examples/jsm/controls/OrbitControls.js';
-import {
-  line
-} from './line.js';
-import {
-  shapeMesh
-} from './shapeMesh.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import { line } from './line.js';
+import { ExtrudeMesh } from './ExtrudeMesh.js';
 
 const scene = new THREE.Scene();
 const loader = new THREE.FileLoader();
@@ -17,75 +13,59 @@ const mapGroup = new THREE.Group();
 scene.add(mapGroup);
 
 const lineGroup = new THREE.Group();
-scene.add(lineGroup);
+mapGroup.add(lineGroup);
 
-const meshGroup = new THREE.Group();
+const meshGroup = new THREE.Group(); //轮廓Mesh组
 mapGroup.add(meshGroup);
 
-lineGroup.position.z += 0.1;
+const minHeight = 2;
+lineGroup.position.z = minHeight + minHeight * 0.1; //适当偏移解决深度冲突
+loader.load(
+  'https://tugua.oss-cn-hangzhou.aliyuncs.com/model/world.json',
+  function (data) {
+    data.features.forEach(function (area) {
+      if (area.geometry.type === 'Polygon') {
+        // 把"Polygon"和"MultiPolygon"的geometry.coordinates数据结构处理为一致
+        area.geometry.coordinates = [area.geometry.coordinates];
+      }
+      // 解析所有封闭轮廓边界坐标area.geometry.coordinates
+      lineGroup.add(line(area.geometry.coordinates)); //国家边界轮廓插入组对象mapGroup
 
-loader.load('https://tugua.oss-cn-hangzhou.aliyuncs.com/model/china.json', function (data) {
-  data.features.forEach(function (area) {
-    if (area.geometry.type === "Polygon") {
-      // 把"Polygon"和"MultiPolygon"的geometry.coordinates数据结构处理为一致
-      area.geometry.coordinates = [area.geometry.coordinates];
-    }
-    // 解析所有封闭轮廓边界坐标area.geometry.coordinates
-    lineGroup.add(line(area.geometry.coordinates)); //省份边界轮廓插入组对象mapGroup
-    meshGroup.add(shapeMesh(area.geometry.coordinates)); //省份轮廓Mesh插入组对象mapGroup
-  })
-});
+      meshGroup.add(ExtrudeMesh(area.geometry.coordinates, minHeight)); //国家轮廓Mesh插入组对象mapGroup
+    });
+  },
+);
 
-loader.load('https://tugua.oss-cn-hangzhou.aliyuncs.com/model/weibo.json', function (data) {
-  const coord = data.coordinates; //所有经纬度坐标数据
-  var verticesArr = []; //所有顶点数据，三个元素为一组
-  for (var i = 0; i < coord.length; i += 2) {
-    verticesArr.push(coord[i], coord[i + 1], 0); //经纬度作为顶点xy坐标，顶点z坐标设置为0        
-  }
-  var geometry = new THREE.BufferGeometry(); //创建一个Buffer类型几何体对象
-  //3个为一组，表示一个顶点的xyz坐标
-  var attribute = new THREE.BufferAttribute(new Float32Array(verticesArr), 3);
-  // 设置几何体attributes属性的位置属性
-  geometry.attributes.position = attribute;
-  // 点渲染模式
-  var material = new THREE.PointsMaterial({
-    color: 0xffff00,
-    size: 1.5 //点对象像素尺寸
-  }); //材质对象
-  var points = new THREE.Points(geometry, material); //点模型对象
-  points.position.z = 0.1;
-  scene.add(points); //点对象添加到场景中
-});
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.position.set(400, 200, 300);
+scene.add(directionalLight);
+
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight2.position.set(-400, -200, -300);
+scene.add(directionalLight2);
+
+const ambient = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambient);
 
 const axesHelper = new THREE.AxesHelper(300);
 scene.add(axesHelper);
 
-// 相机设置
 const width = window.innerWidth;
 const height = window.innerHeight;
-const k = width / height; //窗口宽高比
-// const s = 200;
-const s = 20; //根据包围盒大小(行政区域经纬度分布范围大小)设置渲染范围
-//创建相机对象
-const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
-// camera.position.set(200, 300, 200); //设置相机位置
-camera.position.set(104, 35, 200); //沿着z轴观察
-camera.lookAt(104, 35, 0); //指向中国地图的几何中心
+const s = 90;
+const k = width / height;
+const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1500);
+camera.position.set(0, -100, 200); //沿着z轴观察
+camera.lookAt(0, 0, 0); //指向中国地图的几何中心
 
-const renderer = new THREE.WebGLRenderer({
-  antialias: true, //开启锯齿
-});
-renderer.setSize(width,height);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
-
 function render() {
-  renderer.render(scene,camera);
+  renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
 
 render();
-
-const controls = new OrbitControls(camera,renderer.domElement);
-controls.target.set(104, 35, 0);
-controls.update();
+const controls = new OrbitControls(camera, renderer.domElement);
