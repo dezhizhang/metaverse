8b792cfeb653e66d7046fb0435ab3d23dc644666
dezhizhang <1541609448@qq.com>
2023-01-19 21:24:44 +0800

    feat:添加图形

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,90 +1,83 @@
 import * as THREE from 'three';
-// 引入Three.js扩展库
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { line } from './line.js';
-import { ExtrudeMesh } from './ExtrudeMesh.js';
-import { cityPointMesh } from './cityPointMesh.js';
+import {
+  OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js';
+import {
+  line
+} from './line.js';
+import {
+  shapeMesh
+} from './shapeMesh.js';
 
-
-const mapHeight = 0.8;//拉伸高度
-//  创建场景
 const scene = new THREE.Scene();
-// 创建加载器
 const loader = new THREE.FileLoader();
 loader.setResponseType('json');
 
 const mapGroup = new THREE.Group();
 scene.add(mapGroup);
 
-// 创建线的group
 const lineGroup = new THREE.Group();
-mapGroup.add(lineGroup);
+scene.add(lineGroup);
 
 const meshGroup = new THREE.Group();
 mapGroup.add(meshGroup);
-lineGroup.position.z = mapHeight * 0.1;
+
+lineGroup.position.z += 0.1;
 
 loader.load('https://tugua.oss-cn-hangzhou.aliyuncs.com/model/china.json', function (data) {
-  // 访问所有省份边界坐标数据：data.features
   data.features.forEach(function (area) {
-    // "Polygon"：省份area有一个封闭轮廓
-    //"MultiPolygon"：省份area有多个封闭轮廓
     if (area.geometry.type === "Polygon") {
       // 把"Polygon"和"MultiPolygon"的geometry.coordinates数据结构处理为一致
       area.geometry.coordinates = [area.geometry.coordinates];
     }
     // 解析所有封闭轮廓边界坐标area.geometry.coordinates
-    lineGroup.add(line(area.geometry.coordinates));//省份边界轮廓插入组对象mapGroup
-    // mapHeight：根据行政区尺寸范围设置，比如高度设置为地图尺寸范围的2%、5%等，过小感觉不到高度，过大太高了
-    meshGroup.add(ExtrudeMesh(area.geometry.coordinates, mapHeight));//省份轮廓拉伸Mesh插入组对象mapGroup
-
-    console.log(area);
-
-    // 标注出来省份的行政中心
-    var pos = area.properties.cp;//每个省份行政中心位置经纬度
-    var size = 1.2;//大小根据地图尺寸范围设置或者说相机渲染范围
-    console.log(pos);
-    var mesh = cityPointMesh(size, pos[0], pos[1]);
-    mesh.position.z = mapHeight + mapHeight * 0.01;
-    // mapGroup.add(mesh);
-  });
-  // 地图底部边界线
-  var lineGroup2 = lineGroup.clone();
-  mapGroup.add(lineGroup2);
-  lineGroup2.position.z = -mapHeight * 0.1;//适当偏移解决深度冲突
+    lineGroup.add(line(area.geometry.coordinates)); //省份边界轮廓插入组对象mapGroup
+    meshGroup.add(shapeMesh(area.geometry.coordinates)); //省份轮廓Mesh插入组对象mapGroup
+  })
 });
 
-// 平行光1
-const directionalLight = new THREE.DirectionalLight(0xffffff,0.6);
-directionalLight.position.set(400,200,300);
-scene.add(directionalLight);
-
-// 平行光2
-const directionalLight2 = new THREE.DirectionalLight(0xffffff,0.6);
-directionalLight2.position.set(-400,-200,-300);
-scene.add(directionalLight2);
-
-const ambient = new THREE.AmbientLight(0xffffff,0.6);
-scene.add(ambient);
+loader.load('https://tugua.oss-cn-hangzhou.aliyuncs.com/model/weibo.json', function (data) {
+  const coord = data.coordinates; //所有经纬度坐标数据
+  var verticesArr = []; //所有顶点数据，三个元素为一组
+  for (var i = 0; i < coord.length; i += 2) {
+    verticesArr.push(coord[i], coord[i + 1], 0); //经纬度作为顶点xy坐标，顶点z坐标设置为0        
+  }
+  var geometry = new THREE.BufferGeometry(); //创建一个Buffer类型几何体对象
+  //3个为一组，表示一个顶点的xyz坐标
+  var attribute = new THREE.BufferAttribute(new Float32Array(verticesArr), 3);
+  // 设置几何体attributes属性的位置属性
+  geometry.attributes.position = attribute;
+  // 点渲染模式
+  var material = new THREE.PointsMaterial({
+    color: 0xffff00,
+    size: 1.5 //点对象像素尺寸
+  }); //材质对象
+  var points = new THREE.Points(geometry, material); //点模型对象
+  points.position.z = 0.1;
+  scene.add(points); //点对象添加到场景中
+});
 
 const axesHelper = new THREE.AxesHelper(300);
 scene.add(axesHelper);
 
+// 相机设置
 const width = window.innerWidth;
 const height = window.innerHeight;
-
-const k = width / height;
-const s = 200;
+const k = width / height; //窗口宽高比
+// const s = 200;
+const s = 20; //根据包围盒大小(行政区域经纬度分布范围大小)设置渲染范围
+//创建相机对象
 const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
-camera.position.set(104,-105,200);
-camera.lookAt(104,35,0);
+// camera.position.set(200, 300, 200); //设置相机位置
+camera.position.set(104, 35, 200); //沿着z轴观察
+camera.lookAt(104, 35, 0); //指向中国地图的几何中心
 
 const renderer = new THREE.WebGLRenderer({
-  antialias:true
+  antialias: true, //开启锯齿
 });
-
 renderer.setSize(width,height);
 document.body.appendChild(renderer.domElement);
+
 
 function render() {
   renderer.render(scene,camera);
@@ -94,5 +87,5 @@ function render() {
 render();
 
 const controls = new OrbitControls(camera,renderer.domElement);
-controls.target.set(109,35,0);
+controls.target.set(104, 35, 0);
 controls.update();
--- threejs/src/line.js
+++ threejs/src/line.js
@@ -14,28 +14,20 @@ function line(pointsArrs) {
   return group;
 }
 
-
-// pointArr：行政区一个多边形轮廓边界坐标(2个元素为一组，分别表示一个顶点x、y值)
 function pointLine(pointArr) {
-  /**
-   * 通过BufferGeometry构建一个几何体，传入顶点数据
-   * 通过Line模型渲染几何体，连点成线
-   * LineLoop和Line功能一样，区别在于首尾顶点相连，轮廓闭合
-   */
-  var geometry = new THREE.BufferGeometry(); //创建一个Buffer类型几何体对象
-  //类型数组创建顶点数据
-  var vertices = new Float32Array(pointArr);
-  // 创建属性缓冲区对象
-  var attribue = new THREE.BufferAttribute(vertices, 3); //3个为一组，表示一个顶点的xyz坐标
-  // 设置几何体attributes属性的位置属性
+  const geometry = new THREE.BufferGeometry();
+  const vertices = new Float32Array(pointArr);
+
+  const attribue = new THREE.BufferAttribute(vertices,3);
   geometry.attributes.position = attribue;
-  // 线条渲染几何体顶点数据
-  var material = new THREE.LineBasicMaterial({
+
+  const material = new THREE.LineBasicMaterial({
     color: 0x00cccc //线条颜色
-  });//材质对象
-  // var line = new THREE.Line(geometry, material);//线条模型对象
-  var line = new THREE.LineLoop(geometry, material);//首尾顶点连线，轮廓闭合
+  });
+
+  const line = new THREE.LineLoop(geometry,material);
   return line;
+
 }
 
 export { line };
