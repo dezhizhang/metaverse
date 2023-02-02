4341f6ca643cda4bb0143022e261a725622b34c5
dezhizhang <1541609448@qq.com>
2023-02-02 08:02:51 +0800

    feat:设置多个几何体

--- threejs/examples/84.ShapeGeometry.js
+++ threejs/examples/84.ShapeGeometry.js
@@ -1,30 +1,30 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// 创建场景
+
 const scene = new THREE.Scene();
 
-// 光源设置
+// 平行光1
 const directionalLight = new THREE.DirectionalLight(0xffffff,0.8);
 directionalLight.position.set(400,200,300);
 scene.add(directionalLight);
 
 // 平行光2
-const directionalLigh2 = new  THREE.DirectionalLight(0xffffff,0.8);
-directionalLigh2.position.set(-400,-200,-300);
-scene.add(directionalLigh2);
+const directionalLight2 = new THREE.DirectionalLight(0xffffff,0.8);
+directionalLight2.position.set(-400,-200,-300);
+scene.add(directionalLight2);
 
 // 环境光
 const ambient = new THREE.AmbientLight(0xffffff,0.3);
 scene.add(ambient);
 
-// 三维坐标轴
-const axesHelper = new THREE.AxesHelper(300);
+// 设置辅肋坐标
+const axesHelper = new THREE.AxesHelper(250);
 scene.add(axesHelper);
 
 const model = new THREE.Group();
 
-const shape1 = new THREE.Shape([
+const shape = new THREE.Shape([
   new THREE.Vector2(0, 0),
   new THREE.Vector2(60, 0),
   new THREE.Vector2(60, 80),
@@ -32,32 +32,28 @@ const shape1 = new THREE.Shape([
   new THREE.Vector2(-20, 80),
 ]);
 
-const shape2 = new THREE.Shape([
-  new THREE.Vector2(100, 0),
-  new THREE.Vector2(160, 0),
-  new THREE.Vector2(160, 80),
-  new THREE.Vector2(140, 120),
-])
-
-const geometry = new THREE.ShapeGeometry([shape1,shape2]);
+// 填充多边形
+const geometry = new THREE.ShapeGeometry(shape);
 
 const material = new THREE.MeshLambertMaterial({
   color: 0x00ffff,
-  side:THREE.DoubleSide,
+  side:THREE.DoubleSide
 });
 
 const mesh = new THREE.Mesh(geometry,material);
-model.add(mesh);
+scene.add(mesh);
 scene.add(model);
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const camera = new THREE.PerspectiveCamera(30, width / height,1,3000);
-camera.position.set(292, 223, 185);
-camera.lookAt(scene.position);
+// 透视投影相机设置
+const camera = new THREE.PerspectiveCamera(30,width / height,1,3000);
+camera.position.set(292,223,185);
+camera.lookAt(0,0,0);
 
-// 创建沉浸对像
+
+// 创建渲染对像
 const renderer = new THREE.WebGLRenderer({
   antialias:true
 });
@@ -66,13 +62,17 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(width,height);
 document.body.appendChild(renderer.domElement);
 
-
 const controls = new OrbitControls(camera,renderer.domElement);
 
+window.onresize = function() {
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
 
 function render() {
   renderer.render(scene,camera);
   requestAnimationFrame(render);
 }
-
 render();
+
---
+++ threejs/examples/85.黄浦江.js
@@ -0,0 +1,82 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+// 平行光1
+const directionalLight = new THREE.DirectionalLight(0xffffff,0.8);
+directionalLight.position.set(200,400,300);
+scene.add(directionalLight);
+
+// 平行光2
+const directionalLight2 = new THREE.DirectionalLight(0xffffff,0.8);
+directionalLight2.position.set(-200, -400, 300);
+scene.add(directionalLight2);
+
+// 环境光
+const ambient = new THREE.AmbientLight(0xffffff,0.3);
+scene.add(ambient);
+
+function ShapeMesh(pointsArr) {
+  let vector2Arr = [];
+  pointsArr[0].forEach(elem => {
+    vector2Arr.push(new THREE.Vector2(elem[0],elem[1]));
+  });
+
+  const shape = new THREE.Shape(vector2Arr);
+  const geometry = new THREE.ShapeGeometry(shape);
+  const material = new THREE.MeshLambertMaterial({
+    color: 0x0099ff
+  });
+
+  const mesh = new THREE.Mesh(geometry,material);
+  return mesh;
+}
+
+const model = new THREE.Group();
+const loader = new THREE.FileLoader();
+loader.setResponseType('json');
+
+loader.load('https://tugua.oss-cn-hangzhou.aliyuncs.com/model/huangpu-river.json',function(data) {
+  const buildGroup = new THREE.Group();
+  data.features.forEach(build => {
+    buildGroup.add(ShapeMesh(build.geometry.coordinates));
+  });
+  model.add(buildGroup);
+  scene.add(model);
+});
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+const camera = new THREE.PerspectiveCamera(30,width / height,0.001,3000);
+camera.position.set(292,)
+const x = 121.49526536464691;//东方明珠经纬度坐标
+const y = 31.24189350905988;
+camera.position.set(x+0.02, y+0.02, 0.02);//0.02是根据黄浦江尺寸范围设置  数量级对应即可 具体数值不用精准
+camera.lookAt(x,y,0);//根据黄浦江几何中心坐标或附近某个经纬度坐标设置
+
+// 创建渲染器对象
+const renderer = new THREE.WebGLRenderer({
+  antialias:true
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(width,height);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera,renderer.domElement);
+controls.target.set(x,y,0);
+controls.update();
+
+window.onreset = function() {
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
+
+function render() {
+  renderer.render(scene,camera);
+  requestAnimationFrame(render);
+}
+
+render();
