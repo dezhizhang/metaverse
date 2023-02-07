7469db409a7b626448413d6e0273dd63c4113ed3
dezhizhang <1541609448@qq.com>
2023-02-08 04:56:19 +0800

    feat:program

--- webgl/examples/48函数封装.js
+++ webgl/examples/48函数封装.js
@@ -1,13 +1,3 @@
-/*
- * :file description: 
- * :name: /webgl/src/index.js
- * :author: 张德志
- * :copyright: (c) 2023, Tungee
- * :date created: 2022-07-10 11:12:55
- * :last editor: 张德志
- * :date last edited: 2023-02-08 04:50:03
- */
-
 const canvas = document.createElement('canvas');
 canvas.width = 400;
 canvas.height = 400;
