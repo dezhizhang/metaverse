3971f036a0ac91598e0fa08385659c60086fa7ad
dezhizhang <1541609448@qq.com>
2023-02-08 06:17:04 +0800

    feat:uniform

--- webgl/examples/49attribute.js
+++ webgl/examples/49attribute.js
@@ -1,12 +1,13 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /webgl/examples/49attribute.js
  * :author: 张德志
  * :copyright: (c) 2023, Tungee
- * :date created: 2023-02-08 05:22:48
+ * :date created: 2022-07-10 11:12:55
  * :last editor: 张德志
- * :date last edited: 2023-02-08 05:23:11
+ * :date last edited: 2023-02-08 05:37:34
  */
+
 const canvas = document.createElement('canvas');
 canvas.width = 400;
 canvas.height = 400;
@@ -15,9 +16,10 @@ const gl = canvas.getContext('webgl');
 
 const vertex = `
 attribute vec2 a_position;
+attribute float a_size;
 void main() {
     gl_Position = vec4(a_position, 0, 1);
-    gl_PointSize = 10.0;
+    gl_PointSize = a_size;
 }`;
 
 const fragment = `
@@ -49,8 +51,10 @@ function createProgram(gl, vertexShader, fragmentShader) {
 const program = createProgram(gl, vertexShader, fragmentShader);
 
 const aPostion = gl.getAttribLocation(program, 'a_position');
+const aSize = gl.getAttribLocation(program,'a_size');
 // console.log('aPostion',aPostion)
 gl.vertexAttrib2f(aPostion, 0.6, 0);
+gl.vertexAttrib1f(aSize,20.0);
 
 
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
