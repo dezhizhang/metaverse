8f5c58552a1f263188b02f57ea5aa16874812724
dezhizhang <1541609448@qq.com>
2023-03-13 06:37:06 +0800

    feat:update

--- canvas/src/index.js
+++ canvas/src/index.js
@@ -1,70 +1,10 @@
-/*
- * :file description: 
- * :name: /canvas/src/index.js
- * :author: 张德志
- * :copyright: (c) 2022, Tungee
- * :date created: 2022-09-10 22:09:46
- * :last editor: 张德志
- * :date last edited: 2022-09-11 22:28:43
- */
 
 const canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
-
-canvas.width = 500;
-canvas.height = 500;
-
-canvas.style.background = '#fff';
 const ctx = canvas.getContext('2d');
 
-function Arc(x,y,r,c) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.c = c || 'black';
-    this.draw();
-}
+ctx.moveTo(50,100);
+ctx.lineTo(150,50);
+ctx.stroke();
 
-Arc.prototype.draw = function() {
-    ctx.save();
-    ctx.fillStyle = this.c;
-    ctx.beginPath();
-    ctx.arc(this.x,this.y,this.r,0,360 * Math.PI / 180);
-    ctx.closePath();
-    ctx.fill();
-    ctx.restore();
-
-}
-
-Arc.prototype.reDraw = function(x,y,cb) {
-    ctx.fillStyle = this.c;
-    ctx.beginPath();
-    ctx.arc(this.x,this.y,this.r,0,360 * Math.PI / 180);
-    ctx.closePath();
-    ctx.fill();
-    if(ctx.isPointInPath(x,y)) {
-        cb();
-    }
-}
-
-
-
-const a1 = new Arc(100,100,50);
-const a2 = new Arc(200,200,50,'red');
-
-canvas.onmousedown = function(ev) {
-    let x = ev.pageX - canvas.offsetLeft;
-    let y = ev.pageY - canvas.offsetTop;
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    
-
-    a1.reDraw(x,y,function() {
-        console.log('123');
-    });
-
-    a2.reDraw(x,y,function() {
-        console.log('456');
-        
-    })
-}
+document.body.appendChild(canvas);
 
