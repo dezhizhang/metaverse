fe6637b7ffb629fb6bd89467f5cf3704527c66bd
dezhizhang <1541609448@qq.com>
2023-03-25 22:52:39 +0800

    feat:最小单位

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -10,49 +10,29 @@
 import * as THREE from 'three';
 
 
-// // 创㶳射线ray
-// let ray = new THREE.Ray();
-// ray.origin = new THREE.Vector3(0,0,3);
-
-// ray.direction = new THREE.Vector3(1,1,1).normalize();
-
-// const p1 = new THREE.Vector3(20,0,0);
-// const p2 = new THREE.Vector3(0,0,10);
-// const p3 = new THREE.Vector3(0,30,0);
-
-// const result = ray.intersectTriangle(p1,p2,p3);
-// console.log('查看是否相交',result);
-
-// const triangle = new THREE.Triangle();
-
-// triangle.a = new THREE.Vector3(20,0,0);
-
-// triangle.b = new THREE.Vector3(0,0,10);
-
-// triangle.c = new THREE.Vector3(0,30,0);
-
-// const s = triangle.getArea();
-
-// console.log('三角形面积',s);
+const scene = new THREE.Scene();
 
 
-// const triangle = new THREE.Triangle();
-// triangle.a = new THREE.Vector3(20,0,0);
-// triangle.b = new THREE.Vector3(0,0,10);
-// triangle.c = new THREE.Vector3(0,30,0);
-
-// const s = triangle.getArea();
-// console.log('三角形面积',s);
-
-const triangle = new THREE.Triangle();
-triangle.a = new THREE.Vector3(20,0,0);
-triangle.b = new THREE.Vector3(0,0,10);
-triangle.c = new THREE.Vector3(0,30,0);
-
-const s = triangle.getArea();
-console.log('三角形面积',s);
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial();
+const cube = new THREE.Mesh(geometry,material);
+scene.add(cube);
 
 
+const light = new THREE.AmbientLight();
+scene.add(light);
+
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.set(0,0,5);
+camera.lookAt(scene.position);
+
+
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.render(scene,camera);
+
+document.body.append(renderer.domElement);
 
 
 
