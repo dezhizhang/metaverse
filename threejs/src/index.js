f719db45f485c84617c3f196e49cfeb0f9348a84
dezhizhang <1541609448@qq.com>
2023-03-26 11:31:12 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -8,15 +8,23 @@
  * :date last edited: 2023-02-18 06:27:04
  */
 import * as THREE from 'three';
+import Stats from 'stats.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+const stats = new Stats();
 
 const scene = new THREE.Scene();
-
-
 const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial();
 const cube = new THREE.Mesh(geometry,material);
 scene.add(cube);
+
+
+
+
+const axes = new THREE.AxesHelper(2,2,2);
+scene.add(axes);
+
 
 
 const light = new THREE.AmbientLight();
@@ -32,7 +40,25 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.render(scene,camera);
 
-document.body.append(renderer.domElement);
+const orbitControls = new OrbitControls(camera,renderer.domElement);
+
+let time = Date.now();
+function tick() {
+    let currentTime = Date.now();
+    let deltaTime = currentTime - time;
+    time = currentTime;
+
+    cube.rotation.z += deltaTime * 0.001;
+    renderer.render(scene,camera);
+    requestAnimationFrame(tick);
+    stats.update();
+}
+
+tick();
+
+document.body.appendChild(renderer.domElement);
+document.body.appendChild(stats.domElement);
+
 
 
 
