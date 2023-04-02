0258e267e858f798a12241fe5fe73e9b288c932a
dezhizhang <1541609448@qq.com>
2023-04-02 18:18:32 +0800

    feat:添加几何体

--- threejs6/src/pages/scene/TEngine.ts
+++ threejs6/src/pages/scene/TEngine.ts
@@ -5,159 +5,42 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-01-01 21:46:05
  * :last editor: 张德志
- * :date last edited: 2023-01-04 06:07:00
+ * :date last edited: 2023-04-02 18:18:00
  */
-import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { BoxGeometry, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
 
 class TEngine {
-  private dom: HTMLElement;
-  private renderer: WebGLRenderer;
   private scene:Scene;
+  private dom:HTMLElement;
+  private renderer:WebGLRenderer;
   private camera:PerspectiveCamera;
 
-  constructor(dom: HTMLElement) {
+  constructor(dom:HTMLElement) {
     this.dom = dom;
+    this.scene = new Scene();
     this.renderer = new WebGLRenderer();
+    this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight,0.1,1000);
+    this.camera.position.set(20,20,20);
+    this.camera.lookAt(new Vector3(0,0,0));
+    this.camera.up = new Vector3(0,1,0);
+
     this.dom.appendChild(this.renderer.domElement);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setSize(window.innerWidth,window.innerHeight);
+
+    const box:Mesh = new Mesh(
+      new BoxGeometry(10,10,10),
+      new MeshStandardMaterial(),
+    )
+
+    this.scene.add(box);
+    this.renderer.setClearColor('rgb(255,255,255)');
+
+    this.renderer.render(this.scene,this.camera);
+
+
   }
+  
+
 }
 
 export default TEngine;
-
-// import {
-//   WebGLRenderer,
-//   Scene,
-//   PerspectiveCamera,
-//   Vector3,
-//   Object3D,
-//   Vector2,
-//   Raycaster,
-// } from 'three';
-// import Stats from 'stats.js';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-// import { TransformControls } from 'three/examples/jsm/controls/TransformControls';
-// import { basicObjectList } from './TBasicObject';
-// import { lightsList } from './TLights';
-// import { helperList } from './THelper';
-// import { codeModelList } from './TCodeModel';
-// import { framePromise } from './TLoadModel';
-
-///////////////////////////////////////////////////////
-
-// class TEngine {
-//   private dom: HTMLElement;
-//   private renderer: WebGLRenderer;
-//   private scene: Scene;
-//   private stats: Stats;
-//   private camera: PerspectiveCamera;
-//   constructor(dom: HTMLElement) {
-//     this.dom = dom;
-//     // 创建渲染器
-//     this.renderer = new WebGLRenderer({ antialias: true });
-//     this.renderer.setSize(window.innerWidth, window.innerHeight);
-//     this.renderer.setPixelRatio(window.devicePixelRatio);
-//     this.renderer.shadowMap.enabled = true;
-
-//     // 创建场影
-//     this.scene = new Scene();
-
-//     // 创建相机
-//     this.camera = new PerspectiveCamera(
-//       45,
-//       window.innerWidth / window.innerHeight,
-//       0.1,
-//       1000,
-//     );
-//     this.camera.position.set(20, 20, 20);
-//     this.camera.lookAt(new Vector3(0, 0, 0));
-//     this.camera.up = new Vector3(0, 1, 0);
-
-//     this.addObject(...lightsList);
-//     // this.addObject(...codeModelList);
-//     // this.addObject(...basicObjectList);
-//     framePromise.then((group)=> {
-//       // group.position.y = 10;
-//       this.addObject(group as any);
-//     });
-
-//     // 添加坐标线
-//     this.addObject(...helperList);
-
-//     // 添加性能监控
-//     this.stats = new Stats();
-//     this.dom.appendChild(this.stats.dom);
-
-//     // 初始化
-//     const controls = new OrbitControls(this.camera, this.renderer.domElement);
-
-//     const transformControls = new TransformControls(this.camera,this.renderer.domElement);
-//     this.scene.add(transformControls);
-
-//     let transing = false;
-
-//     const raycaster = new Raycaster()
-//     // const target = new Object3D();
-//     // transformControls.attach(target);
-//     // this.scene.add(target);
-
-//     const mouse = new Vector2();
-//     this.renderer.domElement.addEventListener('mousemove',(event) => {
-//       const x = event.offsetX;
-//       const y = event.offsetY;
-
-//       const width = this.renderer.domElement.offsetWidth;
-//       const height = this.renderer.domElement.offsetHeight;
-
-//       mouse.x = x / width * 2 - 1;
-//       mouse.y = -y * 2 / height + 1;
-
-//       // raycaster.setFromCamera(mouse,this.camera);
-//       // const intersect = raycaster.intersectObjects(this.scene.children);
-//       // if(intersect.length > 0) {
-//       //   console.log(intersect)
-//       // }
-//     });
-
-//     this.renderer.domElement.addEventListener('click',(event) => {
-//       if(transing){
-//         transing = false;
-//         return;
-//       }
-//       raycaster.setFromCamera(mouse,this.camera);
-//       this.scene.remove(transformControls);
-//       const intersect = raycaster.intersectObjects(this.scene.children);
-//       this.scene.remove(transformControls);
-//       if(intersect.length) {
-//         const object = intersect[0].object;
-//         transformControls.attach(object);
-//         console.log(intersect)
-//       }
-//     })
-
-//     this.dom.appendChild(this.renderer.domElement);
-
-//     window.addEventListener('resize', () => {
-//       this.camera.aspect = window.innerWidth / window.innerHeight;
-//       this.camera.updateProjectionMatrix();
-//       this.renderer.setSize(window.innerWidth, window.innerHeight);
-//     });
-
-//     const renderFn = () => {
-
-//       this.stats.update();
-//       controls.update();
-//       this.renderer.render(this.scene, this.camera);
-//       requestAnimationFrame(renderFn);
-//     };
-//     renderFn();
-//   }
-//   // 添加几何体
-//   addObject(...object: Object3D[]) {
-//     object.forEach((element) => {
-//       this.scene.add(element);
-//     });
-//   }
-// }
-
-// export default TEngine;
