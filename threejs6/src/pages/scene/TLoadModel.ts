ce31391fb00c2362eeaf26edfd9d4a1217b5a4fd
张德志 <zhangdezhi@tungee.com>
2023-04-06 05:05:10 +0800

    feat:优化代码

--- threejs6/src/pages/scene/TLoadModel.ts
+++ threejs6/src/pages/scene/TLoadModel.ts
@@ -5,21 +5,44 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-01-03 07:12:16
  * :last editor: 张德志
- * :date last edited: 2023-04-06 04:49:12
+ * :date last edited: 2023-04-06 05:03:23
  */
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { MeshStandardMaterial } from 'three';
+import { Material, Mesh, MeshStandardMaterial, Object3D } from 'three';
 import { frameColorTexture, frameRoughnessTexture,frameDispTexture } from './TTextures';
 
 const url = 'https://tugua.oss-cn-hangzhou.aliyuncs.com/model';
 
 const objLoader: OBJLoader = new OBJLoader();
 
-export const framePromise = objLoader.loadAsync(`${url}/frame.obj`);
 
 export const frameMaterial:MeshStandardMaterial = new MeshStandardMaterial({
   map:frameColorTexture,
   roughnessMap:frameRoughnessTexture,
   bumpMap:frameDispTexture
+});
+
+export const framePromise = new Promise<Object3D>((resolve,reject) => {
+  objLoader.loadAsync(`${url}/frame.obj`).then(group => {
+    const frame:Mesh = group.children[0] as Mesh;
+    (frame.material as Material).dispose();
+    frame.material = frameMaterial;
+    
+    frame.position.y = 45;
+    frame.position.z = -1;
+    frame.rotation.y = Math.PI / 180 * -90;
+    frame.scale.set(2,2,2)
+
+    resolve(frame);
+  }).catch(err => {
+    reject(err)
+  })
 })
+
+
+
+
+
+
+
 
--- threejs6/src/pages/scene/index.tsx
+++ threejs6/src/pages/scene/index.tsx
@@ -5,7 +5,7 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-01-01 21:33:33
  * :last editor: 张德志
- * :date last edited: 2023-04-06 04:47:49
+ * :date last edited: 2023-04-06 05:04:30
  */
 import React, { useRef, useEffect } from 'react';
 import styles from './index.less';
@@ -14,8 +14,7 @@ import { lightsList } from './TLights';
 import {helperList} from './THelper';
 import { basicObjectList } from './TBasicObject';
 import { codeModelList } from './TCodeModel';
-import { framePromise,frameMaterial } from './TLoadModel';
-import { Material, Mesh } from 'three';
+import { framePromise } from './TLoadModel';
 
 const Scene: React.FC = () => {
   const ref = useRef(null);
@@ -27,16 +26,9 @@ const Scene: React.FC = () => {
     TE.addObject(...helperList);
     TE.addObject(...codeModelList);
 
-    framePromise.then(group => {
-      const frameMesh:Mesh = group.children[0] as Mesh;
-      (frameMesh.material as Material).dispose();
-      frameMesh.material = frameMaterial;
-
-      group.position.y = 45;
-      group.position.z = -1;
-      group.rotation.y = Math.PI / 180 * -90;
-      group.scale.set(2,2,2)
-      TE.addObject(group);
+    framePromise.then(frame => {
+     
+      TE.addObject(frame);
     })
 
   }, []);
