368ea48bcda1d0f77b9c8ea57fb93d016727bf25
dezhizhang <1541609448@qq.com>
2023-04-22 21:57:34 +0800

    feat:renderer

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-02-17 21:42:17
  * :last editor: 张德志
- * :date last edited: 2023-04-22 21:47:47
+ * :date last edited: 2023-04-22 21:55:59
  */
 import * as THREE from "three";
 import Stats from "stats.js";
@@ -19,6 +19,27 @@ const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({color:0xff0000});
 const mesh = new THREE.Mesh(geometry,material);
 scene.add(mesh);
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.set(10,10,10);
+camera.lookAt(scene.position);
+
+scene.add(camera);
+
+
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+function render() {
+  renderer.render(scene,camera);
+  requestAnimationFrame(render);
+  
+
+}
+
+render();
 
 
 
