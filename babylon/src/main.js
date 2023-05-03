cf5a917b830ea3ae1db57f19c8c796904beaf225
dezhizhang <1541609448@qq.com>
2023-05-04 06:43:13 +0800

    feat:添加几何体

--- babylon/src/main.js
+++ babylon/src/main.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-05-04 05:38:45
  * :last editor: 张德志
- * :date last edited: 2023-05-04 06:20:55
+ * :date last edited: 2023-05-04 06:37:13
  */
 import './style.css'
 
@@ -25,9 +25,11 @@ const scene = new BABYLON.Scene(engine);
 
 //创建相机
 const camera = new BABYLON.ArcRotateCamera('camera',0,0,10,BABYLON.Vector3.Zero(),scene);
+camera.setPosition(new BABYLON.Vector3(0,5,-10));
 
 camera.attachControl(canvas,true);
 
+// 创建网格
 const sphere = BABYLON.MeshBuilder.CreateSphere(
   'sphere',
   {diameter:2},
@@ -40,7 +42,42 @@ const light = new BABYLON.DirectionalLight(
   scene
 )
 
-const ground = BABYLON.MeshBuilder.CreateGround('ground',{width:6,height:6},scene)
+const ground = BABYLON.MeshBuilder.CreateGround(
+  'ground',
+  {width:6,height:6},
+  scene
+)
+
+ground.position.set(0,-1,0);
+
+// 创建平面
+// const plan = BABYLON.MeshBuilder.CreatePlane(
+//   'plan',
+//   {size:6},
+//   scene
+// )
+
+// 创建几何体
+// const box = BABYLON.MeshBuilder.CreateBox(
+//   'box',
+//   {size:2},
+//   scene
+// );
+
+// 创建圆锥体
+// const cone = BABYLON.MeshBuilder.CreateCylinder(
+//   'cone',
+//   {height:2,diameterTop:2,diameterBottom:2},
+//   scene
+// )
+
+const torus = BABYLON.MeshBuilder.CreateTorus(
+  'tours',
+  { diameter:3,thickness:1,tessellation:32},
+  scene
+)
+
+
 
 engine.runRenderLoop(() =>{
   scene.render();
