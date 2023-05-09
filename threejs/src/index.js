de2e5c99fb64d5cb0ee4da1960ad72888e6d0059
dezhizhang <1541609448@qq.com>
2023-05-10 05:25:28 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,94 +5,166 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2023-05-09 23:19:52
+ * :date last edited: 2023-05-10 05:23:25
  */
 import * as THREE from 'three';
-
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
-import {CSS3DRenderer, CSS3DObject} from 'three/examples/jsm/renderers/CSS3DRenderer.js'
-import * as dat from "dat.gui";
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+import { CSS3DRenderer, CSS3DSprite } from 'three/examples/jsm/renderers/CSS3DRenderer'
+import * as TWEEN from '@tweenjs/tween.js'
 
 let camera, scene, renderer;
-
-let scene2, renderer2;
-
 let controls;
+
+const particlesTotal = 512;
+const positions = [];
+const objects = [];
+let current = 0;
 
 init();
 animate();
 
 function init() {
 
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.set(200, 200, 200);
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 5000);
+  camera.position.set(600, 400, 1500);
+  camera.lookAt(0, 0, 0);
 
   scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xf0f0f0);
 
-  scene2 = new THREE.Scene();
+  const image = document.createElement('img');
+  image.addEventListener('load', function () {
 
-  const material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true, wireframeLinewidth: 1, side: THREE.DoubleSide });
+    for (let i = 0; i < particlesTotal; i++) {
 
-  //
+      const object = new CSS3DSprite(image.cloneNode());
+      object.position.x = Math.random() * 4000 - 2000,
+        object.position.y = Math.random() * 4000 - 2000,
+        object.position.z = Math.random() * 4000 - 2000;
+      scene.add(object);
 
-  for (let i = 0; i < 10; i++) {
+      objects.push(object);
 
-    const element = document.createElement('div');
-    element.style.width = '100px';
-    element.style.height = '100px';
-    element.style.opacity = (i < 5) ? 0.5 : 1;
-    element.style.background = new THREE.Color(Math.random() * 0xffffff).getStyle();
+    }
 
-    const object = new CSS3DObject(element);
-    object.position.x = Math.random() * 200 - 100;
-    object.position.y = Math.random() * 200 - 100;
-    object.position.z = Math.random() * 200 - 100;
-    object.rotation.x = Math.random();
-    object.rotation.y = Math.random();
-    object.rotation.z = Math.random();
-    object.scale.x = Math.random() + 0.5;
-    object.scale.y = Math.random() + 0.5;
-    scene2.add(object);
+    transition();
 
-    const geometry = new THREE.PlaneGeometry(100, 100);
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.copy(object.position);
-    mesh.rotation.copy(object.rotation);
-    mesh.scale.copy(object.scale);
-    scene.add(mesh);
+  });
+  image.src = 'https://threejs.org/examples/textures/sprite.png';
+
+  // Plane
+
+  const amountX = 16;
+  const amountZ = 32;
+  const separationPlane = 150;
+  const offsetX = ((amountX - 1) * separationPlane) / 2;
+  const offsetZ = ((amountZ - 1) * separationPlane) / 2;
+
+  for (let i = 0; i < particlesTotal; i++) {
+
+    const x = (i % amountX) * separationPlane;
+    const z = Math.floor(i / amountX) * separationPlane;
+    const y = (Math.sin(x * 0.5) + Math.sin(z * 0.5)) * 200;
+
+    positions.push(x - offsetX, y, z - offsetZ);
+
+  }
+
+  // Cube
+
+  const amount = 8;
+  const separationCube = 150;
+  const offset = ((amount - 1) * separationCube) / 2;
+
+  for (let i = 0; i < particlesTotal; i++) {
+
+    const x = (i % amount) * separationCube;
+    const y = Math.floor((i / amount) % amount) * separationCube;
+    const z = Math.floor(i / (amount * amount)) * separationCube;
+
+    positions.push(x - offset, y - offset, z - offset);
+
+  }
+
+  // Random
+
+  for (let i = 0; i < particlesTotal; i++) {
+
+    positions.push(
+      Math.random() * 4000 - 2000,
+      Math.random() * 4000 - 2000,
+      Math.random() * 4000 - 2000
+    );
+
+  }
+
+  // Sphere
+
+  const radius = 750;
+
+  for (let i = 0; i < particlesTotal; i++) {
+
+    const phi = Math.acos(- 1 + (2 * i) / particlesTotal);
+    const theta = Math.sqrt(particlesTotal * Math.PI) * phi;
+
+    positions.push(
+      radius * Math.cos(theta) * Math.sin(phi),
+      radius * Math.sin(theta) * Math.sin(phi),
+      radius * Math.cos(phi)
+    );
 
   }
 
   //
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer = new CSS3DRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  renderer2 = new CSS3DRenderer();
-  renderer2.setSize(window.innerWidth, window.innerHeight);
-  renderer2.domElement.style.position = 'absolute';
-  renderer2.domElement.style.top = 0;
-  document.body.appendChild(renderer2.domElement);
+  //
 
-  controls = new TrackballControls(camera, renderer2.domElement);
+  controls = new TrackballControls(camera, renderer.domElement);
+
+  //
 
   window.addEventListener('resize', onWindowResize);
-  // createPanel();
 
 }
 
 function onWindowResize() {
 
   camera.aspect = window.innerWidth / window.innerHeight;
-
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  renderer2.setSize(window.innerWidth, window.innerHeight);
+}
+
+function transition() {
+
+  const offset = current * particlesTotal * 3;
+  const duration = 2000;
+
+  for (let i = 0, j = offset; i < particlesTotal; i++, j += 3) {
+
+    const object = objects[i];
+
+    new TWEEN.Tween(object.position)
+      .to({
+        x: positions[j],
+        y: positions[j + 1],
+        z: positions[j + 2]
+      }, Math.random() * duration + duration)
+      .easing(TWEEN.Easing.Exponential.InOut)
+      .start();
+
+  }
+
+  new TWEEN.Tween(this)
+    .to({}, duration * 3)
+    .onComplete(transition)
+    .start();
+
+  current = (current + 1) % 4;
 
 }
 
@@ -100,73 +172,19 @@ function animate() {
 
   requestAnimationFrame(animate);
 
+  TWEEN.update();
   controls.update();
 
-  renderer.render(scene, camera);
-  renderer2.render(scene2, camera);
+  const time = performance.now();
 
-}
+  for (let i = 0, l = objects.length; i < l; i++) {
 
-// function createPanel() {
-
-//   const panel = new dat.GUI();
-//   const folder1 = panel.addFolder('camera setViewOffset').close();
-
-//   const settings = {
-//     'setViewOffset'() {
-
-//       folder1.children[1].enable().setValue(window.innerWidth);
-//       folder1.children[2].enable().setValue(window.innerHeight);
-//       folder1.children[3].enable().setValue(0);
-//       folder1.children[4].enable().setValue(0);
-//       folder1.children[5].enable().setValue(window.innerWidth);
-//       folder1.children[6].enable().setValue(window.innerHeight);
-
-//     },
-//     'fullWidth': 0,
-//     'fullHeight': 0,
-//     'offsetX': 0,
-//     'offsetY': 0,
-//     'width': 0,
-//     'height': 0,
-//     'clearViewOffset'() {
-
-//       folder1.children[1].setValue(0).disable();
-//       folder1.children[2].setValue(0).disable();
-//       folder1.children[3].setValue(0).disable();
-//       folder1.children[4].setValue(0).disable();
-//       folder1.children[5].setValue(0).disable();
-//       folder1.children[6].setValue(0).disable();
-//       camera.clearViewOffset();
-
-//     }
-//   };
-
-//   folder1.add(settings, 'setViewOffset');
-//   folder1.add(settings, 'fullWidth', window.screen.width / 4, window.screen.width * 2, 1).onChange((val) => updateCameraViewOffset({ fullWidth: val })).disable();
-//   folder1.add(settings, 'fullHeight', window.screen.height / 4, window.screen.height * 2, 1).onChange((val) => updateCameraViewOffset({ fullHeight: val })).disable();
-//   folder1.add(settings, 'offsetX', 0, 256, 1).onChange((val) => updateCameraViewOffset({ x: val })).disable();
-//   folder1.add(settings, 'offsetY', 0, 256, 1).onChange((val) => updateCameraViewOffset({ y: val })).disable();
-//   folder1.add(settings, 'width', window.screen.width / 4, window.screen.width * 2, 1).onChange((val) => updateCameraViewOffset({ width: val })).disable();
-//   folder1.add(settings, 'height', window.screen.height / 4, window.screen.height * 2, 1).onChange((val) => updateCameraViewOffset({ height: val })).disable();
-//   folder1.add(settings, 'clearViewOffset');
-
-// }
-
-function updateCameraViewOffset({ fullWidth, fullHeight, x, y, width, height }) {
-
-  if (!camera.view) {
-
-    camera.setViewOffset(fullWidth || window.innerWidth, fullHeight || window.innerHeight, x || 0, y || 0, width || window.innerWidth, height || window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-  } else {
-
-    camera.setViewOffset(fullWidth || camera.view.fullWidth, fullHeight || camera.view.fullHeight, x || camera.view.offsetX, y || camera.view.offsetY, width || camera.view.width, height || camera.view.height);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    const object = objects[i];
+    const scale = Math.sin((Math.floor(object.position.x) + time) * 0.002) * 0.3 + 1;
+    object.scale.set(scale, scale, scale);
 
   }
+
+  renderer.render(scene, camera);
 
 }
