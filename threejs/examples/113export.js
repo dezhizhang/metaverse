ce17aa2d5459c6a1da4bef0294f3e362e33f4075
dezhizhang <1541609448@qq.com>
2023-05-10 23:42:33 +0800

    feat:模型导出

--- threejs/examples/113export.js
+++ threejs/examples/113export.js
@@ -1,5 +1,5 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /threejs/src/index.js
  * :author: 张德志
  * :copyright: (c) 2023, Tungee
@@ -7,31 +7,33 @@
  * :last editor: 张德志
  * :date last edited: 2023-05-10 23:40:41
  */
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 let scene, camera, renderer, mesh;
-
 
 init();
 animate();
 
 function init() {
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight,1,1000);
-  camera.position.set(200,100,200);
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.set(200, 100, 200);
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xa0a0a0);
-  scene.fog = new THREE.Fog(0xa0a0a0,200,1000);
+  scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
 
-
-
-  const hemiLight = new THREE.HemisphereLight(0xffffff,0x8d8d8d);
-  hemiLight.position.set(0,200,0);
+  const hemiLight = new THREE.HemisphereLight(0xffffff, 0x8d8d8d);
+  hemiLight.position.set(0, 200, 0);
   scene.add(hemiLight);
 
   const directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.position.set(0,200,100);
+  directionalLight.position.set(0, 200, 100);
   directionalLight.castShadow = true;
   directionalLight.shadow.camera.top = 180;
   directionalLight.shadow.camera.bottom = -100;
@@ -40,69 +42,57 @@ function init() {
   scene.add(directionalLight);
 
   const ground = new THREE.Mesh(
-    new THREE.PlaneGeometry(2000,2000),
-    new THREE.MeshPhongMaterial({color:0xcbcbcb,depthWrite:false})
+    new THREE.PlaneGeometry(2000, 2000),
+    new THREE.MeshPhongMaterial({ color: 0xcbcbcb, depthWrite: false })
   );
   ground.rotation.x = -Math.PI / 2;
   ground.receiveShadow = true;
   scene.add(ground);
 
-  const grid = new THREE.GridHelper(2000,20,0x000000,0x000000);
+  const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
   grid.material.opacity = 0.2;
   grid.material.transparent = true;
   scene.add(grid);
 
   //export mesh
-  const geometry = new THREE.BoxGeometry(50,50,50);
-  const material = new THREE.MeshPhongMaterial({vertexColors:true});
+  const geometry = new THREE.BoxGeometry(50, 50, 50);
+  const material = new THREE.MeshPhongMaterial({ vertexColors: true });
 
-  const colors = geometry.getAttribute('position').array.slice();
+  const colors = geometry.getAttribute("position").array.slice();
   for (let i = 0, l = colors.length; i < l; i++) {
-    if(colors[i] > 0) {
+    if (colors[i] > 0) {
       colors[i] = 0.5;
-    }else {
+    } else {
       colors[i] = 0;
     }
   }
-  geometry.setAttribute('color',new THREE.BufferAttribute(colors,3,false));
-  mesh = new THREE.Mesh(geometry,material);
+  geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3, false));
+  mesh = new THREE.Mesh(geometry, material);
   mesh.castShadow = true;
   mesh.position.y = 25;
   scene.add(mesh);
 
-
-  renderer = new THREE.WebGLRenderer({antialias:true});
+  renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.shadowMap.enabled = true;
   document.body.appendChild(renderer.domElement);
-  
-  const controls = new OrbitControls(camera,renderer.domElement);
-  controls.target.set(0,25,0);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 25, 0);
   controls.update();
 
-  
-  window.addEventListener('resize',onWindowResize);
-  
-
-
+  window.addEventListener("resize", onWindowResize);
 }
 
-
-
 function onWindowResize() {
-
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
-
 }
 
 function animate() {
-
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-
 }
-
