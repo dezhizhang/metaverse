a90cc41c5ad5be8026a2fdda6efaa2b86de741d1
dezhizhang <1541609448@qq.com>
2023-05-10 13:43:49 +0800

    feat:update

---
+++ threejs/examples/112cameras.js
@@ -0,0 +1,117 @@
+/*
+ * :file description: 
+ * :name: /threejs/examples/112cameras.js
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-03-13 05:58:33
+ * :last editor: 张德志
+ * :date last edited: 2023-05-10 12:42:39
+ */
+import * as THREE from "three";
+
+let camera, scene, renderer;
+let mesh;
+const AMOUNT = 6;
+
+init();
+animate();
+
+function init() {
+  const ASPECT_RATIO = window.innerWidth / window.innerHeight;
+  const WIDTH = (window.innerWidth / AMOUNT) * window.devicePixelRatio;
+  const HEIGHT = (window.innerHeight / AMOUNT) * window.devicePixelRatio;
+
+  const cameras = [];
+  for(let y = 0;y < AMOUNT;y++) {
+    for(let x = 0; x < AMOUNT;x++) {
+      const subcamera = new THREE.PerspectiveCamera(40,ASPECT_RATIO,0.1,10);
+      subcamera.viewport = new THREE.Vector4(
+        Math.floor(x * WIDTH),
+        Math.floor(y * HEIGHT),
+        Math.ceil(WIDTH),
+        Math.ceil(HEIGHT)
+      )
+      subcamera.position.x = x / AMOUNT - 0.5;
+      subcamera.position.y = 0.5 - y / AMOUNT;
+      subcamera.position.z = 1.5;
+      subcamera.position.multiplyScalar(2);
+      subcamera.lookAt(0,0,0);
+      subcamera.updateMatrixWorld();
+      cameras.push(subcamera);
+
+    }
+  }
+
+  camera = new THREE.ArrayCamera(cameras);
+  camera.position.z = 3;
+
+  scene = new THREE.Scene();
+  scene.add(new THREE.AmbientLight(0x222244));
+
+  const light = new THREE.DirectionalLight();
+  light.position.set(0.5,0.5,1);
+  light.castShadow = true;
+  light.shadow.camera.zoom = 4;
+  scene.add(light);
+
+  const geometryBackground = new THREE.PlaneGeometry(100,100);
+  const materialBackground = new THREE.MeshPhongMaterial({color:0x000066});
+  const background = new THREE.Mesh(geometryBackground,materialBackground);
+  background.receiveShadow = true;
+  background.position.set(0,0,-1);
+  scene.add(background);
+
+  const geometryCylinder = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
+  const materialCylinder = new THREE.MeshPhongMaterial({color: 0xff0000 });
+
+  mesh = new THREE.Mesh(geometryCylinder,materialCylinder);
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  document.body.appendChild(renderer.domElement);
+
+
+  window.addEventListener('resize',onWindowResize)
+}
+
+function onWindowResize() {
+  const ASPECT_RATIO = window.innerWidth / window.innerHeight;
+  const WIDTH =  (window.innerWidth / AMOUNT) * window.devicePixelRatio;
+  const HEIGHT = (window.innerHeight / AMOUNT) * window.devicePixelRatio;
+  camera.aspect = ASPECT_RATIO;
+  camera.updateMatrixWorld();
+
+  for(let y =0;y < AMOUNT;y++) {
+    for(let x = 0; x < AMOUNT;x ++) {
+      const subcamera = camera.cameras[AMOUNT * y + x];
+
+      subcamera.viewport.set(
+        Math.floor(x * WIDTH),
+        Math.floor(y * HEIGHT),
+        Math.ceil(WIDTH),
+        Math.ceil(HEIGHT)
+      );
+
+      subcamera.aspect = ASPECT_RATIO;
+      subcamera.updateProjectionMatrix();
+    }
+  }
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+
+function animate() {
+  mesh.rotation.x += 0.005;
+  mesh.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
+}
--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,125 +1,172 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 
-let camera, scene, renderer;
-let mesh;
-const AMOUNT = 6;
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { PLYExporter } from 'three/examples/jsm/exporters/PLYExporter.js';
+import * as dat from 'dat.gui';
+
+let scene, camera, renderer, exporter, mesh;
+
+const params = {
+  exportASCII: exportASCII,
+  exportBinaryBigEndian: exportBinaryBigEndian,
+  exportBinaryLittleEndian: exportBinaryLittleEndian
+};
 
 init();
 animate();
 
 function init() {
-  const ASPECT_RATIO = window.innerWidth / window.innerHeight;
-  const WIDTH = (window.innerWidth / AMOUNT) * window.devicePixelRatio;
-  const HEIGHT = (window.innerHeight / AMOUNT) * window.devicePixelRatio;
 
-  const cameras = [];
-  for(let y = 0;y < AMOUNT;y++) {
-    for(let x = 0; x < AMOUNT;x++) {
-      const subcamera = new THREE.PerspectiveCamera(40,ASPECT_RATIO,0.1,10);
-      
-    }
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.set(200, 100, 200);
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xa0a0a0);
+  scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
+
+  exporter = new PLYExporter();
+
+  //
+
+  const hemiLight = new THREE.HemisphereLight(0xffffff, 0x8d8d8d);
+  hemiLight.position.set(0, 200, 0);
+  scene.add(hemiLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(0, 200, 100);
+  directionalLight.castShadow = true;
+  directionalLight.shadow.camera.top = 180;
+  directionalLight.shadow.camera.bottom = - 100;
+  directionalLight.shadow.camera.left = - 120;
+  directionalLight.shadow.camera.right = 120;
+  scene.add(directionalLight);
+
+  // ground
+
+  const ground = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0xcbcbcb, depthWrite: false }));
+  ground.rotation.x = - Math.PI / 2;
+  ground.receiveShadow = true;
+  scene.add(ground);
+
+  const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
+  grid.material.opacity = 0.2;
+  grid.material.transparent = true;
+  scene.add(grid);
+
+  // export mesh
+
+  const geometry = new THREE.BoxGeometry(50, 50, 50);
+  const material = new THREE.MeshPhongMaterial({ vertexColors: true });
+
+  // color vertices based on vertex positions
+  const colors = geometry.getAttribute('position').array.slice();
+  for (let i = 0, l = colors.length; i < l; i++) {
+
+    if (colors[i] > 0) colors[i] = 0.5;
+    else colors[i] = 0;
+
   }
+
+  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3, false));
+
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.castShadow = true;
+  mesh.position.y = 25;
+  scene.add(mesh);
+
+  //
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  document.body.appendChild(renderer.domElement);
+
+  //
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 25, 0);
+  controls.update();
+
+  //
+
+  window.addEventListener('resize', onWindowResize);
+
+  const gui = new dat.GUI();
+
+  gui.add(params, 'exportASCII').name('Export PLY (ASCII)');
+  gui.add(params, 'exportBinaryBigEndian').name('Export PLY (Binary BE)');
+  gui.add(params, 'exportBinaryLittleEndian').name('Export PLY (Binary LE)');
+  gui.open();
+
 }
 
-// function init() {
-//   const ASPECT_RATIO = window.innerWidth / window.innerHeight;
-
-//   const WIDTH = (window.innerWidth / AMOUNT) * window.devicePixelRatio;
-//   const HEIGHT = (window.innerHeight / AMOUNT) * window.devicePixelRatio;
-
-//   const cameras = [];
-
-//   for (let y = 0; y < AMOUNT; y++) {
-//     for (let x = 0; x < AMOUNT; x++) {
-//       const subcamera = new THREE.PerspectiveCamera(40, ASPECT_RATIO, 0.1, 10);
-//       subcamera.viewport = new THREE.Vector4(
-//         Math.floor(x * WIDTH),
-//         Math.floor(y * HEIGHT),
-//         Math.ceil(WIDTH),
-//         Math.ceil(HEIGHT)
-//       );
-//       subcamera.position.x = x / AMOUNT - 0.5;
-//       subcamera.position.y = 0.5 - y / AMOUNT;
-//       subcamera.position.z = 1.5;
-//       subcamera.position.multiplyScalar(2);
-//       subcamera.lookAt(0, 0, 0);
-//       subcamera.updateMatrixWorld();
-//       cameras.push(subcamera);
-//     }
-//   }
-
-//   camera = new THREE.ArrayCamera(cameras);
-//   camera.position.z = 3;
-
-//   scene = new THREE.Scene();
-
-//   scene.add(new THREE.AmbientLight(0x222244));
-
-//   const light = new THREE.DirectionalLight();
-//   light.position.set(0.5, 0.5, 1);
-//   light.castShadow = true;
-//   light.shadow.camera.zoom = 4; // tighter shadow map
-//   scene.add(light);
-
-//   const geometryBackground = new THREE.PlaneGeometry(100, 100);
-//   const materialBackground = new THREE.MeshPhongMaterial({ color: 0x000066 });
-
-//   const background = new THREE.Mesh(geometryBackground, materialBackground);
-//   background.receiveShadow = true;
-//   background.position.set(0, 0, -1);
-//   scene.add(background);
-
-//   const geometryCylinder = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
-//   const materialCylinder = new THREE.MeshPhongMaterial({ color: 0xff0000 });
-
-//   mesh = new THREE.Mesh(geometryCylinder, materialCylinder);
-//   mesh.castShadow = true;
-//   mesh.receiveShadow = true;
-//   scene.add(mesh);
-
-//   renderer = new THREE.WebGLRenderer();
-//   renderer.setPixelRatio(window.devicePixelRatio);
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-//   renderer.shadowMap.enabled = true;
-//   document.body.appendChild(renderer.domElement);
-
-//   //
-
-//   window.addEventListener("resize", onWindowResize);
-// }
-
 function onWindowResize() {
-  const ASPECT_RATIO = window.innerWidth / window.innerHeight;
-  const WIDTH = (window.innerWidth / AMOUNT) * window.devicePixelRatio;
-  const HEIGHT = (window.innerHeight / AMOUNT) * window.devicePixelRatio;
 
-  camera.aspect = ASPECT_RATIO;
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  for (let y = 0; y < AMOUNT; y++) {
-    for (let x = 0; x < AMOUNT; x++) {
-      const subcamera = camera.cameras[AMOUNT * y + x];
-
-      subcamera.viewport.set(
-        Math.floor(x * WIDTH),
-        Math.floor(y * HEIGHT),
-        Math.ceil(WIDTH),
-        Math.ceil(HEIGHT)
-      );
-
-      subcamera.aspect = ASPECT_RATIO;
-      subcamera.updateProjectionMatrix();
-    }
-  }
-
   renderer.setSize(window.innerWidth, window.innerHeight);
+
 }
 
 function animate() {
-  mesh.rotation.x += 0.005;
-  mesh.rotation.z += 0.01;
-
-  renderer.render(scene, camera);
 
   requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+
+}
+
+function exportASCII() {
+
+  exporter.parse(mesh, function (result) {
+
+    saveString(result, 'box.ply');
+
+  });
+
+}
+
+function exportBinaryBigEndian() {
+
+  exporter.parse(mesh, function (result) {
+
+    saveArrayBuffer(result, 'box.ply');
+
+  }, { binary: true });
+
+}
+
+function exportBinaryLittleEndian() {
+
+  exporter.parse(mesh, function (result) {
+
+    saveArrayBuffer(result, 'box.ply');
+
+  }, { binary: true, littleEndian: true });
+
+}
+
+const link = document.createElement('a');
+link.style.display = 'none';
+document.body.appendChild(link);
+
+function save(blob, filename) {
+
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
+
+}
+
+function saveString(text, filename) {
+
+  save(new Blob([text], { type: 'text/plain' }), filename);
+
+}
+
+function saveArrayBuffer(buffer, filename) {
+
+  save(new Blob([buffer], { type: 'application/octet-stream' }), filename);
+
 }
