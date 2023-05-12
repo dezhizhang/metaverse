1dff6e77eb45197486004db8304a197ccdd9fd79
dezhizhang <1541609448@qq.com>
2023-05-12 23:23:24 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,128 +1,109 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-			import { AnaglyphEffect } from 'three/addons/effects/AnaglyphEffect.js';
+import Stats from "stats.js";
 
-			let container, camera, scene, renderer, effect;
+let stats;
 
-			const spheres = [];
+let camera, scene, renderer;
 
-			let mouseX = 0;
-			let mouseY = 0;
+let points;
 
-			let windowHalfX = window.innerWidth / 2;
-			let windowHalfY = window.innerHeight / 2;
+init();
+animate();
 
-			document.addEventListener( 'mousemove', onDocumentMouseMove );
+function init() {
+	camera = new THREE.PerspectiveCamera(27,window.innerWidth / window.innerHeight,5,3500);
+	camera.position.z = 2750;
 
-			init();
-			animate();
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0x050505);
+	scene.fog = new THREE.Fog(0x050505,2000,3500);
 
-			function init() {
+	//
+	const particles = 500000;
+	const geometry = new THREE.BufferGeometry();
+	const arrayBuffer = new ArrayBuffer(particles * 16);
+	const interleavedFloat32Buffer = new Float32Array(arrayBuffer);
+	const interleavedUint8Buffer = new Uint8Array(arrayBuffer);
 
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
+	const color = new THREE.Color();
+	const n = 1000;
+	const n2 = n / 2;
 
-				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 100 );
-				camera.position.z = 3;
-				camera.focalLength = 3;
+	for(let i=0;i < interleavedFloat32Buffer.length;i+=4) {
+		const x = Math.random() * n - n2;
+		const y = Math.random() * n - n2;
+		const z = Math.random() * n - n2;
 
-				const path = 'textures/cube/pisa/';
-				const format = '.png';
-				const urls = [
-					path + 'px' + format, path + 'nx' + format,
-					path + 'py' + format, path + 'ny' + format,
-					path + 'pz' + format, path + 'nz' + format
-				];
+		interleavedFloat32Buffer[i + 0] = x;
+		interleavedFloat32Buffer[i + 1] = y;
+		interleavedFloat32Buffer[i + 2] = z;
 
-				const textureCube = new THREE.CubeTextureLoader().load( urls );
+		const vx = x / n + 0.5;
+		const vy = y / n + 0.5;
+		const vz = z / n + 0.5;
 
-				scene = new THREE.Scene();
-				scene.background = textureCube;
+		color.setRGB(vx,vy,vz,THREE.SRGBColorSpace);
+		const j = (i + 3) * 4;
+		
+		interleavedUint8Buffer[j + 0] = color.r * 255;
+		interleavedUint8Buffer[j + 1] = color.g * 255;
+		interleavedUint8Buffer[j + 2] = color.b * 255;
+		interleavedUint8Buffer[j + 3] = 0;
 
-				const geometry = new THREE.SphereGeometry( 0.1, 32, 16 );
-				const material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube } );
+	}
 
-				for ( let i = 0; i < 500; i ++ ) {
+	const interleavedBuffer32 = new THREE.InterleavedBuffer(
+		interleavedFloat32Buffer,
+		4
+	);
 
-					const mesh = new THREE.Mesh( geometry, material );
+	const interleavedBuffer8 = new THREE.InterleavedBuffer(
+		interleavedUint8Buffer,
+		16
+	);
 
-					mesh.position.x = Math.random() * 10 - 5;
-					mesh.position.y = Math.random() * 10 - 5;
-					mesh.position.z = Math.random() * 10 - 5;
+	geometry.setAttribute('position',new THREE.InterleavedBufferAttribute(interleavedBuffer32,3,0,false));
+	geometry.setAttribute('color',new THREE.InterleavedBufferAttribute(interleavedBuffer8,3,12,true));
 
-					mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
 
-					scene.add( mesh );
+	const material = new THREE.PointsMaterial({size:15,vertexColors:true});
+	points = new THREE.Points(geometry,material);
+	scene.add(points);
 
-					spheres.push( mesh );
 
-				}
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth,window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
-				//
+	stats = new Stats();
+	document.body.appendChild(stats.dom);
 
-				renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				container.appendChild( renderer.domElement );
+	window.addEventListener('resize',onWindowResize);
 
-				const width = window.innerWidth || 2;
-				const height = window.innerHeight || 2;
+}
 
-				effect = new AnaglyphEffect( renderer );
-				effect.setSize( width, height );
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 
-				//
+	renderer.setSize(window.innerWidth,window.innerHeight);
+}
 
-				window.addEventListener( 'resize', onWindowResize );
 
-			}
+function animate() {
+  requestAnimationFrame(animate);
 
-			function onWindowResize() {
+  render();
+  stats.update();
+}
 
-				windowHalfX = window.innerWidth / 2;
-				windowHalfY = window.innerHeight / 2;
+function render() {
+  const time = Date.now() * 0.001;
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+  points.rotation.x = time * 0.25;
+  points.rotation.y = time * 0.5;
 
-				effect.setSize( window.innerWidth, window.innerHeight );
-
-			}
-
-			function onDocumentMouseMove( event ) {
-
-				mouseX = ( event.clientX - windowHalfX ) / 100;
-				mouseY = ( event.clientY - windowHalfY ) / 100;
-
-			}
-
-			//
-
-			function animate() {
-
-				requestAnimationFrame( animate );
-
-				render();
-
-			}
-
-			function render() {
-
-				const timer = 0.0001 * Date.now();
-
-				camera.position.x += ( mouseX - camera.position.x ) * .05;
-				camera.position.y += ( - mouseY - camera.position.y ) * .05;
-
-				camera.lookAt( scene.position );
-
-				for ( let i = 0, il = spheres.length; i < il; i ++ ) {
-
-					const sphere = spheres[ i ];
-
-					sphere.position.x = 5 * Math.cos( timer + i );
-					sphere.position.y = 5 * Math.sin( timer + i * 1.1 );
-
-				}
-
-				effect.render( scene, camera );
-
-			}
+  renderer.render(scene, camera);
+}
