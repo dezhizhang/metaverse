d38fe892490c3f1721a7926d8e172535b93ab83e
dezhizhang <1541609448@qq.com>
2023-05-13 21:27:39 +0800

    feat:update

--- 3dmax/src/index.js
+++ 3dmax/src/index.js
@@ -5,12 +5,20 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-04-12 07:13:44
  * :last editor: 张德志
- * :date last edited: 2023-05-13 16:58:25
+ * :date last edited: 2023-05-13 21:25:31
  */
 
 
-const x = 5;
-const y = -4;
-const z = 7;
+// const x = 5;
+// const y = -4;
+// const z = 7;
 
-console.log(Math.sqrt(x * x + y * y + z * z))
+// console.log(Math.sqrt(x * x + y * y + z * z))
+
+const vect3 = [-5,0,0.4];
+
+const newVect3 = vect3.map((item) => {
+    return Number((-3 * item).toFixed(1))
+});
+
+console.log(newVect3);
