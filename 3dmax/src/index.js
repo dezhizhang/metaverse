13b22137e18d69469b398c019bc03849f7caa196
dezhizhang <1541609448@qq.com>
2023-05-13 17:00:26 +0800

    feat:向量

--- 3dmax/src/index.js
+++ 3dmax/src/index.js
@@ -1,11 +1,16 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /3dmax/src/index.js
  * :author: 张德志
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-04-12 07:13:44
  * :last editor: 张德志
- * :date last edited: 2023-04-12 07:16:12
+ * :date last edited: 2023-05-13 16:58:25
  */
 
-console.log('hello')
+
+const x = 5;
+const y = -4;
+const z = 7;
+
+console.log(Math.sqrt(x * x + y * y + z * z))
