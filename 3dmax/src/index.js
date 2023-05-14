8b0c1aca33d67c098b4afb61f0db87eea5563025
dezhizhang <1541609448@qq.com>
2023-05-14 19:34:35 +0800

    feat:update

--- 3dmax/src/index.js
+++ 3dmax/src/index.js
@@ -5,34 +5,17 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-04-12 07:13:44
  * :last editor: 张德志
- * :date last edited: 2023-05-13 22:18:57
+ * :date last edited: 2023-05-14 19:14:32
  */
 
+const arr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [10,11,12]
+]
 
-const objA = {
-    x1: 1,
-    y1: 3,
-    z1: 4
-}
-
-const objB = {
-    x2: 2,
-    y2: -5,
-    z2: 8
-}
-
-function cross(objA, objB) {
-    return [
-        objA.y1 * objB.z2 - objA.z1 * objB.y2,
-        objA.z1 * objB.x2 - objA.x1 * objB.z2,
-        objA.x1 * objB.y2 - objA.y1 * objB.x2
-
-    ]
-}
-
-const res = cross(objA,objB);
-console.log({res});
-
+console.log(arr);
 
 
 
