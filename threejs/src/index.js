a195bfa5de0e7636820fccb6252e0263af0fef53
dezhizhang <1541609448@qq.com>
2023-05-17 23:15:54 +0800

    feat:更新

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -9,10 +9,21 @@ import * as THREE from 'three';
 
 // console.log(d)
 
-const a = new THREE.Vector3(1,1,1);
+// const a = new THREE.Vector3(1,1,1);
 
-const b = new THREE.Vector3();
+// const b = new THREE.Vector3();
 
-const d = a.distanceTo(b);
+// const d = a.distanceTo(b);
 
-console.log(d)
+// console.log(d)
+
+// const geometry = new THREE.SphereGeometry(50,25,25);
+// console.log(geometry)
+
+
+const p1 = new THREE.Vector3(10,8,12);
+const p2 = new THREE.Vector3(30,30,-10);
+
+const l = p1.clone().sub(p2).length();
+
+console.log('两点间的距离',l);
