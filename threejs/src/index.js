58dc5c971c18f1287d3f5cad4f312466c4a6a9de
dezhizhang <1541609448@qq.com>
2023-05-19 05:47:12 +0800

    feat;update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2023-05-19 05:44:10
+ * :date last edited: 2023-05-19 05:46:55
  */
 import * as THREE from 'three';
 
@@ -26,6 +26,12 @@ import * as THREE from 'three';
 // let v2 = v1.clone().applyMatrix4(mate);
 
 // console.log(v2);
+
+// const mesh = new THREE.Mesh();
+
+// mesh.rotateZ(Math.PI);
+
+// console.log(mesh.rotation)
 
 
 
