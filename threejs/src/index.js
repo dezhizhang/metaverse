ce275978211f4f0b3eb8c1ecde1900c911c56c65
dezhizhang <1541609448@qq.com>
2023-05-19 05:44:27 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,9 +5,27 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2023-05-19 05:13:12
+ * :date last edited: 2023-05-19 05:44:10
  */
 import * as THREE from 'three';
 
-const mate = new THREE.Matrix4();
-console.log(mate.elements);
+// const mate = new THREE.Matrix4();
+// console.log(mate.elements);
+
+
+// const mate = new THREE.Matrix4();
+// mate.set(1,0,0,5,0,1,0,3,0,0,1,9,0,0,0,1);
+
+// mate.multiplyScalar(10);
+// console.log(mate.elements);
+
+// const mate = new THREE.Matrix4();
+// mate.makeTranslation(5,3,9);
+// let v1 = new THREE.Vector3(10,10,10);
+
+// let v2 = v1.clone().applyMatrix4(mate);
+
+// console.log(v2);
+
+
+
