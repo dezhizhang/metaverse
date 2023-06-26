b3d47f7af0555afe848ff2f043ca51db55ac60f4
dezhizhang <1541609448@qq.com>
2023-06-27 06:15:34 +0800

    feat:gup配置

--- webgpu/examples/2.配置webgpu配置.js
+++ webgpu/examples/2.配置webgpu配置.js
@@ -1,14 +1,12 @@
 /*
  * :file description: 
- * :name: /webgpu/src/index.js
+ * :name: /webgpu/examples/2.配置webgpu配置.js
  * :author: 张德志
  * :copyright: (c) 2023, Tungee
- * :date created: 2023-06-27 05:54:49
+ * :date created: 2023-06-27 06:14:56
  * :last editor: 张德志
- * :date last edited: 2023-06-27 06:15:05
+ * :date last edited: 2023-06-27 06:15:12
  */
-
-
 const canvas = document.createElement('canvas');
 canvas.width = 500;
 canvas.height = 500;
@@ -34,9 +32,3 @@ async function run() {
 run();
 
 document.body.appendChild(canvas);
-
-
-
-
-
-
