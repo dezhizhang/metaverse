fa7a281d494eafdf6c7603e8f615f166cd822cbb
dezhizhang <1541609448@qq.com>
2023-07-02 20:15:47 +0800

    feat:village

---
+++ babylon/src/Village.ts
@@ -0,0 +1,58 @@
+/*
+ * :file description: 
+ * :name: /babylon/src/Village.ts
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-07-02 20:10:04
+ * :last editor: 张德志
+ * :date last edited: 2023-07-02 20:15:29
+ */
+
+
+/*
+ * :file description: 
+ * :name: /babylon/src/BasicScene.ts
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-07-02 17:49:35
+ * :last editor: 张德志
+ * :date last edited: 2023-07-02 20:07:25
+ */
+
+import { ArcRotateCamera, Engine, HemisphericLight, MeshBuilder, Scene, Vector3 } from "babylonjs";
+
+
+export default class Village {
+    engine: Engine;
+    scene: Scene;
+    constructor(private readonly canvas: HTMLCanvasElement) {
+        this.engine = new Engine(this.canvas);
+        this.scene = this.CreateScene();
+
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
+        })
+    }
+
+    // 创建场景
+    CreateScene(): Scene {
+        const scene = new Scene(this.engine);
+
+        const camera = new ArcRotateCamera('camera', - Math.PI / 2, Math.PI / 2.5, 10, new Vector3(0, 0, 0));
+        camera.attachControl(this.canvas, true);
+
+        // 创建灯光
+        const light = new HemisphericLight('light', new Vector3(1, 1, 0), this.scene);
+
+        const box = MeshBuilder.CreateBox('box');
+        box.position.y = 0.5;
+
+        // 创建平面
+        const ground = MeshBuilder.CreateGround('ground', { width: 10, height: 10 });
+        
+        
+        return scene;
+    }
+
+
+}
--- babylon/src/main.ts
+++ babylon/src/main.ts
@@ -5,16 +5,16 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-05-04 05:38:45
  * :last editor: 张德志
- * :date last edited: 2023-07-02 19:45:46
+ * :date last edited: 2023-07-02 20:10:52
  */
-import BasicScene from './BasicScene';
+import Village from './Village';
 import './style.css'
 
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-new BasicScene(canvas);
+new Village(canvas);
 
 
 document.body.appendChild(canvas);
