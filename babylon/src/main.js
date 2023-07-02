aad089c32359e1462650ffde4f8eff0d631c877b
dezhizhang <1541609448@qq.com>
2023-07-02 17:07:24 +0800

    feat:设置物体

--- babylon/src/main.js
+++ babylon/src/main.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-05-04 05:38:45
  * :last editor: 张德志
- * :date last edited: 2023-07-02 16:52:34
+ * :date last edited: 2023-07-02 17:06:48
  */
 import './style.css'
 
@@ -29,12 +29,12 @@ camera.setPosition(new BABYLON.Vector3(0,5,-10));
 
 camera.attachControl(canvas,true);
 
-// 创建网格
-const sphere = BABYLON.MeshBuilder.CreateSphere(
-  'sphere',
-  {diameter:2},
-  scene
-);
+// // 创建网格
+// const sphere = BABYLON.MeshBuilder.CreateSphere(
+//   'sphere',
+//   {diameter:2},
+//   scene
+// );
 
 // 创建灯光
 const light = new BABYLON.DirectionalLight(
@@ -42,6 +42,53 @@ const light = new BABYLON.DirectionalLight(
   new BABYLON.Vector3(0,-1,0),
   scene
 );
+
+// 创建地面
+// const ground = new BABYLON.MeshBuilder.CreateGround(
+//   'ground',
+//   {width:6,height:6},
+//   scene
+// )
+
+// const plan = new BABYLON.MeshBuilder.CreatePlane('plan',{size:6},scene)
+
+// const box = BABYLON.MeshBuilder.CreateBox(
+//   'box',
+//   {size:2},
+//   scene
+// )
+
+// 圆柱体
+// const cylinder = BABYLON.MeshBuilder.CreateCylinder(
+//   'cylinder',
+//   {height:2,diameter:2},
+//   scene
+// );
+
+// const cone = BABYLON.MeshBuilder.CreateCylinder(
+//   'cone',
+//   {
+//     height:2,
+//     diameterTop:0.25,
+//     diameterBottom:3,
+//     tessellation:3
+//   },
+//   scene
+// );
+
+const torus = BABYLON.MeshBuilder.CreateTorus(
+  'torus',
+  {
+    diameter:3,
+    thickness:1,
+    tessellation:32
+  },
+  scene
+);
+
+
+
+
 
 
 engine.runRenderLoop(() =>{
