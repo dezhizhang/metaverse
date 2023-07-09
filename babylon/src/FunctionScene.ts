70a765d76abc8b62e673ea2b7f10c200c64ec579
dezhizhang <1541609448@qq.com>
2023-07-09 15:08:38 +0800

    feat:粒子系统

--- babylon/src/FunctionScene.ts
+++ babylon/src/FunctionScene.ts
@@ -7,10 +7,10 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-07-02 17:49:35
  * :last editor: 张德志
- * :date last edited: 2023-07-09 14:57:52
+ * :date last edited: 2023-07-09 15:07:18
  */
 
-import { Engine, HemisphericLight, Scene,MeshBuilder,Vector3 } from "babylonjs";
+import { Engine, HemisphericLight, Scene,MeshBuilder,Vector3,ArcRotateCamera } from "babylonjs";
 
 
 export default class FunctionScene {
@@ -18,7 +18,7 @@ export default class FunctionScene {
     scene:Scene;
     constructor(private readonly canvas:HTMLCanvasElement) {
         this.engine = new Engine(this.canvas);
-        this.scene = this.CreateScene();
+        this.scene = this.createScene();
 
         this.engine.runRenderLoop(() => {
             this.scene.render();
@@ -26,29 +26,29 @@ export default class FunctionScene {
     }
 
     // 创建场景
-    CreateScene():Scene {
+    createScene():Scene {
         const scene = new Scene(this.engine);
+        const camera = new ArcRotateCamera('camera',3 * Math.PI / 2,Math.PI / 2.5,70,Vector3.Zero(),this.scene);
+        camera.attachControl(this.scene,true);
 
-        const camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 2.5, 70, BABYLON.Vector3.Zero());
-        camera.attachControl(this.canvas, true);
-    
-        const light = new HemisphericLight("hemi", new Vector3(0, 1, 0),this.scene);
-    
+        const light = new HemisphericLight('hemi',new Vector3(0,1,0),this.scene);
+
         const fountainProfile:any[] = [
-            new BABYLON.Vector3(0, 0, 0),
-            new BABYLON.Vector3(10, 0, 0),
-            new BABYLON.Vector3(10, 4, 0),
-            new BABYLON.Vector3(8, 4, 0),
-            new BABYLON.Vector3(8, 1, 0),
-            new BABYLON.Vector3(1, 2, 0),
-            new BABYLON.Vector3(1, 15, 0),
-            new BABYLON.Vector3(3, 17, 0)
+            new Vector3(0, 0, 0),
+            new Vector3(10, 0, 0),
+            new Vector3(10, 4, 0),
+            new Vector3(8, 4, 0),
+            new Vector3(8, 1, 0),
+            new Vector3(1, 2, 0),
+            new Vector3(1, 15, 0),
+            new Vector3(3, 17, 0)
         ];
-        
-        //Create lathe
-        const fountain = MeshBuilder.CreateLathe("fountain", {shape: fountainProfile, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, this.scene);
-        
+
+        const fountain = MeshBuilder.CreateLathe('fountain',{shape: fountainProfile, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, this.scene);
+
         return scene;
+
     }
+   
 
 }
