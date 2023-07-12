5b7e9293b6ae24b03ec0570c82e45a237e2b4bc2
dezhizhang <1541609448@qq.com>
2023-07-13 06:45:17 +0800

    feat:update

--- webgpu/src/main.ts
+++ webgpu/src/main.ts
@@ -1,8 +1,35 @@
-import './style.css'
 
-const canvas = document.getElementById('canvas');
-const adapter = await navigator.gpu.requesterAdapter();
-const device = await adapter.requesterAdapter();
 
-console.log(device);
 
+
+// async function init() {
+//  const adapter = await navigator.gpu.requestAdapter();
+//  const device = await adapter?.requestDevice();
+
+// //  console.log(device);
+
+// }
+
+// init();
+
+async function initGPU() {
+  if(!navigator.gpu) throw('浏览器不支持')
+
+  const  adapter= await navigator.gpu.requestAdapter();
+  const device = await adapter?.requestDevice();
+  const canvas = document.querySelector('canvas');
+
+  const context = canvas?.getContext('webgpu');
+  // const format = (context as any)?.getPrefeeredFormat(adapter);
+
+  // const context = canvas?.getContext('webgpu')
+
+  context?.configure({
+    device:device as GPUDevice,
+    format:'bgra8unorm',
+  })
+
+  // console.log(format)
+}
+
+initGPU()
