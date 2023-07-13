7f07f042c3ab95020ceba332cca07c5236bb4608
dezhizhang <1541609448@qq.com>
2023-07-13 08:01:58 +0800

    feat:update

--- webgpu/src/main.ts
+++ webgpu/src/main.ts
@@ -1,35 +1,76 @@
 
 
 
-
-// async function init() {
-//  const adapter = await navigator.gpu.requestAdapter();
-//  const device = await adapter?.requestDevice();
-
-// //  console.log(device);
-
-// }
-
-// init();
-
 async function initGPU() {
   if(!navigator.gpu) throw('浏览器不支持')
 
   const  adapter= await navigator.gpu.requestAdapter();
   const device = await adapter?.requestDevice();
+
+  console.log(adapter,device)
   const canvas = document.querySelector('canvas');
 
   const context = canvas?.getContext('webgpu');
-  // const format = (context as any)?.getPrefeeredFormat(adapter);
-
-  // const context = canvas?.getContext('webgpu')
 
   context?.configure({
     device:device as GPUDevice,
     format:'bgra8unorm',
+  
   })
 
-  // console.log(format)
+  return {adapter,device,context}
+}
+
+async function initPipeline(device: GPUDevice) {
+  const vertexShader = device.createShaderModule({
+    code:`
+    @vertex
+    fn main(@builtin(vertex_index) VertexIndex : u32) -> @builtin(position) vec4<f32> {
+        var pos = array<vec2<f32>, 3>(
+          vec2<f32>(0.0, 0.5),
+          vec2<f32>(-0.5, -0.5),
+          vec2<f32>(0.5, -0.5)
+        );
+        return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+    }
+    `
+  });
+  const fregSharder = device.createShaderModule({
+    code:`
+    @fragment
+    fn main() -> @location(0) vec4<f32> {
+        return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+    }
+    `
+  });
+
+  const pipeline = await device.createRenderPipelineAsync({
+    vertex: {
+      module: vertexShader,
+      entryPoint: 'main'
+    },
+    fragment: {
+      module: fregSharder,
+      entryPoint: 'main',
+      targets: [
+        {
+          format: 'bgra8unorm',
+        }
+      ],
+    },
+    primitive: {
+      topology: 'triangle-list'
+    },
+    layout: 'auto'
+  })
+  return {pipeline}
+}
+
+
+
+async function run() {
+  const {device} = await initGPU();
+  const {pipeline} = await initPipeline(device as GPUDevice)
 }
 
 initGPU()
---
+++ webgpu/vite.config.js
@@ -0,0 +1,14 @@
+/*
+ * :file description: 
+ * :name: /webgpu/vite.config.js
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-07-13 07:00:23
+ * :last editor: 张德志
+ * :date last edited: 2023-07-13 07:00:24
+ */
+export default {
+    server:{
+        host:'0.0.0.0'
+    }
+}
