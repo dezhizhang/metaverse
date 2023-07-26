129b0c917ca7f7223604fbc2c36d81a10f06ebf6
dezhizhang <1541609448@qq.com>
2023-07-27 06:09:54 +0800

    feat:材质按钮

---
+++ babylon/src/TextureMode.ts
@@ -0,0 +1,82 @@
+/*
+ * :file description: 
+ * :name: /babylon/src/TextureMode.ts
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-07-27 06:03:41
+ * :last editor: 张德志
+ * :date last edited: 2023-07-27 06:08:54
+ */
+/*
+ * :file description: 
+ * :name: /babylon/src/FullsceenMode.ts
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-07-02 17:49:35
+ * :last editor: 张德志
+ * :date last edited: 2023-07-26 23:21:50
+ */
+
+import {  Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene,SceneLoader,Vector3 } from "babylonjs";
+import * as GUI from 'babylonjs-gui';
+
+
+export default class TextureMode {
+    engine:Engine;
+    scene:Scene;
+    constructor(private readonly canvas:HTMLCanvasElement) {
+        this.engine = new Engine(this.canvas);
+        this.scene = this.createScene();
+
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
+        }) 
+    }
+
+    // 创建场景
+    createScene():Scene {
+        const scene = new Scene(this.engine);
+
+        const camera = new FreeCamera('camera',new Vector3(0,5,-10));
+        camera.setTarget(Vector3.Zero());
+        camera.attachControl(this.canvas,true);
+
+        const light = new HemisphericLight('light',new Vector3(0,1,0),scene);
+        light.intensity = 0.7;
+
+        // 网格 
+        const sphere = MeshBuilder.CreateSphere('sphere',{diameter:2,segments:16});
+        sphere.position.y = 1;
+
+        // 网格
+        const ground = MeshBuilder.CreateGround('ground',{width:6,height:6,subdivisions:2});
+
+        const plane = MeshBuilder.CreatePlane('plan',{size:2});
+        plane.parent = sphere;
+        plane.position.y = 2;
+
+        const adTexture = GUI.AdvancedDynamicTexture.CreateForMesh(plane,128,128,false);
+        adTexture.renderScale = 1;
+
+
+        const button = GUI.Button.CreateSimpleButton('button','click me');
+
+        button.width =  1;
+        button.height = 0.4;
+        button.color = 'white';
+        button.background = 'green';
+        button.cornerRadius = 8;
+        button.onPointerClickObservable.add(() => {
+            alert('you did it');
+        });
+
+        adTexture.addControl(button);
+
+        window.addEventListener('resize',() => {
+            this.engine.resize();
+        })
+        return scene;
+        
+    }
+    
+}
--- babylon/src/main.ts
+++ babylon/src/main.ts
@@ -5,16 +5,16 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-05-04 05:38:45
  * :last editor: 张德志
- * :date last edited: 2023-07-26 23:06:10
+ * :date last edited: 2023-07-27 06:04:20
  */
-import FullsceenMode from './FullsceenMode';
+import TextureMode from './TextureMode';
 import './style.css'
 
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-new FullsceenMode(canvas);
+new TextureMode(canvas);
 
 
 document.body.appendChild(canvas);
