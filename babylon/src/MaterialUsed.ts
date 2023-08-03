699e6e23a3426f2d0e948b21d1ee5979d603c828
dezhizhang <1541609448@qq.com>
2023-08-04 06:04:20 +0800

    feat:material

---
+++ babylon/src/MaterialUsed.ts
@@ -0,0 +1,80 @@
+/*
+ * :file description: 
+ * :name: /babylon/src/MaterialUsed.ts
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-08-04 05:44:38
+ * :last editor: 张德志
+ * :date last edited: 2023-08-04 05:57:48
+ */
+import {  ArcRotateCamera, Color3, Engine, HemisphericLight, Mesh, MeshBuilder, PointLight, Scene,SpotLight,StandardMaterial,Vector3 } from "babylonjs";
+import * as GUI from 'babylonjs-gui';
+
+
+export default class MaterialUsed {
+    engine:Engine;
+    scene:Scene;
+    constructor(private readonly canvas:HTMLCanvasElement) {
+        this.engine = new Engine(this.canvas);
+        this.scene = this.createScene();
+
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
+        }) 
+    }
+
+    // 创建场景
+    createScene():Scene {
+        const scene = new Scene(this.engine);
+
+        const camera = new ArcRotateCamera('camera', -Math.PI / 2,Math.PI / 3,10,new Vector3(0,0,0));
+        camera.attachControl(this.canvas,true);
+
+        // material
+        const redMat = new StandardMaterial('redMat');
+        redMat.emissiveColor = new Color3(1,0,0);
+        
+        const greenMat = new StandardMaterial('greenMat');
+        greenMat.emissiveColor = new Color3(0,1,0);
+
+
+        const blueMat = new StandardMaterial('blueMat');
+        blueMat.emissiveColor = new Color3(0,0,1);
+
+        const whiteMat = new StandardMaterial('whiteMat');
+        whiteMat.emissiveColor = new Color3(1,1,1);
+
+        //red
+        const redLight = new SpotLight('redLight',new Vector3(-0.9,1,-1.8),new Vector3(0,-1,0),Math.PI / 2,1.5,scene);
+        redLight.diffuse = new Color3(1,0,0);
+        redLight.specular = new Color3(0,0,0);
+
+        
+        //green
+        const greenLight = new SpotLight('greenLight',new Vector3(0,1,-0.5),new Vector3(0,-1,0),Math.PI / 2,1.5,scene);
+        greenLight.diffuse = new Color3(0,1,0);
+        greenLight.specular = new Color3(0,0,0);
+
+
+        const blueLight = new SpotLight('blueLight',new Vector3(-0.9,1,-1.8),new Vector3(0,-1,0),Math.PI / 2,1.5,scene);
+        blueLight.diffuse = new Color3(0,0,1);
+        blueLight.specular = new Color3(0,0,0);
+
+        const whiteLight = new SpotLight('whiteLight',new Vector3(0,1,1),new Vector3(0,-1,0),Math.PI / 2,1.5,scene);
+        whiteLight.diffuse = new Color3(1,1,1);
+        whiteLight.specular = new Color3(0,0,0);
+      
+      
+      
+    
+      
+        
+        window.addEventListener('resize',() => {
+            this.engine.resize();
+        })
+        return scene;
+    
+     
+    }
+    
+}
--- babylon/src/main.ts
+++ babylon/src/main.ts
@@ -5,16 +5,16 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-05-04 05:38:45
  * :last editor: 张德志
- * :date last edited: 2023-08-04 05:38:32
+ * :date last edited: 2023-08-04 05:48:03
  */
-import StyleUsed from './StyleUsed';
+import MaterialUsed from './MaterialUsed';
 import './style.css'
 
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-new StyleUsed(canvas);
+new MaterialUsed(canvas);
 
 
 document.body.appendChild(canvas);
