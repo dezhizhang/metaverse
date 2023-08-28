029baaa604aa1de0c9bacbbf6dade99df67c0495
dezhizhang <1541609448@qq.com>
2023-08-29 06:32:45 +0800

    feat:PlayVideoTexture1

--- babylon/src/PlayVideoTexture.ts
+++ babylon/src/PlayVideoTexture.ts
@@ -5,52 +5,77 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-08-29 05:55:26
  * :last editor: 张德志
- * :date last edited: 2023-08-29 06:07:00
+ * :date last edited: 2023-08-29 06:30:13
  */
-import {  ArcRotateCamera, Color3, Engine, Mesh, MeshBuilder, Scene,StandardMaterial,Vector3, VideoTexture } from "babylonjs";
-
-
+import {
+  ArcRotateCamera,
+  Color3,
+  Engine,
+  Mesh,
+  MeshBuilder,
+  Scene,
+  StandardMaterial,
+  Vector3,
+  VideoTexture,
+} from 'babylonjs';
 
 export default class PlayVideoTexture {
-    engine:Engine;
-    scene:Scene;
-    constructor(private readonly canvas:HTMLCanvasElement) {
-        this.engine = new Engine(this.canvas);
-        this.scene = this.createScene();
+  engine: Engine;
+  scene: Scene;
+  constructor(private readonly canvas: HTMLCanvasElement) {
+    this.engine = new Engine(this.canvas);
+    this.scene = this.createScene();
 
-        this.engine.runRenderLoop(() => {
-            this.scene.render();
-        }) 
-    }
+    this.engine.runRenderLoop(() => {
+      this.scene.render();
+    });
+  }
 
-    // 创建场景
-    createScene():Scene {
-        const scene = new Scene(this.engine);
+  // 创建场景
+  createScene(): Scene {
+    const scene = new Scene(this.engine);
 
-        const camera = new ArcRotateCamera('camera', -Math.PI / 2,Math.PI / 2.5,20,new Vector3(0,0,0));
-        camera.attachControl(this.canvas,true);
+    const camera = new ArcRotateCamera(
+      'camera',
+      -Math.PI / 2,
+      Math.PI / 2.5,
+      20,
+      new Vector3(0, 0, 0),
+    );
+    camera.attachControl(this.canvas, true);
 
-        const vPlane = MeshBuilder.CreatePlane('vPlane',{
-            width:7.3967,
-            height:5.762,
-            sideOrientation:Mesh.DOUBLESIDE
-        });
+    const vPlane = MeshBuilder.CreatePlane('vPlane', {
+      width: 7.3967,
+      height: 5.762,
+      sideOrientation: Mesh.DOUBLESIDE,
+    });
 
-        vPlane.position = new Vector3(0,0,0.1);
-        const material = new StandardMaterial('material');
-        const texture = new VideoTexture('texture','https://playground.babylonjs.com/textures/babylonjs.mp4',scene);
-        material.diffuseTexture = texture;
-        material.roughness = 1;
-        material.emissiveColor = Color3.White();
-        vPlane.material = material;
-    
+    vPlane.position = new Vector3(0, 0, 0.1);
+    const material = new StandardMaterial('material');
+    const texture = new VideoTexture(
+      'texture',
+      'https://playground.babylonjs.com/textures/babylonjs.mp4',
+      scene,
+    );
+    material.diffuseTexture = texture;
+    material.roughness = 1;
+    material.emissiveColor = Color3.White();
+    vPlane.material = material;
 
-        window.addEventListener('resize',() => {
-            this.engine.resize();
-        })
+    VideoTexture.CreateFromWebCam(
+      scene,
+      function (vt) {
+        material.emissiveTexture = vt;
+      },
+      { maxWidth: 265, maxHeight: 256, minWidth: 64, minHeight: 64, deviceId: '' },
+      undefined,
+      false,
+    );
 
-        return scene;
+    window.addEventListener('resize', () => {
+      this.engine.resize();
+    });
 
-    }
-    
+    return scene;
+  }
 }
