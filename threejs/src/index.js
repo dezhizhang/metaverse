20b051242a90e28a952f4c4282a33e0b971cc477
dezhizhang <1541609448@qq.com>
2023-08-30 20:58:14 +0800

    feat:misc_exporter_ply

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,77 +1,156 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { PLYExporter } from 'three/examples/jsm/exporters/PLYExporter.js';
 
-const scene = new THREE.Scene();
+let scene, camera, renderer, exporter, mesh;
 
-// 平行光1
-const directionalLight = new THREE.DirectionalLight(0xffffff,0.8);
-directionalLight.position.set(400, 200, 300);
-scene.add(directionalLight);
+const params = {
+  exportASCII: exportASCII,
+  exportBinaryBigEndian: exportBinaryBigEndian,
+  exportBinaryLittleEndian: exportBinaryLittleEndian,
+};
 
-// 平行光2
-const directionalLight2 = new THREE.DirectionalLight(0xffffff,0.8);
-directionalLight2.position.set(-400, -200, -300);
-scene.add(directionalLight2);
+init();
+animate();
 
-// 环境光
-const ambient = new THREE.AmbientLight(0xffffff,0.3);
-scene.add(ambient);
+function init() {
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
+  camera.position.set(4, 2, 4);
 
-// 三维坐标轴
-const axesHelper = new THREE.AxesHelper(300);
-scene.add(axesHelper);
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xa0a0a0);
+  scene.fog = new THREE.Fog(0xa0a0a0, 4, 20);
 
-function createConeMesh(size) {
- const height = size * 4;
- const geometry = new THREE.ConeGeometry(size,height,4);
- geometry.rotateX(-Math.PI / 2);
- geometry.translate(0,0,height / 2);
- const material = new THREE.MeshLambertMaterial({
-   color:0x22ffcc
- });
- const mesh = new THREE.Mesh(geometry,material);
+  exporter = new PLYExporter();
 
- function animation() {
-   mesh.rotateZ(0.05);
-   requestAnimationFrame(animation);
- }
- animation();
- return mesh;
+  //
+
+  const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 3);
+  hemiLight.position.set(0, 20, 0);
+  scene.add(hemiLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+  directionalLight.position.set(0, 20, 10);
+  directionalLight.castShadow = true;
+  directionalLight.shadow.camera.top = 2;
+  directionalLight.shadow.camera.bottom = -2;
+  directionalLight.shadow.camera.left = -2;
+  directionalLight.shadow.camera.right = 2;
+  scene.add(directionalLight);
+
+  // ground
+
+  const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry(40, 40),
+    new THREE.MeshPhongMaterial({ color: 0xcbcbcb, depthWrite: false }),
+  );
+  ground.rotation.x = -Math.PI / 2;
+  ground.receiveShadow = true;
+  scene.add(ground);
+
+  const grid = new THREE.GridHelper(40, 20, 0x000000, 0x000000);
+  grid.material.opacity = 0.2;
+  grid.material.transparent = true;
+  scene.add(grid);
+
+  // export mesh
+
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshPhongMaterial({ vertexColors: true });
+
+  // color vertices based on vertex positions
+  const colors = geometry.getAttribute('position').array.slice();
+  for (let i = 0, l = colors.length; i < l; i++) {
+    if (colors[i] > 0) colors[i] = 0.5;
+    else colors[i] = 0;
+  }
+
+  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3, false));
+
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.castShadow = true;
+  mesh.position.y = 0.5;
+  scene.add(mesh);
+
+  //
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  document.body.appendChild(renderer.domElement);
+
+  //
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0.5, 0);
+  controls.update();
+
+  //
+
+  window.addEventListener('resize', onWindowResize);
+
+  const gui = new dat.GUI();
+
+  gui.add(params, 'exportASCII').name('Export PLY (ASCII)');
+  gui.add(params, 'exportBinaryBigEndian').name('Export PLY (Binary BE)');
+  gui.add(params, 'exportBinaryLittleEndian').name('Export PLY (Binary LE)');
+  gui.open();
 }
 
-const model = new THREE.Group();
-const coneMesh = createConeMesh(10);
-model.add(coneMesh);
-scene.add(model);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-const camera = new THREE.PerspectiveCamera(30, width / height,1,3000);
-camera.position.set(292,233,285);
-camera.lookAt(scene.position);
-
-const renderer = new THREE.WebGLRenderer({
- antialias:true
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(width,height);
-
-document.body.appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera,renderer.domElement);
-
-window.onresize = function() {
- renderer.setSize(window.innerWidth,window.innerHeight);
- camera.aspect = window.innerWidth / window.innerHeight;
- camera.updateProjectionMatrix();
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function render() {
- renderer.render(scene,camera);
- requestAnimationFrame(render);
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
-render();
+function exportASCII() {
+  exporter.parse(mesh, function (result) {
+    saveString(result, 'box.ply');
+  });
+}
 
+function exportBinaryBigEndian() {
+  exporter.parse(
+    mesh,
+    function (result) {
+      saveArrayBuffer(result, 'box.ply');
+    },
+    { binary: true },
+  );
+}
+
+function exportBinaryLittleEndian() {
+  exporter.parse(
+    mesh,
+    function (result) {
+      saveArrayBuffer(result, 'box.ply');
+    },
+    { binary: true, littleEndian: true },
+  );
+}
+
+const link = document.createElement('a');
+link.style.display = 'none';
+document.body.appendChild(link);
+
+function save(blob, filename) {
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
+}
+
+function saveString(text, filename) {
+  save(new Blob([text], { type: 'text/plain' }), filename);
+}
+
+function saveArrayBuffer(buffer, filename) {
+  save(new Blob([buffer], { type: 'application/octet-stream' }), filename);
+}
