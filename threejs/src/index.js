6dbb976bf156c78432bbe90ba52c984af809831d
dezhizhang <1541609448@qq.com>
2023-09-04 23:23:59 +0800

    feat:纹理贴图

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,83 +5,48 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2023-09-04 07:24:02
+ * :date last edited: 2023-09-04 23:22:21
  */
 import * as THREE from 'three';
-import * as dat from 'dat.gui';
-
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-camera.lookAt(0, 0, 0);
 
-scene.add(camera);
-
-const geometry = new THREE.BufferGeometry();
-const vertices = new Float32Array([
-	-1.0,-1.0,1.0,
-	1.0,-1.0,0.0,
-	1.0,1.0,0.0,
-	-1.0,1.0,0.0
-]);
-geometry.setAttribute('position',new THREE.BufferAttribute(vertices,3));
-const indices = new Uint16Array([0,1,2,2,3,0]);
-geometry.setIndex(new THREE.BufferAttribute(indices,1));
-
-geometry.addGroup(0,3,0);
-geometry.addGroup(3,3,1);
-
-
-const material = new THREE.MeshBasicMaterial({
-	color:0x00ff00
-});
-
-const material1 = new THREE.MeshBasicMaterial({
-	color:0xff0000,
-});
-
-
-
-const plane = new THREE.Mesh(geometry,[material,material1]);
-scene.add(plane);
-
-
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.z = 10;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth,window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
-const controls = new OrbitControls(scene, renderer.domElement);
-scene.add(controls);
-
- window.addEventListener('resize',onWindowResize);
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load('https://t7.baidu.com/it/u=2168645659,3174029352&fm=193&f=GIF');
 
 
-function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth,window.innerHeight);
-	
+const planeGeometry = new THREE.PlaneGeometry(1,1);
+const planeMaterial = new THREE.MeshBasicMaterial({
+	color:0xffffff,
+	map:texture,
+	side:THREE.DoubleSide
+});
+
+let plane = new THREE.Mesh(planeGeometry,planeMaterial);
+scene.add(plane);
+
+const controls = new OrbitControls(camera,renderer.domElement);
+
+
+
+
+
+
+
+function render() {
+	requestAnimationFrame(render);
+	renderer.render(scene,camera)
 }
 
-
-
-
-function animate() {
-	controls.update();
-
-	requestAnimationFrame(animate);
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
-	renderer.render(scene, camera)
-}
-
-animate();
+render();
