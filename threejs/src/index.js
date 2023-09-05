ad4b197e49e1a5c05910bcf48bddcf28ca810966
dezhizhang <1541609448@qq.com>
2023-09-05 09:03:29 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,20 +5,21 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2023-09-05 05:34:12
+ * :date last edited: 2023-09-05 09:03:08
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+// import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 
 scene.fog = new THREE.Fog()
 
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0,32,32);
-camera.lookAt(scene.position);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 100);
+camera.position.set(- 5, 3, 10);
+camera.lookAt(0, 2, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -29,16 +30,53 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// 创建长方体
-const boxGeometry = new THREE.BoxGeometry(1,1,100);
-const boxMaterial = new THREE.MeshBasicMaterial({
+
+// const boxGeometry = new THREE.BoxGeometry(1, 1, 100);
+// const boxMaterial = new THREE.MeshBasicMaterial({
+// 	color: 0x00ff00
+// });
+// const box = new THREE.Mesh(boxGeometry, boxMaterial);
+// scene.add(box);
+
+// scene.fog = new THREE.Fog(0x999999, 0.1, 50);
+// scene.background = new THREE.Color(0x999999);
+
+// const gltfloader = new GLTFLoader();
+// gltfloader.load('https://threejs.org/examples/models/gltf/RobotExpressive/RobotExpressive.glb', (gltf) => {
+// 	console.log(gltf);
+// 	scene.add(gltf.scene);
+// })
+
+const sphereGeometry = new THREE.SphereGeometry(1,32,32);
+const sphereMaterial = new THREE.MeshBasicMaterial({
 	color:0x00ff00
 });
-const box = new THREE.Mesh(boxGeometry,boxMaterial);
-scene.add(box);
+const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+sphere.position.x = -2;
+scene.add(sphere);
 
-scene.fog = new THREE.Fog(0x999999,0.1,50);
-scene.background = new THREE.Color(0x999999)
+const sphereGeometry1 = new THREE.SphereGeometry(1,32,32);
+const sphereMaterial1 = new THREE.MeshBasicMaterial({
+	color:0x0000ff
+});
+const sphere2 = new THREE.Mesh(sphereGeometry1,sphereMaterial1);
+scene.add(sphere2);
+
+const sphereGeometry2 = new THREE.SphereGeometry(1,32,32);
+const sphereMaterial2 = new THREE.MeshBasicMaterial({
+	color:0xff00ff
+});
+
+const sphere3 = new THREE.Mesh(sphereGeometry2,sphereMaterial2);
+sphere3.position.x = 2;
+scene.add(sphere3);
+
+const raycaster = 
+ 
+
+
+
+
 
 
 
