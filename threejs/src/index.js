debd4fe9a61ca960b56f4c050c83d77e59c3171c
dezhizhang <1541609448@qq.com>
2023-09-06 07:30:46 +0800

    feat:buffer

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2023-09-06 05:36:01
+ * :date last edited: 2023-09-06 06:36:38
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -32,97 +32,29 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 
-// const boxGeometry = new THREE.BoxGeometry(1, 1, 100);
-// const boxMaterial = new THREE.MeshBasicMaterial({
-// 	color: 0x00ff00
-// });
-// const box = new THREE.Mesh(boxGeometry, boxMaterial);
-// scene.add(box);
-
-// scene.fog = new THREE.Fog(0x999999, 0.1, 50);
-// scene.background = new THREE.Color(0x999999);
-
-// const gltfloader = new GLTFLoader();
-// gltfloader.load('https://threejs.org/examples/models/gltf/RobotExpressive/RobotExpressive.glb', (gltf) => {
-// 	console.log(gltf);
-// 	scene.add(gltf.scene);
-// })
-
-// const sphereGeometry = new THREE.SphereGeometry(1,32,32);
-// const sphereMaterial = new THREE.MeshBasicMaterial({
-// 	color:0x00ff00
-// });
-// const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
-// sphere.position.x = -2;
-// scene.add(sphere);
-
-// const sphereGeometry1 = new THREE.SphereGeometry(1,32,32);
-// const sphereMaterial1 = new THREE.MeshBasicMaterial({
-// 	color:0x0000ff
-// });
-// const sphere2 = new THREE.Mesh(sphereGeometry1,sphereMaterial1);
-// scene.add(sphere2);
-
-// const sphereGeometry2 = new THREE.SphereGeometry(1,32,32);
-// const sphereMaterial2 = new THREE.MeshBasicMaterial({
-// 	color:0xff00ff
-// });
-
-// const sphere3 = new THREE.Mesh(sphereGeometry2,sphereMaterial2);
-// sphere3.position.x = 2;
-// scene.add(sphere3);
+const vertices = new Float32Array([
+	-1.0,-1.0,1.0,
+	1.0,-1.0,1.0,
+	1.0,1.0,1.0,
+	1.0,1.0,1.0,
+	-1.0,1.0,1.0,
+	-1.0,-1.0,1.0
+]);
 
 
-// const raycaster = new THREE.Raycaster();
-
-// const mouse = new THREE.Vector2();
-
-// window.addEventListener('click',(event) => {
-// 	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-// 	mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-
-// 	raycaster.setFromCamera(mouse,camera);
-// 	const intersects = raycaster.intersectObjects(scene.children);
-// 	if(intersects.length) {
-// 		intersects[0].object.material.color.set(0xff0000)
-// 	}
-// })
-
-const sphere = new THREE.Mesh(
-	new THREE.SphereGeometry(1,32,32),
-	new THREE.MeshBasicMaterial({
-		color:0x00ff00
-	})
-);
-sphere.position.x = -4;
-scene.add(sphere);
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute('position',new THREE.BufferAttribute(vertices,3))
+const material = new THREE.MeshBasicMaterial({color:0xff0000,side:THREE.DoubleSide});
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
 
-const tween = new Tween(sphere.position);
-tween.to({x:4},1000);
-//tween.repeat(Infinity);
-//tween.easing(Tween.easing.U)
-
-
-const tween2 = new Tween(sphere.position);
-tween2.to({y:-4},1000);
-tween.chain(tween2);
-
-tween.start();
-// tween2.start();
-
-
-
-
-
-
 function render() {
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
-	tween.update();
 	
 }
 
