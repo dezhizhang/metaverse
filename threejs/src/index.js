9af481ec5bc0955ae7d81bf7f0bbce2dafc279a3
dezhizhang <1541609448@qq.com>
2023-09-12 06:02:59 +0800

    feat:阴影

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2023-09-11 07:54:03
+ * :date last edited: 2023-09-12 06:02:12
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -22,6 +22,7 @@ camera.lookAt(0, 2, 0);
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 
 document.body.appendChild(renderer.domElement);
 
@@ -29,33 +30,29 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 
-
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const url = `https://threejs.org/examples/textures/cube/angus/`;
-const textureCube = cubeTextureLoader.load([
-	`${url}cube_m00_c00.jpg`,`${url}cube_m00_c01.jpg`,
-	`${url}cube_m00_c02.jpg`,`${url}cube_m00_c03.jpg`,
-	`${url}cube_m00_c04.jpg`,`${url}cube_m00_c05.jpg`
-]);
-scene.background = textureCube;
-scene.environment = textureCube;
+const sphereGeometry = new THREE.SphereGeometry(1,20,20);
+const material = new THREE.MeshStandardMaterial();
+const sphere = new THREE.Mesh(sphereGeometry,material);
+sphere.castShadow = true;
+scene.add(sphere);
 
 
+const planeGeometry = new THREE.PlaneGeometry(10,10);
+const plane = new THREE.Mesh(planeGeometry,material);
+plane.position.set(0,-1,0);
+plane.rotation.x = -Math.PI / 2;
+plane.receiveShadow = true;
+scene.add(plane);
 
 
-
-const light = new THREE.AmbientLight(0xffffff);
+const light = new THREE.AmbientLight(0xffffff,0.5);
 scene.add(light);
 
-const directionLight = new THREE.DirectionalLight(0xffffff,0.5);
-directionLight.position.set(10,10,10);
-scene.add(directionLight);
-
-
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube } );
-const cube = new THREE.Mesh(geometry,material);
-scene.add(cube);
+// 平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff,0.5);
+directionalLight.position.set(10,10,10);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
 
 
 const axesHelper = new THREE.AxesHelper(5);
