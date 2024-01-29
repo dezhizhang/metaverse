2aa55637687c334c5f90991940fbff2434ed0135
dezhizhang <1541609448@qq.com>
2024-01-29 22:55:52 +0800

    feat:update

--- webgpu/src/main.ts
+++ webgpu/src/main.ts
@@ -5,59 +5,32 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2023-07-13 05:31:18
  * :last editor: 张德志
- * :date last edited: 2024-01-29 21:15:08
+ * :date last edited: 2024-01-29 22:55:23
  */
 
+async function initwebgpu() {
+  const adapter =await (navigator.gpu as any).requestAdapter();
+  const device =await adapter?.requestDevice();
 
-async function  init() {
-  const adapter = await navigator.gpu.requestAdapter();
-  const device = await adapter?.requestDevice();
 
-  const canvas = document.getElementById('canvas');
+  const canvas = document.createElement('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.style.background = "#00000";
 
-  // 配置gpu
+  const context = canvas.getContext('webgpu');
+  const format  = navigator.gpu.getPreferredCanvasFormat();
 
-  const context = (canvas as any)?.getContext('webgpu');
-  const format = navigator.gpu.getPreferredCanvasFormat();
-  context.configure({
-    device,
-    format:format
+  context?.configure({
+    device:device,
+    format:format,
+   
   });
 
-
-  // 创建顶点缓冲区表示顶点数据
-  const vertexArray = new Float32Array([
-    0.0,0.0,0.0,
-    1.0,0.0,0.0,
-    0.0,1.0,0.0
-  ]);
-
-  const vertexBuffer = (device as any)?.createBuffer({
-    size:vertexArray.byteLength,
-    usege:GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
-  });
-
-  device?.queue.writeBuffer(vertexBuffer,0,vertexArray);
-
-  // const vertexBuffer = (device as  any)?.createBuffer({
-  //   size:vertexArray.byteLength,
-  // });
-
-  const pipeline = (device as any)?.createRenderPipeline({
-    vertex:{
-      buffers:[]
-    }
-  });
-  
-
-  console.log('vertexBuffer',vertexBuffer);
-
-  
+  document.body.appendChild(canvas);
 
 
-  
 }
 
-init();
-
+initwebgpu();
 
