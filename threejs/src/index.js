4c9acaf6694f9ecfb1b0cea53efca552ffb6f5e1
dezhizhang <1541609448@qq.com>
2024-02-02 07:25:41 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,117 +1,230 @@
-/*
- * :file description:
- * :name: /threejs/src/index.js
- * :author: 张德志
- * :copyright: (c) 2023, Tungee
- * :date created: 2023-03-13 05:58:33
- * :last editor: 张德志
- * :date last edited: 2024-02-02 05:56:41
- */
 import * as THREE from 'three';
 import dat from 'dat.gui';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls.js';
 
-// 创建场影
-const scene = new THREE.Scene();
-//  添加背影
-// scene.background = new THREE.CubeTextureLoader()
-//   .setPath('/')
-//   .load(['01.jpg', '01.jpg', '01.jpg', '01.jpg', '01.jpg', '01.jpg']);
+let camera, controls, scene, renderer, light;
 
-// 添加雾
-const fog = new THREE.Fog(new THREE.Color(0xcccccc, 10, 15));
+let material1, material2, material3;
 
-scene.add(fog);
+let analyser1, analyser2, analyser3;
 
-// 创建纹理
-const texture = new THREE.TextureLoader().load('/01.jpg');
+const clock = new THREE.Clock();
 
-const cubeTexture = new THREE.CubeTextureLoader().setPath('parliament/').load([
-	'negx.jpg',
-	'negy.jpg',
-	'negz.jpg',
-	'posx.jpg',
-	'posy.jpg',
-	'posz.jpg',
-	
-]);
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', init);
 
+function init() {
+  const overlay = document.getElementById('overlay');
+  overlay.remove();
 
-scene.background = cubeTexture;
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.set(0, 25, 0);
 
+  const listener = new THREE.AudioListener();
+  camera.add(listener);
 
+  scene = new THREE.Scene();
+  scene.fog = new THREE.FogExp2(0x000000, 0.0025);
 
-// 创建相机
-const camera = new THREE.PerspectiveCamera();
-camera.position.z = 10;
-camera.position.y = 2;
-camera.lookAt(scene.position);
+  light = new THREE.DirectionalLight(0xffffff, 3);
+  light.position.set(0, 0.5, 1).normalize();
+  scene.add(light);
 
-// 创建立方体
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ 
-	// color: 0x0ff00,
-	map:texture 
-});
+  const sphere = new THREE.SphereGeometry(20, 32, 16);
 
-const cube = new THREE.Mesh(geometry, material);
-cube.position.set(0, 3, 0);
+  material1 = new THREE.MeshPhongMaterial({ color: 0xffaa00, flatShading: true, shininess: 0 });
+  material2 = new THREE.MeshPhongMaterial({ color: 0xff2200, flatShading: true, shininess: 0 });
+  material3 = new THREE.MeshPhongMaterial({ color: 0x6622aa, flatShading: true, shininess: 0 });
 
-scene.add(cube);
+  // sound spheres
 
+  const mesh1 = new THREE.Mesh(sphere, material1);
+  mesh1.position.set(-250, 30, 0);
+  scene.add(mesh1);
 
-// 创建渲染器
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+  const sound1 = new THREE.PositionalAudio(listener);
+  const songElement = document.getElementById('song');
+  sound1.setMediaElementSource(songElement);
+  sound1.setRefDistance(20);
+  songElement.play();
+  mesh1.add(sound1);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.01;
+  //
 
-controls.autoRotate = false;
+  const mesh2 = new THREE.Mesh(sphere, material2);
+  mesh2.position.set(250, 30, 0);
+  scene.add(mesh2);
 
-const axesHelper = new THREE.AxesHelper(5);
-axesHelper.position.y = 3;
-scene.add(axesHelper);
+  const sound2 = new THREE.PositionalAudio(listener);
+  const skullbeatzElement = document.getElementById('skullbeatz');
+  sound2.setMediaElementSource(skullbeatzElement);
+  sound2.setRefDistance(20);
+  skullbeatzElement.play();
+  mesh2.add(sound2);
 
+  //
 
-const controlsConfig = {
-	rotationSpeed:0.01,
-	color:'#66ccff',
-	wireframe:false,
-	envMap:'无'
+  const mesh3 = new THREE.Mesh(sphere, material3);
+  mesh3.position.set(0, 30, -250);
+  scene.add(mesh3);
+
+  const sound3 = new THREE.PositionalAudio(listener);
+  const oscillator = listener.context.createOscillator();
+  oscillator.type = 'sine';
+  oscillator.frequency.setValueAtTime(144, sound3.context.currentTime);
+  oscillator.start(0);
+  sound3.setNodeSource(oscillator);
+  sound3.setRefDistance(20);
+  sound3.setVolume(0.5);
+  mesh3.add(sound3);
+
+  // analysers
+
+  analyser1 = new THREE.AudioAnalyser(sound1, 32);
+  analyser2 = new THREE.AudioAnalyser(sound2, 32);
+  analyser3 = new THREE.AudioAnalyser(sound3, 32);
+
+  // global ambient audio
+
+  const sound4 = new THREE.Audio(listener);
+  const utopiaElement = document.getElementById('utopia');
+  sound4.setMediaElementSource(utopiaElement);
+  sound4.setVolume(0.5);
+  utopiaElement.play();
+
+  // ground
+
+  const helper = new THREE.GridHelper(1000, 10, 0x444444, 0x444444);
+  helper.position.y = 0.1;
+  scene.add(helper);
+
+  //
+
+  const SoundControls = function () {
+    this.master = listener.getMasterVolume();
+    this.firstSphere = sound1.getVolume();
+    this.secondSphere = sound2.getVolume();
+    this.thirdSphere = sound3.getVolume();
+    this.Ambient = sound4.getVolume();
+  };
+
+  const GeneratorControls = function () {
+    this.frequency = oscillator.frequency.value;
+    this.wavetype = oscillator.type;
+  };
+
+  const gui = new dat.GUI();
+  const soundControls = new SoundControls();
+  const generatorControls = new GeneratorControls();
+  const volumeFolder = gui.addFolder('sound volume');
+  const generatorFolder = gui.addFolder('sound generator');
+
+  volumeFolder
+    .add(soundControls, 'master')
+    .min(0.0)
+    .max(1.0)
+    .step(0.01)
+    .onChange(function () {
+      listener.setMasterVolume(soundControls.master);
+    });
+  volumeFolder
+    .add(soundControls, 'firstSphere')
+    .min(0.0)
+    .max(1.0)
+    .step(0.01)
+    .onChange(function () {
+      sound1.setVolume(soundControls.firstSphere);
+    });
+  volumeFolder
+    .add(soundControls, 'secondSphere')
+    .min(0.0)
+    .max(1.0)
+    .step(0.01)
+    .onChange(function () {
+      sound2.setVolume(soundControls.secondSphere);
+    });
+
+  volumeFolder
+    .add(soundControls, 'thirdSphere')
+    .min(0.0)
+    .max(1.0)
+    .step(0.01)
+    .onChange(function () {
+      sound3.setVolume(soundControls.thirdSphere);
+    });
+  volumeFolder
+    .add(soundControls, 'Ambient')
+    .min(0.0)
+    .max(1.0)
+    .step(0.01)
+    .onChange(function () {
+      sound4.setVolume(soundControls.Ambient);
+    });
+  volumeFolder.open();
+  generatorFolder
+    .add(generatorControls, 'frequency')
+    .min(50.0)
+    .max(5000.0)
+    .step(1.0)
+    .onChange(function () {
+      oscillator.frequency.setValueAtTime(
+        generatorControls.frequency,
+        listener.context.currentTime,
+      );
+    });
+  generatorFolder
+    .add(generatorControls, 'wavetype', ['sine', 'square', 'sawtooth', 'triangle'])
+    .onChange(function () {
+      oscillator.type = generatorControls.wavetype;
+    });
+
+  generatorFolder.open();
+
+  //
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  //
+
+  controls = new FirstPersonControls(camera, renderer.domElement);
+
+  controls.movementSpeed = 70;
+  controls.lookSpeed = 0.05;
+  controls.lookVertical = false;
+
+  //
+
+  window.addEventListener('resize', onWindowResize);
+
+  animate();
 }
 
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-const gui = new dat.GUI();
-const f = gui.addFolder('配置');
-f.add(controlsConfig,'rotationSpeed').min(0.01).max(0.1).step(0.01);
-f.add(controlsConfig,'color');
-f.add(controlsConfig,'envMap',['无','全']);
-f.add(controlsConfig,'wireframe');
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-f.open();
-
-
-
-
-
-document.body.appendChild(renderer.domElement);
-
-const helper = new THREE.GridHelper(10, 10);
-scene.add(helper);
+  controls.handleResize();
+}
 
 function animate() {
   requestAnimationFrame(animate);
-
-//   cube.rotation.x += controlsConfig.rotationSpeed;
-//   cube.material = controlsConfig.wireframe;
-  
-
-
-  // controls.update();
-  renderer.render(scene, camera);
+  render();
 }
 
-animate();
+function render() {
+  const delta = clock.getDelta();
+
+  controls.update(delta);
+
+  material1.emissive.b = analyser1.getAverageFrequency() / 256;
+  material2.emissive.b = analyser2.getAverageFrequency() / 256;
+  material3.emissive.b = analyser3.getAverageFrequency() / 256;
+
+  renderer.render(scene, camera);
+}
