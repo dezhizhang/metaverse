f91f397bdf72d6f89d23836481f74ea5ee48126b
dezhizhang <1541609448@qq.com>
2024-02-07 22:02:00 +0800

    feat:更新条件

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,74 +1,102 @@
-
 import * as THREE from 'three';
-import Stats from 'stats.js';
-import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls.js';
 
+import Stats from 'three/addons/libs/stats.module.js';
 
-let camera,controls,scene,renderer,stats;
+import { FlyControls } from 'three/addons/controls/FlyControls.js';
+import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js';
 
-let mesh,geometry,material,clock;
+let stats;
 
-const worldWidth = 128;
-const worldDepth = 128;
+let camera, scene, renderer;
+let controls;
+
+const clock = new THREE.Clock();
 
 init();
 animate();
 
-
-
 function init() {
-	camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight,1,2000);
-	camera.position.y = 200;
-
-	clock = new THREE.Clock();
+	camera = new THREE.PerspectiveCamera(40,window.innerWidth / window.innerHeight,1,15000);
+	camera.position.z = 250;
 
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xaaccff);
-	scene.fog = new THREE.FogExp2(0xaaccff,0.0007);
+	scene.background = new THREE.Color().setHSL(0.51, 0.4, 0.01,THREE.SRGBColorSpace);
+	scene.fog = new THREE.Fog(scene.background,3500,15000);
 
-	geometry = new THREE.PlaneGeometry(2000,2000,worldWidth - 1,worldDepth - 1);
-	geometry.rotateX(-Math.PI / 2);
+	const s = 250;
+	const geometry = new THREE.BoxGeometry(s,s,s);
+	const material = new THREE.MeshPhongMaterial({
+		color:0xffffff,
+		specular:0xffffff,
+		shininess:50,
+	});
 
-	const position = geometry.attributes.position;
-	position.usage = THREE.DynamicDrawUsage;
+	for(let i=0;i < 3000;i++) {
+		const mesh = new THREE.Mesh(geometry,material);
+		mesh.position.x = 8000 * (2.0 * Math.random() - 1.0);
+		mesh.position.y = 8000 * (2.0 * Math.random() - 1.0);
+		mesh.position.z = 8000 * (2.0 * Math.random() - 1.0);
 
-	for(let i=0;i < position.count;i++) {
-		const y = 35 * Math.sin( i / 2);
-		position.setY(i,y);
+		mesh.rotation.x = Math.random() * Math.PI;
+		mesh.rotation.y = Math.random() * Math.PI;
+		mesh.rotation.z = Math.random() * Math.PI;
+
+		mesh.matrixAutoUpdate = false;
+		mesh.updateMatrix();
+
+		scene.add(mesh);
 
 	}
 
-	const texture = new THREE.TextureLoader().load('textures/water.jpg');
-	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-	texture.repeat.set(5,5);
-	texture.colorSpace = THREE.SRGBColorSpace;
+	const dirLight = new THREE.DirectionalLight(0xffffff,0.15);
+	dirLight.position.set(0,-1,0).normalize();
+	// dirLight.position.setHSL(0.1,0.7,0.5);
+	scene.add(dirLight);
 
-	material = new THREE.MeshBasicMaterial({color:0x0044ff,map:texture});
-	mesh = new THREE.Mesh(geometry,material);
-	scene.add(mesh);
+	const textureLoader = new THREE.TextureLoader();
+	const textureFlare0 = textureLoader.load('textures/lensflare/lensflare0.png');
+	const textureFlare3 = textureLoader.load('textures/lensflare/lensflare3.png');
+
+	addLight(0.55,0.9,0.5, 5000, 0, -1000);
+	addLight(0.08, 0.8, 0.5, 0, 0, -1000);
+	addLight(0.995, 0.5, 0.9, 5000, 5000, -1000);
+
+	function addLight(h, s, l, x, y, z) {
+		const light = new THREE.PointLight(0xffffff, 1.5, 2000, 0);
+		light.color.setHSL(h, s, l);
+		light.position.set(x, y, z);
+		scene.add(light);
+	
+		const lensflare = new Lensflare();
+		lensflare.addElement(new LensflareElement(textureFlare0, 700, 0, light.color));
+		lensflare.addElement(new LensflareElement(textureFlare3, 60, 0.6));
+		lensflare.addElement(new LensflareElement(textureFlare3, 70, 0.7));
+		lensflare.addElement(new LensflareElement(textureFlare3, 120, 0.9));
+		lensflare.addElement(new LensflareElement(textureFlare3, 70, 1));
+		light.add(lensflare);
+	}
 
 	renderer = new THREE.WebGLRenderer({antialias:true});
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth,window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 
-	controls = new FirstPersonControls(camera,renderer.domElement);
-	controls.movementSpeed = 500;
-	controls.lookSpeed = 0.1;
-	
+	controls = new FlyControls(camera,renderer.domElement);
+	controls.movementSpeed = 2500;
+	controls.domElement = document.body;
+	controls.rollSpeed = Math.PI / 6;
+
 	stats = new Stats();
 	document.body.appendChild(stats.dom);
 
 	window.addEventListener('resize',onWindowResize);
+
 }
 
-
 function onWindowResize() {
+	renderer.setSize(window.innerWidth,window.innerHeight);
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-
-	renderer.setSize(window.innerWidth,window.innerHeight);
-	controls.handleResize();
 
 }
 
@@ -76,25 +104,12 @@ function animate() {
 	requestAnimationFrame(animate);
 	render();
 	stats.update();
-
 }
 
 function render() {
 	const delta = clock.getDelta();
-	const time = clock.getElapsedTime() * 10;
-
-	const position = geometry.attributes.position;
-
-	for ( let i = 0; i < position.count; i ++ ) {
-
-		const y = 35 * Math.sin( i / 5 + ( time + i ) / 7 );
-		position.setY( i, y );
-
-	}
-
-	position.needsUpdate = true;
-
-	controls.update( delta );
-	renderer.render( scene, camera );
+	controls.update(delta);
+	renderer.render(scene,camera);
 }
+
 
