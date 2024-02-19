b90870a049d6c7f14444bc1429bec6d93b58b059
dezhizhang <1541609448@qq.com>
2024-02-19 20:35:39 +0800

    feat:TransformControls

--- threejs/examples/transformControls.js
+++ threejs/examples/transformControls.js
@@ -1,3 +1,12 @@
+/*
+ * :file description: 
+ * :name: /threejs/examples/transformControls.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-02-19 20:34:29
+ * :last editor: 张德志
+ * :date last edited: 2024-02-19 20:34:30
+ */
 import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
