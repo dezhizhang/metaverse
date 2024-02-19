61565530187f221f34dab5bef16152ac944c10bc
dezhizhang <1541609448@qq.com>
2024-02-19 21:58:03 +0800

    feat:Timer

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,31 +1,106 @@
 import * as THREE from 'three';
-import { scene, renderer, camera } from './scene.js';
 
-document.body.appendChild(renderer.domElement);
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { Timer } from 'three/addons/misc/Timer.js';
 
-const height = 100;
-const radius = 25;
+let camera, scene, renderer, timer;
 
-const geometry = new THREE.ConeGeometry(radius,height,4);
-geometry.rotateX(-Math.PI / 2);
-geometry.translate(0,0,height / 2);
-const material = new THREE.MeshLambertMaterial({
-  color: 0x00ffff,
-});
-const mesh = new THREE.Mesh(geometry,material);
-scene.add(mesh);
+let mesh;
 
+let sign = 1;
+const speed = 0.5;
 
-function render() {
-  renderer.render(scene,camera);
-  requestAnimationFrame(render);
+init();
+animate();
+
+function init() {
+  const container = document.getElementById('container');
+
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.2, 100);
+  camera.position.set(0, 5, 5);
+
+  scene = new THREE.Scene();
+
+  timer = new Timer();
+
+  const light1 = new THREE.PointLight(0xff2200, 50000);
+  light1.position.set(100, 100, 100);
+  scene.add(light1);
+
+  const light2 = new THREE.PointLight(0x22ff00, 10000);
+  light2.position.set(-100, -100, -100);
+  scene.add(light2);
+
+  scene.add(new THREE.AmbientLight(0x111111));
+
+  const loader = new GLTFLoader();
+  loader.load('https://threejs.org/examples/models/gltf/AnimatedMorphSphere/glTF/AnimatedMorphSphere.gltf', function (gltf) {
+    mesh = gltf.scene.getObjectByName('AnimatedMorphSphere');
+    mesh.rotation.z = Math.PI / 2;
+    scene.add(mesh);
+
+    //
+
+    const pointsMaterial = new THREE.PointsMaterial({
+      size: 10,
+      sizeAttenuation: false,
+      map: new THREE.TextureLoader().load('https://threejs.org/examples/textures/sprites/disc.png'),
+      alphaTest: 0.5,
+    });
+
+    const points = new THREE.Points(mesh.geometry, pointsMaterial);
+    points.morphTargetInfluences = mesh.morphTargetInfluences;
+    points.morphTargetDictionary = mesh.morphTargetDictionary;
+    mesh.add(points);
+  });
+
+  //
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  //
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 1;
+  controls.maxDistance = 20;
+
+  //
+
+  window.addEventListener('resize', onWindowResize);
 }
 
-render();
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-const mesh2 = mesh.clone();
-mesh2.scale.z = 0.5;
-mesh2.position.z = height*(1+mesh2.scale.z);
-mesh2.rotateX(Math.PI);
-scene.add(mesh2);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
+function animate() {
+  requestAnimationFrame(animate);
+  timer.update();
+  render();
+}
+
+function render() {
+  const delta = timer.getDelta();
+
+  if (mesh !== undefined) {
+    const step = delta * speed;
+
+    mesh.rotation.y += step;
+
+    mesh.morphTargetInfluences[1] = mesh.morphTargetInfluences[1] + step * sign;
+
+    if (mesh.morphTargetInfluences[1] <= 0 || mesh.morphTargetInfluences[1] >= 1) {
+      sign *= -1;
+    }
+  }
+
+  renderer.render(scene, camera);
+}
