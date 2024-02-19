33261b90668f6e3b6f7e11e616d29fd64f395ab5
dezhizhang <1541609448@qq.com>
2024-02-20 07:38:32 +0800

    feat:webgl_loader_pdb

---
+++ threejs/examples/webgl_instancing_scatter.js
@@ -0,0 +1,9 @@
+/*
+ * :file description: 
+ * :name: /threejs/examples/webgl_instancing_scatter.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-02-20 07:30:22
+ * :last editor: 张德志
+ * :date last edited: 2024-02-20 07:30:23
+ */
