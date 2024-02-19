cbc6d57a203c83c74ad406adf0a8f69feb0b0b0f
dezhizhang <1541609448@qq.com>
2024-02-19 21:49:51 +0800

    feat:ConeGeometry

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,85 +1,31 @@
 import * as THREE from 'three';
+import { scene, renderer, camera } from './scene.js';
 
-import { FlyControls } from 'three/examples/jsm/controls/FlyControls.js';
+document.body.appendChild(renderer.domElement);
 
-let camera, scene, renderer, controls;
+const height = 100;
+const radius = 25;
 
-const clock = new THREE.Clock();
+const geometry = new THREE.ConeGeometry(radius,height,4);
+geometry.rotateX(-Math.PI / 2);
+geometry.translate(0,0,height / 2);
+const material = new THREE.MeshLambertMaterial({
+  color: 0x00ffff,
+});
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
 
-init();
-animate();
-
-function init() {
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 15000);
-  camera.position.z = 1000;
-
-  scene = new THREE.Scene();
-  scene.fog = new THREE.Fog(0x000000, 0.1, 15000);
-
-  const pointLight = new THREE.PointLight(0xff2200, 3, 0, 0);
-  pointLight.position.set(0, 0, 0);
-  scene.add(pointLight);
-
-  const dirLight = new THREE.DirectionalLight(0xffffff, 3);
-  dirLight.position.set(0, 0, 1).normalize();
-  scene.add(dirLight);
-
-  const geometry = [
-    [new THREE.IcosahedronGeometry(100, 16), 50],
-    [new THREE.IcosahedronGeometry(100, 8), 300],
-    [new THREE.IcosahedronGeometry(100, 4), 1000],
-    [new THREE.IcosahedronGeometry(100, 2), 2000],
-    [new THREE.IcosahedronGeometry(100, 1), 8000],
-  ];
-  const material = new THREE.MeshLambertMaterial({
-    color: 0xffffff,
-    wireframe: true,
-  });
-
-  for (let j = 0; j < 1000; j++) {
-    const lod = new THREE.LOD();
-    for (let i = 0; i < geometry.length; i++) {
-      const mesh = new THREE.Mesh(geometry[i][0], material);
-      mesh.scale.set(1.5, 1.5, 1.5);
-      mesh.updateMatrix();
-      mesh.matrixAutoUpdate = false;
-      lod.addLevel(mesh, geometry[i][1]);
-    }
-    lod.position.x = 10000 * (0.5 - Math.random());
-    lod.position.y = 7500 * (0.5 - Math.random());
-    lod.position.z = 10000 * (0.5 - Math.random());
-    lod.updateMatrix();
-    lod.matrixAutoUpdate = false;
-    scene.add(lod);
-  }
-
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  controls = new FlyControls(camera, renderer.domElement);
-  controls.movementSpeed = 1000;
-  controls.rollSpeed = Math.PI / 10;
-
-  window.addEventListener('resize', onWindowResize);
-}
-
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-  render();
-}
 
 function render() {
-  controls.update(clock.getDelta());
-
-  renderer.render(scene, camera);
+  renderer.render(scene,camera);
+  requestAnimationFrame(render);
 }
+
+render();
+
+const mesh2 = mesh.clone();
+mesh2.scale.z = 0.5;
+mesh2.position.z = height*(1+mesh2.scale.z);
+mesh2.rotateX(Math.PI);
+scene.add(mesh2);
+
