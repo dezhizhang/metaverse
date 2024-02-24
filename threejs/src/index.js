c3229c747175c62a3a2b389e28bf8e00e9c81ab5
dezhizhang <1541609448@qq.com>
2024-02-24 13:38:31 +0800

    feat:BufferAttribute

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,70 +1,56 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /threejs/src/index.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2024-02-24 13:13:00
+ * :date last edited: 2024-02-24 13:37:06
  */
-
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-//创建场影
+// 创建场影
 const scene = new THREE.Scene();
 
-//创建相机
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
-
-// 设置相机位置
-camera.position.set(0,0,10);
+// 创建相机
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 10);
 scene.add(camera);
 
-// 创建几何体
-const geometry = new THREE.BufferGeometry();
-const vertices = new Float32Array([
-  -1.0,-1.0,1.0,
-  1.0,-1.0,1.0,
-  1.0,1.0,1.0,
-  1.0,1.0,1.0,
-  -1.0,1.0,1.0,
-  -1.0,-1.0,1.0
-]);
-
-geometry.setAttribute('position',new THREE.BufferAttribute(vertices,3));
+for(let i=0;i < 50;i++) {
+  const geometry = new THREE.BufferGeometry();
+  const positionArray = new Float32Array(9);
 
 
-const material = new THREE.MeshBasicMaterial({
-  color:0xffff00,
-  side:THREE.DoubleSide
-});
-const mesh = new THREE.Mesh(geometry,material);
+  for(let j=0;j < 9;j++) {
+    positionArray[j] = Math.random() * 5;
 
-console.log(mesh);
+  }
+    
+  geometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3));
 
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff * Math.random(),
+    side: THREE.DoubleSide,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-// 将几何体添加到场景中
-scene.add(mesh);
-
-// 初始化渲染器
-const renderer = new THREE.WebGL1Renderer();
-
-// 设置渲染器大小
-renderer.setSize(window.innerWidth,window.innerHeight);
-
-const controls = new OrbitControls(camera,renderer.domElement);
+}
 
 
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function render() {
   requestAnimationFrame(render);
-  renderer.render(scene,camera);
+  renderer.render(scene, camera);
 }
 
 render();
 
-document.body.append(renderer.domElement);
-
-// renderer.render(scene,camera);
-
+document.body.appendChild(renderer.domElement);
