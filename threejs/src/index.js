eb732714813ece6a5d0580f720dae7e2bd452de2
dezhizhang <1541609448@qq.com>
2024-02-24 14:14:45 +0800

    feat: 纹理对像

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,39 +1,37 @@
-// import * as THREE from 'three';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
-// camera.position.set(0,0,10);
-// scene.add(camera);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.set(0,0,10);
+scene.add(camera);
 
-// // 导入纹理
-// const textureLoader = new THREE.TextureLoader();
-// const texture = textureLoader.load('./01.jpg');
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load('./01.jpg');
 
-
-// const cubeGeometry = new THREE.BoxGeometry(1,1,1);
-// const cubeMaterial = new THREE.MeshBasicMaterial({
-//   // color:0xff00ff,
-//   map:texture,
-//   side:THREE.DoubleSide
-// });
-
-// const cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
-// scene.add(cube);
+const cubeGeometry = new THREE.BoxGeometry();
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  map:texture,
+  side:THREE.DoubleSide
+});
+const cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
+scene.add(cube);
 
 
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth,window.innerHeight);
-// renderer.setPixelRatio(window.devicePixelRatio);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 
-// const controls = new OrbitControls(camera,renderer.domElement);
+const controls = new OrbitControls(camera,renderer.domElement);
 
-// function render() {
-//   requestAnimationFrame(render);
-//   renderer.render(scene,camera);
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene,camera);
+}
 
-// }
+render();
 
-// render();
+document.body.appendChild(renderer.domElement);
 
-// document.body.appendChild(renderer.domElement);
+
+
