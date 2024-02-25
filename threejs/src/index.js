1d798e7a60d1fac0f7bfd0cd41fe5467f05e4df1
dezhizhang <1541609448@qq.com>
2024-02-26 07:26:57 +0800

    feat:物理世界

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,69 +1,84 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+
+const clock = new THREE.Clock();
+//创建场影
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,10000);
+
+//创建相机
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+
+// 设置相机位置
 camera.position.set(0,0,10);
 scene.add(camera);
 
-const textureLoader = new THREE.TextureLoader();
-const snowflake = textureLoader.load('./snowflake1.png');
 
-const geometry = new THREE.BufferGeometry();
-
-
-const count = 5000;
-
-const colors = new Float32Array(count * 3);
-const positions = new Float32Array(count * 3);
-
-for(let i=0;i < count * 3;i++) {
-  colors[i] = new THREE.Color(0xffffff * Math.random());
-  positions[i] = (Math.random()  - 0.5) * 100;
-  
-}
-
-geometry.setAttribute('position',new THREE.BufferAttribute(positions,3));
-geometry.setAttribute('color',new THREE.BufferAttribute(colors,3))
-
-
-// const sphereGeometry = new THREE.SphereGeometry(3,30,30);
-const pointMaterial = new THREE.PointsMaterial();
-pointMaterial.size = 1;
-pointMaterial.color.set(0xff0000);
-pointMaterial.map = snowflake;
-pointMaterial.alphaMap = snowflake;
-pointMaterial.transparent = true;
-pointMaterial.sizeAttenuation = true;
-pointMaterial.depthWrite = false;
-pointMaterial.vertexColors = true;
-pointMaterial.blendAlpha = THREE.AdditiveBlending;
-
-const sphere = new THREE.Points(geometry,pointMaterial);
+const sphereGeometry = new THREE.SphereGeometry(1,20,20);
+const sphereMaterial = new THREE.MeshStandardMaterial();
+const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+sphere.castShadow = true;
 scene.add(sphere);
 
-const light = new THREE.AmbientLight(0xfffff);
-scene.add(light);
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20,20),
+  new THREE.MeshStandardMaterial(),
+);
+floor.position.set(0,-5,0)
+floor.rotation.x = -Math.PI / 2;
+floor.receiveShadow = true;
+scene.add(floor);
 
-const pointLight = new THREE.PointLight(0xfffff,0.5);
-pointLight.position.set(3,3,3);
-scene.add(pointLight);
 
-const renderer = new THREE.WebGLRenderer();
+const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
+scene.add(ambientLight);
+
+
+const directionalLight = new THREE.DirectionalLight(0xffffff,0.5);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
+
+const renderer = new THREE.WebGL1Renderer();
+
+// 设置渲染器大小
 renderer.setSize(window.innerWidth,window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.shadowMap.enabled = true;
+renderer.shadowMap.enabled = true; 
+
+document.body.append(renderer.domElement);
+
+
+const controls = new OrbitControls(camera,renderer.domElement);
+
 
 const axesHelper = new THREE.AxesHelper(10);
 scene.add(axesHelper);
 
+
+const world = new CANNON.World();
+world.gravity.set(0,-9.8,0);
+const sphereShape = new CANNON.Sphere(1);
+
+const sphereBody = new CANNON.Body({
+  shape:sphereShape,
+  position:new CANNON.Vec3(0,0,0),
+  mass:1,
+  material:new CANNON.Material(),
+});
+
+world.addBody(sphereBody);
+
+
 function render() {
   requestAnimationFrame(render);
+  world.step(1 / 120,clock.getDelta());
+  sphere.position.copy(sphereBody.position);
   renderer.render(scene,camera);
 }
 
 render();
 
-const controls = new OrbitControls(camera,renderer.domElement);
 
-document.body.appendChild(renderer.domElement);
+
