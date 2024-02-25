1656fd5e8ec8892705a7a9ada034c226318ec10c
dezhizhang <1541609448@qq.com>
2024-02-25 15:15:38 +0800

    feat: 聚光灯

--- threejs/examples/castShadow.js
+++ threejs/examples/castShadow.js
@@ -3,7 +3,73 @@
  * :name: /threejs/examples/castShadow.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-02-25 14:24:55
+ * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2024-02-25 14:24:56
+ * :date last edited: 2024-02-25 14:26:17
  */
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+//创建场影
+const scene = new THREE.Scene();
+
+//创建相机
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.set(0,0,10);
+scene.add(camera);
+
+// 
+const sphereGeometry = new THREE.SphereGeometry(1,20,20);
+const material = new THREE.MeshStandardMaterial({
+
+});
+
+const sphere = new THREE.Mesh(sphereGeometry,material);
+sphere.castShadow = true;
+scene.add(sphere);
+
+const planeGeometry = new THREE.PlaneGeometry(10,10);
+const plane = new THREE.Mesh(planeGeometry,material);
+plane.position.set(0,-1,0);
+plane.rotation.x = -Math.PI / 2;
+plane.receiveShadow = true;
+scene.add(plane);
+
+
+// 灯光
+const directionalLight = new THREE.DirectionalLight(0xffffff,0.5);
+directionalLight.position.set(10,10,10);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
+const light = new THREE.AmbientLight(0xffffff,0.5);
+scene.add(light);
+
+
+
+const renderer = new THREE.WebGLRenderer(); 
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true; 
+
+
+const axesHelper = new THREE.AxesHelper(10);
+scene.add(axesHelper);
+
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene,camera);
+}
+
+render();
+
+const controls = new OrbitControls(camera,renderer.domElement);
+
+
+document.body.appendChild(renderer.domElement);
+
+
+
--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,36 +1,21 @@
-/*
- * :file description: 
- * :name: /threejs/src/index.js
- * :author: 张德志
- * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
- * :last editor: 张德志
- * :date last edited: 2024-02-25 14:24:01
- */
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-
-//创建场影
 const scene = new THREE.Scene();
 
-//创建相机
+// 创建相机
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
 camera.position.set(0,0,10);
 scene.add(camera);
 
-// 
 const sphereGeometry = new THREE.SphereGeometry(1,20,20);
-const material = new THREE.MeshStandardMaterial({
-
-});
-
+const material = new THREE.MeshStandardMaterial();
 const sphere = new THREE.Mesh(sphereGeometry,material);
 sphere.castShadow = true;
+
 scene.add(sphere);
 
-const planeGeometry = new THREE.PlaneGeometry(10,10);
+const planeGeometry = new THREE.PlaneGeometry(50,50);
 const plane = new THREE.Mesh(planeGeometry,material);
 plane.position.set(0,-1,0);
 plane.rotation.x = -Math.PI / 2;
@@ -39,37 +24,42 @@ scene.add(plane);
 
 
 // 灯光
-const directionalLight = new THREE.DirectionalLight(0xffffff,0.5);
-directionalLight.position.set(10,10,10);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
+const spotLight = new THREE.SpotLight(0xffffff,0.5);
+spotLight.position.set(5,5,5);
+spotLight.castShadow = true;
+spotLight.shadow.radius = 20;
+spotLight.color = new THREE.Color(0xff00ff);
+spotLight.shadow.mapSize.set(4096,4096);
+spotLight.target = sphere;
+scene.add(spotLight);
+
+
+const spotLightHelper = new THREE.SpotLightHelper( spotLight );
+scene.add(spotLightHelper)
 
 const light = new THREE.AmbientLight(0xffffff,0.5);
 scene.add(light);
 
 
-
-const renderer = new THREE.WebGLRenderer(); 
+// 渲染
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.shadowMap.enabled = true; 
-
+renderer.shadowMap.enabled = true;
 
 const axesHelper = new THREE.AxesHelper(10);
 scene.add(axesHelper);
 
-
 function render() {
   requestAnimationFrame(render);
   renderer.render(scene,camera);
+
 }
 
 render();
 
 const controls = new OrbitControls(camera,renderer.domElement);
 
-
 document.body.appendChild(renderer.domElement);
-
 
 
