0d9a97ff103232efc357045dcf8ebc44f131feea
dezhizhang <1541609448@qq.com>
2024-02-27 22:33:27 +0800

    ftr: 自定义着色器

--- threejs/examples/shaderMaterial.js
+++ threejs/examples/shaderMaterial.js
@@ -1,4 +1,13 @@
 /*
+ * :file description: 
+ * :name: /threejs/examples/shaderMaterial.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-02-27 22:32:53
+ * :last editor: 张德志
+ * :date last edited: 2024-02-27 22:32:54
+ */
+/*
  * :file description:
  * :name: /threejs/src/index.js
  * :author: 张德志
