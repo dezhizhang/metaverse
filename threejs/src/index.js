c8d4d5ed6864498953219522b2a48fdfed985023
dezhizhang <1541609448@qq.com>
2024-03-02 21:10:03 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,53 +1,46 @@
+/*
+ * :file description:
+ * :name: /threejs/src/index.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2023-03-13 05:58:33
+ * :last editor: 张德志
+ * :date last edited: 2024-03-02 21:09:07
+ */
 import * as THREE from 'three';
-import vertexShader from './shader/water/vertex.glsl';
-import fragmentShader from './shader/water/fragment.glsl';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
-const clock = new THREE.Clock();
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth/ window.innerHeight,0.1,10000);
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
 camera.position.set(0,0,10);
 scene.add(camera);
 
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1,1),new THREE.MeshBasicMaterial({
+	color:0xff00ff,
+}));
+plane.rotation.x = -Math.PI / 2;
+scene.add(plane);
 
-
-const shaderMaterial = new THREE.ShaderMaterial({
-  vertexShader,
-  fragmentShader
-});
-
-
-const geometry = new THREE.PlaneGeometry();
-
-const floor = new THREE.Mesh(geometry,shaderMaterial);
-floor.rotation.x = -Math.PI / 2;
-scene.add(floor);
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera,renderer.domElement);
 
+const controls = new OrbitControls(camera,renderer.domElement);
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
 
-window.addEventListener('resize',function() {
-  camera.aspect = window.innerWidth / this.window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth,window.innerHeight);
-})
 
 function render() {
-  requestAnimationFrame(render);
-  renderer.render(scene,camera);
+	requestAnimationFrame(render);
+	renderer.render(scene,camera);
 }
 
 render();
-
-
 
