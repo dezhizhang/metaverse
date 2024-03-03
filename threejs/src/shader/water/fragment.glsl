0949f345d113e78a3d82ec3267c283f518ecd0b9
dezhizhang <1541609448@qq.com>
2024-03-03 21:16:38 +0800

    ftr: 编译前修改着色器

--- threejs/src/shader/water/fragment.glsl
+++ threejs/src/shader/water/fragment.glsl
@@ -1,4 +1,8 @@
 
+precision mediump float;
+varying float vElevation;
+
 void main() {
-	gl_FragColor = vec4(1.0,1.0,0.0,1.0);
+	float opacity = (vElevation + 1.0) / 2.0;
+	gl_FragColor = vec4(1.0 * opacity,1.0 * opacity,0.0 * opacity,1);
 }
--- threejs/src/shader/water/vertex.glsl
+++ threejs/src/shader/water/vertex.glsl
@@ -1,3 +1,23 @@
+precision mediump float;
+
+uniform float uWaresFrequency;
+uniform float uScale;
+
+// 计算出的高度传给片元着钯器
+varying float vElevation;
+
+
+
 void main() {
-	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position,1.0);
+	vec4 modelPosition = modelMatrix * vec4(position,1.0);
+
+	float elevation = sin(modelPosition.x * 10.0 * uWaresFrequency) * sin(modelPosition.z * uWaresFrequency);
+
+	elevation *= uScale;
+
+	vElevation = elevation;
+
+	modelPosition.y += elevation;
+
+	gl_Position = projectionMatrix * viewMatrix * modelPosition;
 }
