0f2368bdb1d7f62af5d7acfbbb12f599f6fc8312
dezhizhang <1541609448@qq.com>
2024-03-06 20:19:45 +0800

    feat: 曲线动画

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,23 +1,13 @@
-/*
- * :file description: 
- * :name: /threejs/src/index.js
- * :author: 张德志
- * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
- * :last editor: 张德志
- * :date last edited: 2024-03-06 19:52:43
- */
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-//创建场影
+const clock = new THREE.Clock();
+
+const textureLoader = new THREE.TextureLoader();
+
+
 const scene = new THREE.Scene();
-
-//创建相机
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,200);
-
-// 设置相机位置
 camera.position.set(0,0,10);
 scene.add(camera);
 
@@ -26,33 +16,45 @@ const curve = new THREE.CatmullRomCurve3([
   new THREE.Vector3(-5,5,5),
   new THREE.Vector3(0,0,0),
   new THREE.Vector3(5,-5,5),
-  new THREE.Vector3(10,0,10),
+  new THREE.Vector3(10,0,10)
 ]);
+
+scene.add(new THREE.AmbientLight(0xffffff,3));
 
 const points = curve.getPoints(50);
 const geometry = new THREE.BufferGeometry().setFromPoints(points);
 const material = new THREE.LineBasicMaterial({color:0xff0000});
-const curveObject = new THREE.Line(geometry,material);
+const curveObject = new THREE.LineLoop(geometry,material);
 scene.add(curveObject);
 
 
-// 初始化渲染器
-const renderer = new THREE.WebGL1Renderer();
+const moonGeometry = new THREE.SphereGeometry(0.75, 16, 16);
+const moonMaterial = new THREE.MeshPhongMaterial({
+  shininess: 5,
+  map: textureLoader.load('https://threejs.org/examples/textures/planets/moon_1024.jpg'),
+});
+moonMaterial.map.colorSpace = THREE.SRGBColorSpace;
+const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+scene.add(moon);
 
+const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls(camera,renderer.domElement);
 
-
-
-// 设置渲染器大小
 renderer.setSize(window.innerWidth,window.innerHeight);
-
-document.body.append(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
 function render() {
-  requestAnimationFrame(render)
+  requestAnimationFrame(render);
+  const elapsed = clock.getElapsedTime();
+
+  const time = elapsed % 1 * 0.1;
+  const point = curve.getPoint(time);
+  moon.position.copy(point);
   renderer.render(scene,camera);
 }
+
 render();
+
 
 
 
