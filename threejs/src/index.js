d23c4567c8f31b4467e96b3a69aa08a78bf0bce6
dezhizhang <1541609448@qq.com>
2024-03-10 19:24:26 +0800

    feat:物体的清除

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,56 +1,31 @@
-
 import * as THREE from 'three';
-import dat from 'dat.gui';
+import Stat from 'stats.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
 camera.position.set(1,1,10);
-scene.add(camera);
 
 const ambientLight = new THREE.AmbientLight(0xffffff,3);
 scene.add(ambientLight);
 
 const rgbLoader = new RGBELoader();
 rgbLoader.load('/Alex_Hart-Nature_Lab_Bones_2k.hdr',(envMap) => {
-  envMap.mapping = THREE.EquirectangularRefractionMapping;
+  envMap.mapping = THREE.EquirectangularReflectionMapping;
   scene.environment = envMap;
   scene.background = envMap;
-
 });
+
+const stats = new Stat();
+document.body.appendChild(stats.domElement);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
-
-const textureLoader = new THREE.TextureLoader();
-const textureMap = textureLoader.load('/colors.png');
-
-const sphereGeometry = new THREE.SphereGeometry(1,32,32);
-
-const sphereMaterial = new THREE.MeshPhysicalMaterial({
-  color:0xffffff,
-  roughness:0.05,
-  transmission:1,
-  thickness:0.1,
-  iridescence:1,
-  reflectivity:1,
-  iridescenceThicknessMap:textureMap,
-  iridescenceThicknessRange:[200,600],
- 
-});
-
-const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
-scene.add(sphere);
-
-const gui = new dat.GUI();
-gui.add(sphereMaterial,'iridescence',0,1).name('彩虹色');
-gui.add(sphereMaterial,'reflectivity',0,1).name('反射痃');
 
 const control = new OrbitControls(camera,renderer.domElement);
 
@@ -62,8 +37,22 @@ window.addEventListener('resize',() => {
 });
 
 function render() {
+  const sphereGeometry = new THREE.SphereGeometry(2,Math.random() * 64,Math.random() * 64);
+  const sphereMaterial = new THREE.MeshBasicMaterial({
+    color:Math.random() * 0xfffff
+  });
+  const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+  scene.add(sphere);
+
+  control.update();
+
   requestAnimationFrame(render);
   renderer.render(scene,camera);
+
+  sphereGeometry.dispose();
+  sphereMaterial.dispose();
+
+  scene.remove(sphere);
 }
 
 render();
