48618f157dbf7b6c172b98de0ae78f7b04e6fa5a
dezhizhang <1541609448@qq.com>
2024-03-10 13:54:00 +0800

    feat: 标准材质

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,67 +5,53 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-04 22:01:21
  * :last editor: 张德志
- * :date last edited: 2024-03-10 11:55:11
+ * :date last edited: 2024-03-10 13:51:43
  */
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
-//创建场影
+import  {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+
 const scene = new THREE.Scene();
-
-//创建相机
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// 设置相机位置
-camera.position.set(1, 1, 10);
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.set(1,1,10);
 scene.add(camera);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 3);
+const ambientLight = new THREE.AmbientLight(0xffffff,3);
 scene.add(ambientLight);
 
 const rgbLoader = new RGBELoader();
-rgbLoader.load('/Alex_Hart-Nature_Lab_Bones_2k.hdr', (envMap) => {
+rgbLoader.load('/Alex_Hart-Nature_Lab_Bones_2k.hdr',(envMap) => {
   envMap.mapping = THREE.EquirectangularRefractionMapping;
   scene.environment = envMap;
   scene.background = envMap;
 
   const gltfLoader = new GLTFLoader();
-  gltfLoader.load('/Duck.glb', (gltf) => {
+  gltfLoader.load('/sword.glb',(gltf) => {
     scene.add(gltf.scene);
 
-    const duckMesh = gltf.scene.getObjectByName('LOD3spShape');
-    const preMaterial = duckMesh.material;
-    duckMesh.material = new THREE.MeshPhongMaterial({
-      color: 0xffffff,
-      map: preMaterial.map,
-      refractionRatio: 0.7,
-      reflectivity: 0.99,
-      envMap: envMap,
-    });
-  });
+    const mesh = gltf.scene.getObjectByName('Pommeau_Plane001');
+    mesh.material.aoMap = envMap;
+    
+  })
 });
 
-// 初始化渲染器
-const renderer = new THREE.WebGL1Renderer();
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// 设置渲染器大小
-renderer.setSize(window.innerWidth, window.innerHeight);
+const controls = new OrbitControls(camera,renderer.domElement);
 
-document.body.append(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-window.addEventListener('resize', () => {
+window.addEventListener('resize',() => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth,window.innerHeight);
 });
 
 function render() {
   requestAnimationFrame(render);
-  renderer.render(scene, camera);
+  renderer.render(scene,camera);
 }
 
 render();
