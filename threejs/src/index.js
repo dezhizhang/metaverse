b41408577b100f611363b30cbc8f43cb95371e1c
dezhizhang <1541609448@qq.com>
2024-03-12 06:39:24 +0800

    feat:anisotropy

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -3,7 +3,7 @@ import dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff);
+// scene.background = new THREE.Color(0xffffff);
 
 const camera  = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
 camera.position.set(2,10,2);
@@ -19,7 +19,7 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera,renderer.domElement);
 
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('/rain.png');
+const texture = textureLoader.load('/brick/brick_diffuse.jpg');
 
 const planeGeometry = new THREE.PlaneGeometry(1,1);
 const planeMaterial = new THREE.MeshBasicMaterial({
@@ -31,8 +31,15 @@ const planeMaterial = new THREE.MeshBasicMaterial({
 const plane = new THREE.Mesh(planeGeometry,planeMaterial);
 scene.add(plane);
 
-texture.flipY = false;
-texture.premultiplyAlpha = true;
+// texture.flipY = false;
+// texture.premultiplyAlpha = true;
+
+texture.minFilter = THREE.LinearFilter;
+texture.colorSpace = THREE.SRGBColorSpace;
+texture.minFilter = THREE.LinearMipMapNearestFilter;
+texture.anisotropy = 4;
+
+
 
 const gui = new dat.GUI();
 gui.add(texture,'premultiplyAlpha').name('premultiplyAlpha').onChange(() => {
