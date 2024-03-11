f8cc244050bb2a4b58676b5384f44f98b4046200
dezhizhang <1541609448@qq.com>
2024-03-11 22:43:33 +0800

    feat:premultiplyAlpha

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,55 +1,58 @@
 import * as THREE from 'three';
+import dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(2, 10, 2);
+const camera  = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.set(2,10,2);
 
-const renderer = new THREE.WebGL1Renderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+const renderer = new THREE.WebGLRenderer({
+  antialias:true
+});
+renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+
 document.body.appendChild(renderer.domElement);
 
-const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('/watercover/CityNewYork002_COL_VAR1_1K.png')
+const controls = new OrbitControls(camera,renderer.domElement);
 
-const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load('/rain.png');
+
+const planeGeometry = new THREE.PlaneGeometry(1,1);
 const planeMaterial = new THREE.MeshBasicMaterial({
-  // color: 0xff00ff,
   side:THREE.DoubleSide,
-  map:texture
+  map:texture,
+  transparent:true,
 });
+
 const plane = new THREE.Mesh(planeGeometry,planeMaterial);
 scene.add(plane);
-texture.offset.set(0.5,0.5);
 
-texture.repeat.set(4,4);
-// texture.wrapS = THREE.RepeatWrapping;
-texture.wrapS = THREE.MirroredRepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.center.set(0.5,0.5);
-texture.rotation = Math.PI / 4;
+texture.flipY = false;
+texture.premultiplyAlpha = true;
 
+const gui = new dat.GUI();
+gui.add(texture,'premultiplyAlpha').name('premultiplyAlpha').onChange(() => {
+  texture.needsUpdate = true;
+});
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
+scene.add(new THREE.AxesHelper(5));
 
 window.addEventListener('resize',() => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth,window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
-})
+});
 
-
-
-const control = new OrbitControls(camera, renderer.domElement);
 
 function render() {
   requestAnimationFrame(render);
-  renderer.render(scene, camera);
+  renderer.render(scene,camera);
 }
 
 render();
+
