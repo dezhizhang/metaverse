4b066ff753ce198cfba606e668bd708309a96d35
dezhizhang <1541609448@qq.com>
2024-03-12 23:05:22 +0800

    feat:update

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,64 +1,84 @@
- import * as THREE from 'three';
+/*
+ * :file description:
+ * :name: /threejs/src/index.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-03-04 22:01:21
+ * :last editor: 张德志
+ * :date last edited: 2024-03-12 23:01:25
+ */
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
-camera.position.set(2,10,2);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+camera.position.set(2, 10, 2);
 
 const renderer = new THREE.WebGL1Renderer({
-  antialias:true
+  antialias: true,
 });
-renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
-
 document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera,renderer.domElement);
-
-const ktx2loader = new KTX2Loader();
-ktx2loader.setTranscoderPath('/basis/').detectSupport(renderer);
-
-ktx2loader.load('/ktx2/Alex_Hart-Nature_Lab_Bones_2k_etc1s-nomip.ktx2',(envMap) => {
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load('/Alex_Hart-Nature_Lab_Bones_2k.hdr', (envMap) => {
+  envMap.mapping = THREE.EquirectangularReflectionMapping;
   scene.environment = envMap;
   scene.background = envMap;
 });
 
+// const DRACOLoader
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/draco/');
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+
+gltfLoader.load('/cup.glb', (gltf) => {
+  console.log(gltf);
+
+  const cup = gltf.scene.getObjectByName('copo_low_01_vidro_0');
+  const water = gltf.scene.getObjectByName('copo_low_02_agua_0');
+  const ice = gltf.scene.getObjectByName('copo_low_04_vidro_0');
+
+  ice.scale.set(0.86,0.86,0.86);
+  water.position.z = -1;
+  ice.renderOrder = 1;
+  water.renderOrder = 2;
+  cup.renderOrder = 3;
+
+  cup.visible = false;
+  
+  
+
+  console.log(cup);
+  
+
+  scene.add(gltf.scene);
+
+
+});
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
 window.addEventListener('resize',() => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth,window.innerHeight);
-});
-
-const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('/brick/brick_diffuse.jpg');
-
-const planeGeometry = new THREE.PlaneGeometry(1,1,64,64);
-const planeMaterial = new THREE.MeshBasicMaterial({
-  side:THREE.DoubleSide,
-  map:texture,
-  transparent:true
-});
-
-const plane = new THREE.Mesh(planeGeometry,planeMaterial);
-scene.add(plane);
-
-texture.colorSpace = THREE.SRGBColorSpace;
-texture.magFilter = THREE.LinearFilter;
-
-texture.anisotropy = 4;
+  renderer.setPixelRatio(window.devicePixelRatio);
+})
 
 function render() {
+  controls.update();
+  renderer.render(scene, camera);
   requestAnimationFrame(render);
-  renderer.render(scene,camera);
 }
 
 render();
-
-
-
-
-
