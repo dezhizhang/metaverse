c94ea804a5e506c7685dc4b8b83332e4f7431dcc
dezhizhang <1541609448@qq.com>
2024-03-15 07:07:26 +0800

    feat: 动画

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,18 +5,24 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-04 22:01:21
  * :last editor: 张德志
- * :date last edited: 2024-03-12 23:01:25
+ * :date last edited: 2024-03-15 07:05:35
  */
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+const clock = new THREE.Clock();
+
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(2, 10, 2);
+
+scene.background = new THREE.Color(0xffffff);
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0, 0, 10);
 
 const renderer = new THREE.WebGL1Renderer({
   antialias: true,
@@ -26,45 +32,51 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 document.body.appendChild(renderer.domElement);
 
-const rgbeLoader = new RGBELoader();
-rgbeLoader.load('/Alex_Hart-Nature_Lab_Bones_2k.hdr', (envMap) => {
-  envMap.mapping = THREE.EquirectangularReflectionMapping;
-  scene.environment = envMap;
-  scene.background = envMap;
-});
 
-// const DRACOLoader
+const ambientLight = new THREE.AmbientLight(0xffffff,1);
+scene.add(ambientLight);
 
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath('/draco/');
+const directionalLight = new THREE.DirectionalLight(0xfffff,1);
+directionalLight.position.set(1,2,5);
+scene.add(directionalLight);
+
+
+const pointLight = new THREE.PointLight(0xfffff,1);
+scene.add(pointLight);
+
+
+// scene.add(new THREE.DirectionalLightHelper(directionalLight))
+
+
+
+
+const axeshelper = new THREE.AxesHelper(5);
+scene.add(axeshelper);
 
 const gltfLoader = new GLTFLoader();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/draco/');
 gltfLoader.setDRACOLoader(dracoLoader);
 
-gltfLoader.load('/cup.glb', (gltf) => {
+let mixer = null;
+
+gltfLoader.load('/huawei.glb',(gltf) => {
   console.log(gltf);
-
-  const cup = gltf.scene.getObjectByName('copo_low_01_vidro_0');
-  const water = gltf.scene.getObjectByName('copo_low_02_agua_0');
-  const ice = gltf.scene.getObjectByName('copo_low_04_vidro_0');
-
-  ice.scale.set(0.86,0.86,0.86);
-  water.position.z = -1;
-  ice.renderOrder = 1;
-  water.renderOrder = 2;
-  cup.renderOrder = 3;
-
-  cup.visible = false;
-  
-  
-
-  console.log(cup);
-  
-
   scene.add(gltf.scene);
 
+  // 创建动画混合器
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  // 获取动画
+  const action = mixer.clipAction(gltf.animations[0]); 
+  // 播放动画
+  action.play();
 
-});
+
+  
+})
+
+
+
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -76,8 +88,13 @@ window.addEventListener('resize',() => {
 })
 
 function render() {
+
+  const delta = clock.getDelta();
   controls.update();
   renderer.render(scene, camera);
+  if(mixer) {
+    mixer.update(delta);
+  }
   requestAnimationFrame(render);
 }
 
