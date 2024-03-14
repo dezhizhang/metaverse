f6b43a8cb92ff3e27fd020aaad3f9112a58a006f
dezhizhang <1541609448@qq.com>
2024-03-14 22:36:30 +0800

    feat:update

---
+++ showings/src/App.js
@@ -0,0 +1,17 @@
+/*
+ * :file description: 
+ * :name: /showings/src/App.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-03-14 22:25:31
+ * :last editor: 张德志
+ * :date last edited: 2024-03-14 22:35:19
+ */
+import React from "react";
+import * as THREE from 'three';
+
+function App() {
+  return <div>hello</div>
+}
+
+export default App;
