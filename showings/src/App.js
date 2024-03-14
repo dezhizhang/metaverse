54ff432cf3f13cf4223e4b15e834ca5d98fd518e
dezhizhang <1541609448@qq.com>
2024-03-15 05:44:01 +0800

    feat:update

--- showings/src/App.js
+++ showings/src/App.js
@@ -5,16 +5,16 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-14 22:25:31
  * :last editor: 张德志
- * :date last edited: 2024-03-14 23:19:59
+ * :date last edited: 2024-03-15 05:41:09
  */
 import React, { useEffect } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
 
 function App() {
 
-  
   function init() {
     const scene = new THREE.Scene();
     // scene.background = new THREE.Color(0xff00ff);
@@ -24,27 +24,67 @@ function App() {
       0.1,
       1000,
     );
-    camera.position.set(0, 0, 0);
+    camera.position.set(0,0,0);
+    scene.add(camera);
+
+  
+    const boxGeometry = new THREE.BoxGeometry(10,10,10);
+  
+    boxGeometry.scale(1,1,-1);
+    const textureLoader = new THREE.TextureLoader();
+
+    const textureUrl = ' https://tugua.oss-cn-hangzhou.aliyuncs.com/'
+
+    const textureIndex = 0;
+
+    const textureArr = [
+      `${textureIndex}_l`,
+      `${textureIndex}_r`,
+      `${textureIndex}_u`,
+      `${textureIndex}_d`,
+      `${textureIndex}_b`,
+      `${textureIndex}_f`,
+    ];
+
+    const boxMaterial = [];
 
 
-    const boxGeometry = new THREE.BoxGeometry(1,1,1);
-    // boxGeometry.scale(1,1,-1);
-    const boxMaterial = new THREE.MeshBasicMaterial({
-      color:0xff00ff
-    });
-    const box = new THREE.Mesh(boxGeometry,boxMaterial)
-    scene.add(box);
+     textureArr.forEach((item) => {
+      const texture = textureLoader.load(textureUrl + item + '.jpg');
+      console.log('texture',texture);
+
+      boxMaterial.push(new THREE.MeshBasicMaterial({
+        map:texture
+      }))
+    
+     });
+
+     const box = new THREE.Mesh(boxGeometry,boxMaterial)
+     scene.add(box);
+
+
+
+
+
+
+
 
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+
     document.getElementById('container').appendChild(renderer.domElement)
 
     const axesHelper = new THREE.AxesHelper(5);
     scene.add(axesHelper);
 
     function render() {
+      controls.update();
+
       requestAnimationFrame(render);
       renderer.render(scene,camera);
     }
