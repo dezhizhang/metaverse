1967af8086eaf2dccf64cc50d6da4ecb414ec001
dezhizhang <1541609448@qq.com>
2024-03-18 23:02:57 +0800

    feat: 生成3D地图

--- threejs/project/china.js
+++ threejs/project/china.js
@@ -1,11 +1,20 @@
 /*
+ * :file description: 
+ * :name: /threejs/project/china.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-03-18 22:47:29
+ * :last editor: 张德志
+ * :date last edited: 2024-03-18 22:47:30
+ */
+/*
  * :file description:
  * :name: /threejs/src/index.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-03-18 22:55:53
+ * :date last edited: 2024-03-18 22:37:31
  */
 
 import * as THREE from 'three';
