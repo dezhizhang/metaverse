45be511546d54d55342cb1305e6ea91ed6d0d789
dezhizhang <1541609448@qq.com>
2024-03-19 05:08:43 +0800

    feat: 优化渲染

--- smart-park/src/App.js
+++ smart-park/src/App.js
@@ -1,11 +1,11 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /smart-park/src/App.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-19 04:38:30
  * :last editor: 张德志
- * :date last edited: 2024-03-19 04:56:48
+ * :date last edited: 2024-03-19 05:06:31
  */
 import * as THREE from 'three';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
@@ -28,39 +28,40 @@ function App() {
     );
     camera.position.set(1000, 1000, 1000);
 
-    const renderer = new THREE.WebGL1Renderer();
+    const renderer = new THREE.WebGL1Renderer({
+      antialias: true,
+      logarithmicDepthBuffer: true,
+      physicallCorrectLights: true,
+    });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     renderer.toneMappingExposure = 1.5;
-    
+
     container.appendChild(renderer.domElement);
 
     // 加载环境贴图
     const hdrLoader = new RGBELoader();
-    hdrLoader.load('/textures/023.hdr',(envMap) => {
+    hdrLoader.load('/textures/023.hdr', (envMap) => {
       scene.background = envMap;
       scene.environment = envMap;
       scene.environment.mapping = THREE.EquirectangularReflectionMapping;
-      
     });
 
-    const controls = new OrbitControls(camera,renderer.domElement);
-    
+    // 设置环境贴图
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    scene.add(directionalLight);
 
-
+    const controls = new OrbitControls(camera, renderer.domElement);
 
     const gltfLoader = new GLTFLoader();
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath('/draco/');
     gltfLoader.setDRACOLoader(dracoLoader);
 
-
-    gltfLoader.load('/model/city4.glb',(gltf) => {
+    gltfLoader.load('/model/city.glb', (gltf) => {
       scene.add(gltf.scene);
-
-    })
-
+    });
 
     window.addEventListener('resize', () => {
       camera.aspect = window.innerWidth / window.innerHeight;
