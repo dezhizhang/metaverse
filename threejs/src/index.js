6bb9a7df1bc795a36b2347455cd29eaa104ccab8
dezhizhang <1541609448@qq.com>
2024-03-20 04:52:22 +0800

    ftr: 向量的方法

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-03-19 06:35:04
+ * :date last edited: 2024-03-20 04:51:52
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -15,7 +15,6 @@ const scene = new THREE.Scene();
 
 //创建相机
 const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,0.1,1000);
-// camera.position.set(292,223,185);
 camera.position.set(0,0,400);
 camera.lookAt(0,0,0);
 
@@ -25,29 +24,34 @@ const material = new THREE.MeshBasicMaterial({
   color:0x00ffff
 });
 
+const sphere1 = new THREE.Mesh(geometry,material);
+scene.add(sphere1);
 
 
-const R = 100;
-const N = 36;
-const sp = Math.PI  * 2 / N;
-const group = new THREE.Group();
+const sphere2 = new THREE.Mesh(geometry,material);
+scene.add(sphere2);
 
-for(let i=0;i < N + 1;i++) {
-  const angle = sp * i;
-  const x = R * Math.cos(angle);
-  const y = R * Math.sin(angle);
 
-  const mesh = new THREE.Mesh(geometry,material);
-  mesh.position.set(x,y,0);
-  group.add(mesh)
-}
+const A = new THREE.Vector3(30,30,0);
 
-scene.add(group);
+const v = new THREE.Vector3(1,1,0);
+const walk = v.clone().multiplyScalar(50);
+
+
+const B = A.clone().add(walk);
+
+sphere1.position.copy(A);
+sphere2.position.copy(B);
+
+
+
+
+
 
 // 初始化渲染器
 const renderer = new THREE.WebGL1Renderer();
 
-// 设置渲染器大小
+// // 设置渲染器大小
 renderer.setSize(window.innerWidth,window.innerHeight);
 
 document.body.append(renderer.domElement);
