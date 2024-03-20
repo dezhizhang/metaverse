1d115983eb6de20b6fea0351424cd5ec79221855
dezhizhang <1541609448@qq.com>
2024-03-20 22:42:20 +0800

    feat: 几何体法线

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-03-20 07:27:01
+ * :date last edited: 2024-03-20 22:41:00
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -13,36 +13,27 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 //创建场影
 const scene = new THREE.Scene();
 
-//创建相机
 const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,0.1,1000);
 camera.position.set(202,123,125);
 camera.lookAt(0,0,0);
 
+const geometry = new THREE.BoxGeometry(10,10,10);
+const material = new THREE.MeshBasicMaterial({
+  color:0x00ffff,
+});
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
 
-const A = new THREE.Vector3(0,30,0);
-const B = new THREE.Vector3(80,0,0);
+const p = mesh.geometry.attributes.position; // 顶点位置
+const n = mesh.geometry.attributes.normal; // 顶点法线
 
-const sphere1 = createMesh(0xff00ff,2);
-sphere1.position.copy(A);
-scene.add(sphere1);
+for(let i=0; i < p.count;i++) {
+  const m = new THREE.Vector3(p.getX(i),p.getY(i),p.getZ(i));
+  const dir = new THREE.Vector3(n.getX(i),n.getY(i),n.getZ(i));
 
-const sphere2 = createMesh(0x00ff00,2);
-sphere2.position.copy(B);
-scene.add(sphere2);
-
-const AB = B.clone().sub(A);
-const length = AB.length();
-
-const dir = AB.clone().normalize();
-const arrow = new THREE.ArrowHelper(dir,A,length);
-scene.add(arrow);
-
-function createMesh(color,r) {
-  const sphereGeometry = new THREE.SphereGeometry(r);
-  const sphereMaterial = new THREE.MeshBasicMaterial({
-    color:color
-  });
-  return new THREE.Mesh(sphereGeometry,sphereMaterial);
+  const arrowHelper = new THREE.ArrowHelper(dir,m,10);
+  mesh.add(arrowHelper);
+  
 }
 
 
