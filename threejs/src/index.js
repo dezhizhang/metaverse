1c21103e0e1726f8b0f4ae5135d8f37ffa66bd56
dezhizhang <1541609448@qq.com>
2024-03-23 21:19:05 +0800

    feat:Color

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,14 +5,14 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-03-23 20:38:43
+ * :date last edited: 2024-03-23 21:16:14
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(200, 200, 200);
+camera.position.set(10, 10, 10);
 camera.lookAt(0, 0, 0);
 
 const gridHelper = new THREE.GridHelper(30, 25, 0x004444, 0x004444);
@@ -43,19 +43,18 @@ scene.add(axesHelper);
 
 const controls = new OrbitControls(camera,renderer.domElement);
 
-const planeGeometry = new THREE.PlaneGeometry(100,100);
-
-const planeMaterial = new THREE.MeshBasicMaterial({
-  color: 0x00ffff,
-  side: THREE.DoubleSide,
+const boxGeometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({
+  color:0xff00ff
 });
 
-const euler = new THREE.Euler(Math.PI / 2,0,0);
+const color = new THREE.Color(0x00ff00);
+material.color = color;
 
-const mesh = new THREE.Mesh(planeGeometry,planeMaterial);
-mesh.rotation.set(euler.x,euler.y,euler.z);
+const cube = new THREE.Mesh(boxGeometry,material);
+scene.add(cube);
 
-scene.add(mesh);
+
 
 
 
