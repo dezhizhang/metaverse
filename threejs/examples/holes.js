a030986157302c260f6da4117ef3efb1e0c2b67f
dezhizhang <1541609448@qq.com>
2024-03-28 20:59:34 +0800

    feat:holes

--- threejs/examples/holes.js
+++ threejs/examples/holes.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/holes.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-03-13 22:44:48
+ * :date created: 2024-03-28 20:57:11
  * :last editor: 张德志
- * :date last edited: 2024-03-28 20:57:32
+ * :date last edited: 2024-03-28 20:57:11
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
