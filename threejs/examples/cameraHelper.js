8cec67c819487f05ef21227d82402037b0579cc2
dezhizhang <1541609448@qq.com>
2024-03-29 21:56:40 +0800

    feat:camerahelper

--- threejs/examples/cameraHelper.js
+++ threejs/examples/cameraHelper.js
@@ -1,20 +1,11 @@
 /*
- * :file description: 
- * :name: /threejs/examples/spotLight.js
- * :author: 张德志
- * :copyright: (c) 2024, Tungee
- * :date created: 2024-03-29 20:38:58
- * :last editor: 张德志
- * :date last edited: 2024-03-29 20:51:12
- */
-/*
  * :file description:
  * :name: /threejs/src/index.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-03-29 20:38:09
+ * :date last edited: 2024-03-29 21:20:01
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -30,7 +21,7 @@ const k = width / height;
 const s = 1.5;
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(20, 20, 20);
+camera.position.set(200, 200, 200);
 camera.lookAt(0, 0, 0);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
@@ -38,10 +29,20 @@ directionalLight.position.set(100, 60, 50);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
 
+
+directionalLight.shadow.camera.left = -50;
+directionalLight.shadow.camera.right = 50;
+directionalLight.shadow.camera.top = 200;
+directionalLight.shadow.camera.bottom = -100;
+directionalLight.shadow.camera.near = 0.5;
+directionalLight.shadow.camera.far = 600;
+
+
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -51,30 +52,29 @@ const gltfLoader = new GLTFLoader
 dracoLoader.setDecoderPath('/draco/');
 gltfLoader.setDRACOLoader(dracoLoader);
 
-
-
-const spotLight = new THREE.SpotLight(0xffffff,1.0);
-spotLight.angle = Math.PI / 6;
-spotLight.position.set(4,4,4);
-spotLight.target.position.set(0,0,0);
-scene.add(spotLight);
-
-scene.add(new THREE.PointLightHelper(spotLight));
-
-
-
-
-const geometry = new THREE.BoxGeometry(1,1,1);
+const geometry = new THREE.BoxGeometry(50,100,50);
 const material = new THREE.MeshLambertMaterial({
-  color:0x00ff00
+  color:0x00ffff
 });
 const mesh = new THREE.Mesh(geometry,material);
+mesh.position.y = 50;
+mesh.castShadow = true;
 scene.add(mesh);
 
 
+const planeGeometry = new THREE.PlaneGeometry(400,250);
+const planeMaterial = new THREE.MeshLambertMaterial({
+  color:0xcccccc,
+  side:THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry,planeMaterial);
+plane.receiveShadow = true;
+plane.rotateX(Math.PI / 2);
 
+scene.add(plane);
 
-
+const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(cameraHelper);
 
 
 document.body.appendChild(renderer.domElement);
--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-03-29 20:48:17
+ * :date last edited: 2024-03-29 21:50:49
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -21,17 +21,26 @@ const k = width / height;
 const s = 1.5;
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(20, 20, 20);
+camera.position.set(200, 200, 200);
 camera.lookAt(0, 0, 0);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(100, 60, 50);
+const directionalLight = new THREE.DirectionalLight(0xffffff,1);
+directionalLight.position.set(100 * 2,60 * 2,50 * 2);
+directionalLight.castShadow = true;
 scene.add(directionalLight);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+directionalLight.shadow.camera.left = -100;
+directionalLight.shadow.camera.right = 100
+directionalLight.shadow.camera.top = 100;
+directionalLight.shadow.camera.bottom = -100;
+directionalLight.shadow.camera.near = 0.5;
+directionalLight.shadow.camera.far = 600;
+
+const ambientLight = new THREE.AmbientLight(0xffffff,1);
 scene.add(ambientLight);
 
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -41,29 +50,38 @@ const gltfLoader = new GLTFLoader
 dracoLoader.setDecoderPath('/draco/');
 gltfLoader.setDRACOLoader(dracoLoader);
 
-
-
-const spotLight = new THREE.SpotLight(0xfffff,1.0);
-spotLight.angle = Math.PI / 6;
-spotLight.position.set(4,4,4);
-spotLight.target.position.set(0,0,0);
-scene.add(spotLight);
-
-scene.add(new THREE.PointLightHelper(spotLight));
-
-
-
-
-const geometry = new THREE.BoxGeometry(1,1,1);
+const geometry = new THREE.BoxGeometry(50,100,50);
 const material = new THREE.MeshLambertMaterial({
-  color:0x00ff00
+  color:0x00ffff
 });
 const mesh = new THREE.Mesh(geometry,material);
+mesh.position.y = 50;
+mesh.castShadow = true;
 scene.add(mesh);
 
 
+const planeGeometry = new THREE.PlaneGeometry(400,800);
+const planeMaterial = new THREE.MeshLambertMaterial({
+  color:0xcccccc,
+  side:THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry,planeMaterial);
+plane.receiveShadow = true;
+plane.rotateX(Math.PI / 2);
+
+scene.add(plane);
+
+for(let i=-3;i < 4;i++) {
+  const mesh2 = mesh.clone();
+  mesh2.castShadow = true;
+  mesh2.position.z = 100 * i;
+  scene.add(mesh2);
+}
 
 
+
+const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(cameraHelper);
 
 
 
