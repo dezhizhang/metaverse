0518a7948f9fbba2df4ced600d6a675099a05e19
dezhizhang <1541609448@qq.com>
2024-03-31 19:57:56 +0800

    feat: 模型加载优化

--- things/src/scene/factory.js
+++ things/src/scene/factory.js
@@ -5,23 +5,21 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-31 19:27:44
  * :last editor: 张德志
- * :date last edited: 2024-03-31 19:47:23
+ * :date last edited: 2024-03-31 19:54:54
  */
-
+import * as THREE from 'three';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const model = new THREE.Group();
 
 const dracoLoader = new DRACOLoader();
 const gltfLoader = new GLTFLoader();
 dracoLoader.setDecoderPath('/draco/');
 gltfLoader.setDRACOLoader(dracoLoader);
 
-async function factory() {
-  return new Promise((resolve) => {
-    gltfLoader.load('/factory.glb', (gltf) => {
-      resolve(gltf.scene);
-    });
-  });
-}
+gltfLoader.load('/factory.glb',(gltf) => {
+    model.add(gltf.scene);
+})
 
-export { factory };
+export default model;
--- things/src/scene/index.js
+++ things/src/scene/index.js
@@ -5,15 +5,15 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-31 16:54:14
  * :last editor: 张德志
- * :date last edited: 2024-03-31 19:49:34
+ * :date last edited: 2024-03-31 19:55:30
  */
 import * as THREE from 'three';
-import { factory } from './factory';
+import model from './factory';
 
 const scene = new THREE.Scene();
 
-factory().then((gltf) => {
-  scene.add(gltf);
-});
+// 加载模型
+scene.add(model);
+
 
 export default scene;
