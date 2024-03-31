18f462a8f22a46ef5cad98a9a830f8625bd0bd7a
dezhizhang <1541609448@qq.com>
2024-03-31 21:19:47 +0800

    feat:下落的的盒子

--- threejs/examples/下落的的盒子.js
+++ threejs/examples/下落的的盒子.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/下落的的盒子.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-03-13 22:44:48
+ * :date created: 2024-03-31 21:19:19
  * :last editor: 张德志
- * :date last edited: 2024-03-31 21:17:43
+ * :date last edited: 2024-03-31 21:19:20
  */
 import * as THREE from 'three';
 import * as CANNON from 'cannon-es';
