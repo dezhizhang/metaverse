76ed57d3b6d164613e8c5d596b223b48c0c52b47
dezhizhang <1541609448@qq.com>
2024-03-31 16:16:54 +0800

    feat: 最小渲染物体

--- things/src/index.js
+++ things/src/index.js
@@ -5,13 +5,14 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-24 20:16:05
  * :last editor: 张德志
- * :date last edited: 2024-03-24 20:25:31
+ * :date last edited: 2024-03-31 16:16:26
  */
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(200, 200, 200);
+camera.position.set(10, 10, 10);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({
@@ -22,6 +23,20 @@ renderer.setPixelRatio(window.devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
 
+
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({
+  color:0x00ffff
+});
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
+
+const axesHelper = new THREE.AxesHelper(10);
+scene.add(axesHelper);
+
+const controls = new OrbitControls(camera,renderer.domElement);
+
+
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -30,6 +45,7 @@ window.addEventListener('resize', () => {
 });
 
 function render() {
+  controls.update();
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
