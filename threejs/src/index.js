399f4f99f3aeec9b9091e651a3ea93f3d6e41e09
dezhizhang <1541609448@qq.com>
2024-04-02 07:05:02 +0800

    feat: 立体几何体

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-04-02 06:00:39
+ * :date last edited: 2024-04-02 06:27:39
  */
 import * as THREE from 'three';
 import {
@@ -14,9 +14,9 @@ import {
 
 const scene = new THREE.Scene();
 
-// const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-// camera.position.set(300, 300, 200);
-// camera.lookAt(scene.position);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(300, 300, 200);
+camera.lookAt(scene.position);
 
 
 
@@ -31,101 +31,43 @@ document.body.appendChild(renderer.domElement);
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
-const loader = new THREE.FileLoader();
-loader.setResponseType('json');
+const arr = [
+  [0,0],
+  [0,50],
+  [50,50],
+  [50,0],
+  [50,0]
+];
 
-const meshGroup = new THREE.Group();
-const lineGroup = new THREE.Group();
-const mapGroup = new THREE.Group();
-
-lineGroup.position.z = 0.01;
-
-
- loader.load('https://tugua.oss-cn-hangzhou.aliyuncs.com/model/china.json', (data) => {
-  data.features.forEach(function (area) {
-    if (area.geometry.type === 'Polygon') {
-      area.geometry.coordinates = [area.geometry.coordinates]
-    }
-    lineGroup.add(lineLoop(area.geometry.coordinates));
-    meshGroup.add(shapeMesh(area.geometry.coordinates));
-    mapGroup.add(lineGroup, meshGroup);
-  });
+const vector2Arr = [];
+arr.forEach((elem) => {
+  vector2Arr.push(new THREE.Vector2(elem[0],elem[1]));
 });
 
+const material = new THREE.MeshLambertMaterial({
+  color:0x009999,
+  side:THREE.DoubleSide
+});
 
- // 包围盒
- const box3 = new THREE.Box3();
- box3.expandByObject(mapGroup);
+const shape = new THREE.Shape(vector2Arr);
+const geometry = new THREE.ExtrudeGeometry(shape,{
+  depth:10,
+  bevelEnabled:false
+});
 
- const scaleV3 = new THREE.Vector3();
- box3.getSize(scaleV3);
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
 
- const center = new THREE.Vector3();
- box3.getCenter(center);
+const directionalLight1 = new THREE.DirectionalLight(0xffffff,1);
+directionalLight1.position.set(400,200,300);
+scene.add(directionalLight1);
 
- console.log('center',scaleV3);
+const directionalLight2 = new THREE.DirectionalLight(0xffffff,1);
+directionalLight2.position.set(-400,-200,-300);
+scene.add(directionalLight2);
 
+scene.add(directionalLight1,directionalLight2);
 
-
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-const k = width / height;
-const s = 35;
-
-const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
-camera.position.set(104, 35, 200);
-camera.lookAt(104, 35, 0);
-
-
-scene.add(mapGroup);
-
-
-
-function lineLoop(pointArr) {
-
-  const group = new THREE.Group();
-  pointArr.forEach((polygon) => {
-    const pointArr = [];
-    polygon[0].forEach((elem) => {
-      pointArr.push(elem[0], elem[1],0)
-    });
-    group.add(line(pointArr));
-  });
-  return group;
-}
-
-function line(pointArr) {
-  const geometry = new THREE.BufferGeometry();
-  const vertices = new Float32Array(pointArr);
-  const attribue = new THREE.BufferAttribute(vertices, 3);
-  geometry.attributes.position = attribue;
-  const material = new THREE.LineBasicMaterial({
-    color: 0x00cccc
-  });
-  // const line = new THREE.Line(geometry,material);
-  return new THREE.LineLoop(geometry, material);
-
-}
-
-function shapeMesh(pointsArrs) {
-  const shapeArr = [];
-  pointsArrs.forEach((pointsArr) => {
-    const vector2Arr = [];
-    pointsArr[0].forEach((elem) => {
-      vector2Arr.push(new THREE.Vector2(elem[0],elem[1]))
-    });
-    const shape = new THREE.Shape(vector2Arr);
-    shapeArr.push(shape)
-  });
-  const material = new THREE.MeshBasicMaterial({
-    color: 0x002222,//对应陆地颜色
-    side:THREE.DoubleSide,
-  });
-  const geometry = new THREE.ShapeGeometry(shapeArr);
-  const mesh = new THREE.Mesh(geometry,material);
-  return mesh;
-}
 
 
 
