cb0a1d1d85cbbf7b988121d7c0b499abe1893bca
dezhizhang <1541609448@qq.com>
2024-04-01 22:19:05 +0800

    feat: 生成多面体

--- threejs/examples/多面体.js
+++ threejs/examples/多面体.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/多面体.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-03-13 22:44:48
+ * :date created: 2024-04-01 22:18:35
  * :last editor: 张德志
- * :date last edited: 2024-04-01 22:17:58
+ * :date last edited: 2024-04-01 22:18:36
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
