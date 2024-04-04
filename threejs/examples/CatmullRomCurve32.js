6c6a00f75cbf2693865734342103007b2e6c0868
dezhizhang <1541609448@qq.com>
2024-04-04 21:38:49 +0800

    feat:CatmullRomCurve32

--- threejs/examples/CatmullRomCurve32.js
+++ threejs/examples/CatmullRomCurve32.js
@@ -1,13 +1,12 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/CatmullRomCurve32.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-04 21:38:22
  * :last editor: 张德志
- * :date last edited: 2024-04-04 21:36:34
+ * :date last edited: 2024-04-04 21:38:23
  */
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
