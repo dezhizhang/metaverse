be9d828aec5fd44f5460df807c9f93ad4a907118
dezhizhang <1541609448@qq.com>
2024-04-04 22:11:38 +0800

    feat:中国地图实现飞线

--- threejs/examples/CatmullRomCurve32.js
+++ threejs/examples/CatmullRomCurve32.js
@@ -1,41 +1,44 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /threejs/examples/CatmullRomCurve32.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-04-04 21:38:22
+ * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2024-04-04 21:38:23
+ * :date last edited: 2024-04-04 21:45:30
  */
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 //创建场影
 const scene = new THREE.Scene();
 
-//创建相机
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// 设置相机位置
-camera.position.set(200, 200, 200);
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.set(200,200,200);
 camera.lookAt(scene.position);
-scene.add(camera);
 
 const geometry = new THREE.BufferGeometry();
 const curve = new THREE.CatmullRomCurve3([
-  new THREE.Vector3(100, 0, -100),
-  new THREE.Vector3(0, 80, 0),
-  new THREE.Vector3(-100, 0, 100),
+  new THREE.Vector3(100,0,-100),
+  new THREE.Vector3(0,80,0),
+  new THREE.Vector3(-100,0,100),
 ]);
 
 const points = curve.getSpacedPoints(100);
 geometry.setFromPoints(points);
 
 const material = new THREE.LineBasicMaterial({
-  color: 0x00ffff,
+  color:0x00ffff
 });
-
-const line = new THREE.Line(geometry, material);
+const line = new THREE.Line(geometry,material);
 scene.add(line);
+
+const axesHelper = new THREE.AxesHelper(100);
+scene.add(axesHelper);
+
+
+
 
 
 const gridHelper = new THREE.GridHelper(300, 25);
--- threejs/project/中国地图实现飞线.js
+++ threejs/project/中国地图实现飞线.js
@@ -1,12 +1,13 @@
 /*
  * :file description: 
- * :name: /threejs/src/index.js
+ * :name: /threejs/project/中国地图实现飞线.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-04-03 07:02:22
+ * :date created: 2024-04-04 22:06:26
  * :last editor: 张德志
- * :date last edited: 2024-04-04 22:06:09
+ * :date last edited: 2024-04-04 22:11:22
  */
+
 import * as THREE from 'three';
 import {
   OrbitControls
