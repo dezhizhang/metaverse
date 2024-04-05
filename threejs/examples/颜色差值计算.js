848d900dccdb1753fccdc7347d7f3e0b0c194833
dezhizhang <1541609448@qq.com>
2024-04-05 17:51:58 +0800

    feat:颜色差值计算

--- threejs/examples/颜色差值计算.js
+++ threejs/examples/颜色差值计算.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/颜色差值计算.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-03-13 22:44:48
+ * :date created: 2024-04-05 17:51:24
  * :last editor: 张德志
- * :date last edited: 2024-04-05 17:48:49
+ * :date last edited: 2024-04-05 17:51:25
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
