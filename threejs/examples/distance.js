ea7cdef97f2390636d56c52e37f9b8ee19c16906
dezhizhang <1541609448@qq.com>
2024-04-05 21:53:39 +0800

    feat:distance

--- threejs/examples/distance.js
+++ threejs/examples/distance.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/distance.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-03-13 22:44:48
+ * :date created: 2024-04-05 21:53:17
  * :last editor: 张德志
- * :date last edited: 2024-04-05 21:52:30
+ * :date last edited: 2024-04-05 21:53:18
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
