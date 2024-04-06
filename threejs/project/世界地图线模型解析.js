18b49af79c5655f8a218bd85bbf152a739cc9261
dezhizhang <1541609448@qq.com>
2024-04-06 11:45:34 +0800

    feat:世界地图线模型解析

--- threejs/project/世界地图线模型解析.js
+++ threejs/project/世界地图线模型解析.js
@@ -1,12 +1,13 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/project/世界地图线模型解析.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-03-13 22:44:48
+ * :date created: 2024-04-06 11:45:06
  * :last editor: 张德志
- * :date last edited: 2024-04-06 11:44:38
+ * :date last edited: 2024-04-06 11:45:07
  */
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
