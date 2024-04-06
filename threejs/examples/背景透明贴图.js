8412051198011873e92fcfad45a2a4885665a388
dezhizhang <1541609448@qq.com>
2024-04-06 18:56:00 +0800

    feat:背景透明贴图

--- threejs/examples/背景透明贴图.js
+++ threejs/examples/背景透明贴图.js
@@ -1,13 +1,12 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/背景透明贴图.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-06 18:55:22
  * :last editor: 张德志
- * :date last edited: 2024-04-06 18:54:08
+ * :date last edited: 2024-04-06 18:55:23
  */
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
