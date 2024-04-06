74b7ffa8ece95dc1ef5dc459a6482b892358c909
dezhizhang <1541609448@qq.com>
2024-04-06 14:16:37 +0800

    feat:saveFile

--- threejs/examples/saveFile.js
+++ threejs/examples/saveFile.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/saveFile.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-03-13 22:44:48
+ * :date created: 2024-04-06 14:16:07
  * :last editor: 张德志
- * :date last edited: 2024-04-06 14:15:08
+ * :date last edited: 2024-04-06 14:16:08
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
