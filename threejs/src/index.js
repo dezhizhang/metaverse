8a22b28b9a5dc3868002a512862c8b6f913aefde
dezhizhang <1541609448@qq.com>
2024-04-07 11:13:15 +0800

    feat:上海城市数据解析

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -3,9 +3,9 @@
  * :name: /threejs/src/index.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-07 10:08:15
  * :last editor: 张德志
- * :date last edited: 2024-04-07 10:05:35
+ * :date last edited: 2024-04-07 11:12:08
  */
 
 import * as THREE from 'three';
@@ -16,18 +16,27 @@ import { lon2xy } from './math';
 const scene = new THREE.Scene();
 
 //创建相机
-const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 5000000);
+const camera = new THREE.PerspectiveCamera(30,window.innerWidth / window.innerHeight,1,50000);
+const E = 121.49526536464691;
+const N = 31.24189350905988;
+const xy = lon2xy(E,N);
+camera.position.set(xy.x + 5000,xy.y +  5000,5000);
+camera.lookAt(xy.x,xy.y,0);
 
-const E = 121.4952;
-const N = 31.24189;
 
-const { x, y } = lon2xy(E, N);
-
-camera.position.set(x + 5000, y + 5000, 5000);
-camera.lookAt(x, y, 0);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
+
+
+const directionalLigh1 = new THREE.DirectionalLight(0xffffff,1);
+directionalLigh1.position.set(200,400,300);
+scene.add(directionalLigh1);
+
+const directionalLight2 = new THREE.DirectionalLight(0xffffff,1);
+directionalLight2.position.set(-200,-400,300);
+scene.add(directionalLight2);
+
 
 const modelGroup = new THREE.Group();
 
@@ -40,28 +49,53 @@ loader.load('/上海外滩.json', (data) => {
       if (build.geometry.type === 'Polygon') {
         build.geometry.coordinates = [build.geometry.coordinates];
       }
-      buildingGroup.add(shapeMesh(build.geometry.coordinates));
+      const height = build.properties.Floor * 3;
+      buildingGroup.add(ExtrudeMesh(build.geometry.coordinates, height));
     }
   });
   modelGroup.add(buildingGroup);
 });
 
-loader.load('/黄浦江.json',(data) => {
+loader.load('/黄浦江.json', (data) => {
   const riverGroup = new THREE.Group();
   data.features.forEach((build) => {
-    if(build.geometry) {
-      if(build.geometry.type === 'Polygon') {
-        build.geometry.coordinates = [build.geometry.coordinates]
+    if (build.geometry) {
+      if (build.geometry.type === 'Polygon') {
+        build.geometry.coordinates = [build.geometry.coordinates];
       }
       riverGroup.add(shapeMesh(build.geometry.coordinates));
     }
   });
   modelGroup.add(riverGroup);
-
-})
+});
 
 scene.add(modelGroup);
 
+function ExtrudeMesh(pointsArrs, height) {
+  const shapeArr = [];
+  pointsArrs.forEach((pointsArr) => {
+    const vector2Arr = [];
+    pointsArr[0].forEach((elem) => {
+      const xy = lon2xy(elem[0], elem[1]);
+      vector2Arr.push(new THREE.Vector2(xy.x, xy.y));
+    });
+    const shape = new THREE.Shape(vector2Arr);
+    shapeArr.push(shape);
+  });
+
+  
+  const geometry = new THREE.ExtrudeGeometry(shapeArr, {
+    depth: height,
+    bevelEnabled: false,
+  });
+
+  const material = new THREE.MeshLambertMaterial({
+    color: 0x009999,
+    side:THREE.DoubleSide
+  });
+
+  return new THREE.Mesh(geometry, material);
+}
 
 function shapeMesh(pointsArrs) {
   const shapeArr = [];
@@ -78,7 +112,7 @@ function shapeMesh(pointsArrs) {
 
   const geometry = new THREE.ShapeGeometry(shapeArr);
   const material = new THREE.MeshLambertMaterial({
-    color: 0x009999,
+    color: 0x009966,
   });
   return new THREE.Mesh(geometry, material);
 }
@@ -91,12 +125,22 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.append(renderer.domElement);
 
-const axesHelper = new THREE.AxesHelper(100);
+const axesHelper = new THREE.AxesHelper(250);
+axesHelper.position.set(xy.x,xy.y,0);
 scene.add(axesHelper);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(x,y,5000);
+
+const controls = new OrbitControls(camera,renderer.domElement);
+controls.target.set(xy.x,xy.y,0);
 controls.update();
+
+window.addEventListener('resize',() => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
+
 
 function render() {
   requestAnimationFrame(render);
