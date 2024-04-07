f7ab10e8ba1bbfdf4e1937b8e8d36e2712547d75
dezhizhang <1541609448@qq.com>
2024-04-07 13:40:50 +0800

    feat:流光围qian效果

--- threejs/examples/流光围qian效果.js
+++ threejs/examples/流光围qian效果.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/流光围qian效果.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-07 13:40:22
  * :last editor: 张德志
- * :date last edited: 2024-04-07 13:37:31
+ * :date last edited: 2024-04-07 13:40:23
  */
 
 import * as THREE from 'three';
