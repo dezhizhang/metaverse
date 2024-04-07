96ba8a00171d69726525ff5d42e8ba7763223f5b
dezhizhang <1541609448@qq.com>
2024-04-07 11:42:20 +0800

    feat:生成环形带

--- threejs/examples/生成环形带.js
+++ threejs/examples/生成环形带.js
@@ -1,13 +1,12 @@
 /*
  * :file description: 
- * :name: /threejs/src/index.js
+ * :name: /threejs/examples/生成环形带.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-07 11:41:54
  * :last editor: 张德志
- * :date last edited: 2024-04-07 11:41:34
+ * :date last edited: 2024-04-07 11:41:55
  */
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
