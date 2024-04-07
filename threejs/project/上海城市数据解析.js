a0d51146de0399db745df2e986f4fe9b8cd1e176
dezhizhang <1541609448@qq.com>
2024-04-07 10:08:55 +0800

    feat:上海城市数据解析

--- threejs/project/上海城市数据解析.js
+++ threejs/project/上海城市数据解析.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/project/上海城市数据解析.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-07 10:08:15
  * :last editor: 张德志
- * :date last edited: 2024-04-07 10:05:35
+ * :date last edited: 2024-04-07 10:08:16
  */
 
 import * as THREE from 'three';
--- threejs/src/math.js
+++ threejs/src/math.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-02-08 22:54:34
  * :last editor: 张德志
- * :date last edited: 2024-04-06 10:45:46
+ * :date last edited: 2024-04-07 09:20:52
  */
 function lon2xyz(R, longitude, latitude) {
   let lon = (longitude * Math.PI) / 180;
@@ -24,4 +24,17 @@ function lon2xyz(R, longitude, latitude) {
   };
 }
 
-export { lon2xyz };
+// 经纬度转墨卡托
+function lon2xy(longitude, latitude) {
+  const E = longitude;
+  const N = latitude;
+  let x = (E * 20037508.34) / 180;
+  let y = Math.log(Math.tan(((90 + N) * Math.PI) / 360)) / (Math.PI / 180);
+  y = (y * 20037508.34) / 180;
+  return {
+    x: x, //墨卡托x坐标——对应经度
+    y: y, //墨卡托y坐标——对应维度
+  };
+}
+
+export { lon2xyz, lon2xy };
