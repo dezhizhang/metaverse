ecfe687aac10d21fc7bf0614720133e931757067
dezhizhang <1541609448@qq.com>
2024-04-09 06:53:53 +0800

    feat:高光贴图

--- threejs/examples/高光贴图.js
+++ threejs/examples/高光贴图.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/高光贴图.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-09 06:53:25
  * :last editor: 张德志
- * :date last edited: 2024-04-09 06:52:46
+ * :date last edited: 2024-04-09 06:53:26
  */
 
 import * as THREE from 'three';
