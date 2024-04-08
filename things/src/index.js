64aa125c517e13fe545a5fdd1999e87880734fc1
dezhizhang <1541609448@qq.com>
2024-04-08 20:12:34 +0800

    feat: 加载gltf模型

--- things/src/index.js
+++ things/src/index.js
@@ -5,22 +5,52 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-24 20:16:05
  * :last editor: 张德志
- * :date last edited: 2024-03-31 20:34:54
+ * :date last edited: 2024-04-08 20:10:58
  */
-import light from './light';
-import scene from './scene';
-import renderer from './renderer';
-import controls from './controls';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { baseUrl } from '../config';
 
+const scene = new THREE.Scene();
 
-scene.add(light);
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-controls.update();
+// camera
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
+camera.position.set(200, 200, 200);
+camera.lookAt(scene.position);
 
-
+// render
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+renderer.setSize(width, height);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
+// controls
+const controls = new OrbitControls(camera, renderer.domElement);
 
+// light 
+const ambientLight = new THREE.AmbientLight(0xffffff,1.0);
+scene.add(ambientLight)
 
+//loader
+const dracoLoader = new DRACOLoader();
+const gltfLoader = new GLTFLoader();
+dracoLoader.setDecoderPath('/draco/');
+gltfLoader.setDRACOLoader(dracoLoader);
 
+gltfLoader.load(`${baseUrl}/model/factory.glb`, (gltf) => {
+  scene.add(gltf.scene);
+});
 
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+render();
