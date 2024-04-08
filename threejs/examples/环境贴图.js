dfe1e23580e18a3293e5fd6313c85e1345a2c725
dezhizhang <1541609448@qq.com>
2024-04-09 07:10:19 +0800

    feat:环境贴图

--- threejs/examples/环境贴图.js
+++ threejs/examples/环境贴图.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/环境贴图.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-09 07:09:51
  * :last editor: 张德志
- * :date last edited: 2024-04-09 07:08:58
+ * :date last edited: 2024-04-09 07:09:52
  */
 
 import * as THREE from 'three';
