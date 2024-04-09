acad55bfd782f64d00918bc286f2f9336a3b3535
dezhizhang <1541609448@qq.com>
2024-04-09 20:21:26 +0800

    feat:精灵模型解析中国数据

--- threejs/examples/精灵模型解析中国数据.js
+++ threejs/examples/精灵模型解析中国数据.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/精灵模型解析中国数据.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-09 20:18:23
  * :last editor: 张德志
- * :date last edited: 2024-04-09 20:13:05
+ * :date last edited: 2024-04-09 20:18:24
  */
 
 import * as THREE from 'three';
