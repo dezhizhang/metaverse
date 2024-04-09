a037ce0e010dc7b6c637f16c9749f4d1070ecedc
dezhizhang <1541609448@qq.com>
2024-04-09 22:19:15 +0800

    feat: 动画

--- threejs/examples/动画.js
+++ threejs/examples/动画.js
@@ -1,3 +1,12 @@
+/*
+ * :file description: 
+ * :name: /threejs/examples/动画.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-04-09 22:18:20
+ * :last editor: 张德志
+ * :date last edited: 2024-04-09 22:18:21
+ */
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
