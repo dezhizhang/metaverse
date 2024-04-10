6a7ff40b8c902ecdbbaf2e6dc3c96b04353b647b
dezhizhang <1541609448@qq.com>
2024-04-10 21:30:08 +0800

    feat:动画

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,79 +1,82 @@
+/*
+ * :file description: 
+ * :name: /threejs/src/index.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-04-07 13:40:22
+ * :last editor: 张德志
+ * :date last edited: 2024-04-10 21:29:46
+ */
 
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+//创建场影
 const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(300, 200, 200);
+//创建相机
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// 设置相机位置
+camera.position.set(200, 200, 200);
 camera.lookAt(scene.position);
 
-const renderer = new THREE.WebGLRenderer({
-  antialias: true,
-});
-renderer.shadowMap.enabled = true;
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(ambientLight);
-
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(150,150,150);
-scene.add(pointLight);
-
-const group = new THREE.Group();
-
-const geometry = new THREE.BoxGeometry(40,6,6);
+const geometry = new THREE.BoxGeometry(6,40,6);
 const material = new THREE.MeshLambertMaterial({
   color:0x0000ff
 });
-const cube = new THREE.Mesh(geometry,material);
-cube.name = 'Box';
-group.add(cube);
+const mesh = new THREE.Mesh(geometry,material);
+mesh.name = 'Box';
+scene.add(mesh);
 
 
-const geometry1 = new THREE.SphereGeometry(10,25,25);
-const material2 = new THREE.MeshLambertMaterial({
-  color:0xff00ff
-});
-const mesh2 = new THREE.Mesh(geometry1,material2);
-mesh2.name = 'Sphere';
-group.add(mesh2);
-scene.add(group);
+const posTrack = new THREE.KeyframeTrack('Box.position',[0,20],[0,0,0,0,200,0]);
+const clip = new THREE.AnimationClip('default',20,[posTrack]);
 
-
-const times = [0,10];
-const values = [0,0,0,150,0,0];
-
-const posTrack = new THREE.KeyframeTrack('Box.position',times,values);
-const scaleTrack = new THREE.KeyframeTrack('Sphere.scale',[0,20],[1,1,1,3,3,3]);
-
-const duration = 20;
-const clip = new THREE.AnimationClip('default',duration,[posTrack,scaleTrack]);
-
-const mixer = new THREE.AnimationMixer(group);
+const mixer = new THREE.AnimationMixer(mesh);
 const AnimationAction = mixer.clipAction(clip);
 
-AnimationAction.timeScale = 20;
 AnimationAction.play();
+
+scene.add(new THREE.AxesHelper(200));
+
+scene.add(new THREE.AmbientLight(0xffffff,1));
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click',() => {
+
+})
+
+
+
+
+
 
 
 
 const axesHelper = new THREE.AxesHelper(100);
 scene.add(axesHelper);
 
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer();
+
+// 设置渲染器大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.append(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
 
 const clock = new THREE.Clock();
 
 function render() {
-  requestAnimationFrame(render);
-  renderer.render(scene,camera);
   mixer.update(clock.getDelta());
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
 }
 
 render();
