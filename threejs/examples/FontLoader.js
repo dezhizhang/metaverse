7f0617ff9e504c1def49b2b155cda982094b174b
dezhizhang <1541609448@qq.com>
2024-04-13 10:35:50 +0800

    feat: 旋转动画的控制

--- threejs/examples/FontLoader.js
+++ threejs/examples/FontLoader.js
@@ -1,18 +1,17 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /threejs/examples/FontLoader.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-04-12 07:28:23
  * :last editor: 张德志
- * :date last edited: 2024-04-12 07:30:05
+ * :date last edited: 2024-04-12 09:40:58
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-
 
 //创建场影
 const scene = new THREE.Scene();
@@ -21,78 +20,39 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 // 设置相机位置
 camera.position.set(2, -5, 169);
 
-
 camera.lookAt(scene.position);
-
 
 const dracoLoader = new DRACOLoader();
 const gltfLoader = new GLTFLoader();
 dracoLoader.setDecoderPath('/draco/');
 gltfLoader.setDRACOLoader(dracoLoader);
 
-
-gltfLoader.load('/手机.gltf',(gltf) => {
+gltfLoader.load('/手机.gltf', (gltf) => {
   scene.add(gltf.scene);
 });
 
-
-// const R = 60;
-// const geometry = new THREE.BufferGeometry();
-// const arc = new THREE.ArcCurve(0,0,R,Math.PI / 2 + Math.PI / 6,Math.PI / 2 - Math.PI / 6);
-// const points = arc.getPoints(50);
-// geometry.setFromPoints(points);
-
-// const material = new THREE.LineBasicMaterial({
-//   color:0xffffff
-// });
-// const line = new THREE.Line(geometry,material);
-// line.rotateX(-Math.PI / 2);
-// line.position.y = -85;
-// scene.add(line);
-
 const R = 60;
 const geometry = new THREE.BufferGeometry();
-const arc = new THREE.ArcCurve(0,0,R,Math.PI / 2 + Math.PI / 6,Math.PI / 2 - Math.PI / 6);
+const arc = new THREE.ArcCurve(0, 0, R, Math.PI / 2 + Math.PI / 6, Math.PI / 2 - Math.PI / 6);
 const points = arc.getPoints(50);
 geometry.setFromPoints(points);
 
-
 const material = new THREE.LineBasicMaterial({
-  color:0xffffff
+  color: 0xffffff,
 });
-const line = new THREE.Line(geometry,material);
+const line = new THREE.Line(geometry, material);
 line.rotateX(-Math.PI / 2);
 line.position.y = -85;
 scene.add(line);
 
 
-const loader = new FontLoader();
-loader.load('https://threejs.org/examples/fonts/helvetiker_bold.typeface.json',(font) => {
-  const material = new THREE.MeshLambertMaterial({
-    color:0xffffff,
-    side:THREE.DoubleSide,
-  });
-  const shapes = font.generateShapes('abbc',10);
-  const geometry = new THREE.ShapeGeometry(shapes);
-  const text = new THREE.Mesh(geometry,material);
-
-  text.position.z = -R;
-  text.position.x = -12;
-  text.position.y = -85;
-
-  scene.add(text);
-  
-});
-
-
-const directionLight = new THREE.DirectionalLight(0xffffff,0.8);
-directionLight.position.set(0,50,100);
+const directionLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionLight.position.set(0, 50, 100);
 scene.add(directionLight);
 
-const directionLight2 = new THREE.DirectionalLight(0xffffff,0.8);
-directionLight2.position.set(0,50,-100);
+const directionLight2 = new THREE.DirectionalLight(0xffffff, 0.8);
+directionLight2.position.set(0, 50, -100);
 scene.add(directionLight2);
-
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
@@ -111,7 +71,6 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.minDistance = 200;
 controls.maxDistance = 600;
 
-
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -119,9 +78,7 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(window.devicePixelRatio);
 });
 
-
 function render() {
-  
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,11 +1,11 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /threejs/src/index.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-04-12 07:02:27
  * :last editor: 张德志
- * :date last edited: 2024-04-12 07:26:58
+ * :date last edited: 2024-04-13 10:32:48
  */
 
 import * as THREE from 'three';
@@ -14,89 +14,69 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 
-
+let rotate = {
+  bool: true,
+};
 
 //创建场影
 const scene = new THREE.Scene();
 //创建相机
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 // 设置相机位置
-camera.position.set(2, -5, 169);
-
+camera.position.set(-28, -30, -205);
 
 camera.lookAt(scene.position);
-
 
 const dracoLoader = new DRACOLoader();
 const gltfLoader = new GLTFLoader();
 dracoLoader.setDecoderPath('/draco/');
 gltfLoader.setDRACOLoader(dracoLoader);
 
+const model = new THREE.Group();
 
-gltfLoader.load('/手机.gltf',(gltf) => {
-  scene.add(gltf.scene);
+gltfLoader.load('/手机.gltf', (gltf) => {
+  model.add(gltf.scene);
 });
-
-
-// const R = 60;
-// const geometry = new THREE.BufferGeometry();
-// const arc = new THREE.ArcCurve(0,0,R,Math.PI / 2 + Math.PI / 6,Math.PI / 2 - Math.PI / 6);
-// const points = arc.getPoints(50);
-// geometry.setFromPoints(points);
-
-// const material = new THREE.LineBasicMaterial({
-//   color:0xffffff
-// });
-// const line = new THREE.Line(geometry,material);
-// line.rotateX(-Math.PI / 2);
-// line.position.y = -85;
-// scene.add(line);
+scene.add(model);
 
 const R = 60;
 const geometry = new THREE.BufferGeometry();
-const arc = new THREE.ArcCurve(0,0,R,Math.PI / 2 + Math.PI / 6,Math.PI / 2 - Math.PI / 6);
+const arc = new THREE.ArcCurve(0, 0, R, Math.PI / 2 + Math.PI / 6, Math.PI / 2 - Math.PI / 6);
 const points = arc.getPoints(50);
 geometry.setFromPoints(points);
 
-
 const material = new THREE.LineBasicMaterial({
-  color:0xffffff
+  color: 0xffffff,
 });
-const line = new THREE.Line(geometry,material);
+const line = new THREE.Line(geometry, material);
 line.rotateX(-Math.PI / 2);
 line.position.y = -85;
 scene.add(line);
 
-
 const loader = new FontLoader();
-loader.load('https://threejs.org/examples/fonts/helvetiker_bold.typeface.json',(font) => {
+loader.load('https://threejs.org/examples/fonts/helvetiker_bold.typeface.json', (font) => {
   const material = new THREE.MeshLambertMaterial({
-    color:0xffffff,
-    side:THREE.DoubleSide,
+    color: 0xffffff,
+    side: THREE.DoubleSide,
   });
-  const shapes = font.generateShapes('abbc',10);
+  const shapes = font.generateShapes('abbc', 10);
   const geometry = new THREE.ShapeGeometry(shapes);
-  const text = new THREE.Mesh(geometry,material);
+  const text = new THREE.Mesh(geometry, material);
 
   text.position.z = -R;
   text.position.x = -12;
   text.position.y = -85;
 
   scene.add(text);
-  
-})
+});
 
-
-
-
-const directionLight = new THREE.DirectionalLight(0xffffff,0.8);
-directionLight.position.set(0,50,100);
+const directionLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionLight.position.set(0, 50, 100);
 scene.add(directionLight);
 
-const directionLight2 = new THREE.DirectionalLight(0xffffff,0.8);
-directionLight2.position.set(0,50,-100);
+const directionLight2 = new THREE.DirectionalLight(0xffffff, 0.8);
+directionLight2.position.set(0, 50, -100);
 scene.add(directionLight2);
-
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
@@ -115,7 +95,6 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.minDistance = 200;
 controls.maxDistance = 600;
 
-
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -123,9 +102,22 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(window.devicePixelRatio);
 });
 
+const div = document.getElementById('rotate');
+const img = document.getElementById('img');
+
+div.addEventListener('click', () => {
+  if (rotate.bool) {
+    img.src = '/停止旋转.png';
+  } else {
+    img.src = '旋转.png';
+  }
+  rotate.bool = !rotate.bool;
+});
 
 function render() {
-  
+  if (rotate.bool) {
+    model.rotateY(0.01);
+  }
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
