ac9fba6b30089532b11f77915fb6e380158fa646
dezhizhang <1541609448@qq.com>
2024-04-14 15:50:24 +0800

    ftr: 绘制三角形

--- webgpu/src/main.ts
+++ webgpu/src/main.ts
@@ -1,36 +1,111 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /webgpu/src/main.ts
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-07-13 05:31:18
+ * :date created: 2024-04-14 15:44:37
  * :last editor: 张德志
- * :date last edited: 2024-01-29 22:55:23
+ * :date last edited: 2024-04-14 15:49:22
  */
 
-async function initwebgpu() {
-  const adapter =await (navigator.gpu as any).requestAdapter();
-  const device =await adapter?.requestDevice();
+const canvas = document.createElement('canvas');
+canvas.width = 500;
+canvas.height = 500;
 
+document.body.appendChild(canvas);
 
-  const canvas = document.createElement('canvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  canvas.style.background = "#00000";
+async function render() {
+  const adapter: any = await navigator.gpu.requestAdapter();
 
-  const context = canvas.getContext('webgpu');
-  const format  = navigator.gpu.getPreferredCanvasFormat();
+  const device = await adapter.requestDevice();
+  const format = navigator.gpu.getPreferredCanvasFormat();
 
-  context?.configure({
-    device:device,
-    format:format,
-   
+  const ctx: any = canvas.getContext('webgpu');
+  ctx.configure({
+    device,
+    format,
   });
 
-  document.body.appendChild(canvas);
+  // 创建顶点缓冲区数据
+  const vertexArray = new Float32Array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0]);
 
+  const vertexBuffer = device.createBuffer({
+    size: vertexArray.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
 
+  const vertex = /*wgsl*/ `
+   @vertex
+   fn main(@location(0) pos:vec3<f32>) ->@builtin(position) vec4<f32> {
+      var pos2 = vec4<f32>(pos,1.0);
+      pos2.x -= 0.2;
+      return pos2;
+   }
+  `;
+
+  const fragment = /*wgsl*/ `
+  @fragment
+  fn main() ->@location(0) vec4<f32> {
+    return vec4<f32>(1.0,0.0,0.0,1.0);
+  }
+  `;
+
+  //
+  device.queue.writeBuffer(vertexBuffer, 0, vertexArray);
+
+  const pipeline = device.createRenderPipeline({
+    layout: 'auto',
+    vertex: {
+      module: device.createShaderModule({ code: vertex }),
+      entryPoint: 'main',
+      buffers: [
+        {
+          arrayStride: 3 * 4,
+          attributes: [
+            {
+              shaderLocation: 0,
+              format: 'float32x3',
+              offset: 0,
+            },
+          ],
+        },
+      ],
+    },
+    fragment: {
+      module: device.createShaderModule({ code: fragment }),
+      entryPoint: 'main',
+      targets: [
+        {
+          format,
+        },
+      ],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+  });
+
+  const commandEncoder = device.createCommandEncoder();
+  const renderPass = commandEncoder.beginRenderPass({
+    colorAttachments: [
+      {
+        view: ctx.getCurrentTexture().createView(),
+        storeOp: 'store',
+        loadOp: 'clear',
+        clearValue: { r: 0.5, g: 0.5, b: 0.5, a: 1.0 },
+      },
+    ],
+  });
+
+  renderPass.setPipeline(pipeline);
+  renderPass.setVertexBuffer(0, vertexBuffer);
+
+  renderPass.draw(3);
+  renderPass.end();
+
+  const commandBuffer = commandEncoder.finish();
+
+  device.queue.submit([commandBuffer]);
 }
 
-initwebgpu();
-
+render();
