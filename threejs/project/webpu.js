c1f346800410b970fdc928f8dccab1f8aa7e39e8
dezhizhang <1541609448@qq.com>
2024-04-14 15:45:10 +0800

    feat:webpu

--- threejs/project/webpu.js
+++ threejs/project/webpu.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/project/webpu.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-04-14 15:44:37
  * :last editor: 张德志
- * :date last edited: 2024-04-14 15:44:09
+ * :date last edited: 2024-04-14 15:44:38
  */
 
 const canvas = document.createElement('canvas');
