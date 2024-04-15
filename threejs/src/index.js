a7d96541ac758b9d6075ff9c00c192a46ef6f46a
dezhizhang <1541609448@qq.com>
2024-04-16 05:28:02 +0800

    feat:getWorldDirection

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2024-04-15 07:04:51
+ * :date last edited: 2024-04-16 05:26:48
  */
 
 import * as THREE from 'three';
@@ -17,7 +17,7 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
 camera.position.set(0, 1.6, -5.6);
-camera.lookAt(0,1.6,0);
+camera.lookAt(0, 1.6, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -43,7 +43,6 @@ gltfLoader.load('/person.glb', (gltf) => {
 
   const clipAction = mixer.clipAction(gltf.animations[10]);
   clipAction.play();
-
 });
 
 // const controls = new OrbitControls(camera, renderer.domElement);
@@ -62,12 +61,12 @@ const keyStates = {
 window.addEventListener('keydown', (event) => {
   const key = event.key.toUpperCase();
   console.log('ke', key);
-  keyStates[key] = true
+  keyStates[key] = true;
 });
 
 window.addEventListener('keyup', (event) => {
   const key = event.key.toUpperCase();
-  keyStates[key] = false
+  keyStates[key] = false;
 });
 
 window.addEventListener('resize', () => {
@@ -84,32 +83,32 @@ const damping = -0.04;
 
 function render() {
   const deltaTime = clock.getDelta();
-  // 向前运动
-  if (keyStates.W) {
-    const front = new THREE.Vector3(0, 0, 1);
-    v.add(front.multiplyScalar(a * deltaTime));
-
-    if (v.length() < 5) {
+  if (player && mixer) {
+    // 向前运动
+    if (keyStates.W) {
+      const front = new THREE.Vector3(0, 0, 0);
+      player.getWorldDirection(front);
       v.add(front.multiplyScalar(a * deltaTime));
-    }
-    // 向后运动
-  }else if(keyStates.S) {
-    const front = new THREE.Vector3(0, 0, -1);
-    v.add(front.multiplyScalar(a * deltaTime));
 
-    if (v.length() < 5) {
-      v.add(front.multiplyScalar(a * deltaTime));
-    }
-  }
+      if (v.length() < 5) {
+        v.add(front.multiplyScalar(a * deltaTime));
+      }
+      // 向后运动
+    } else if (keyStates.S) {
+      const front = new THREE.Vector3(0, 0, 0);
+      player.getWorldDirection(front);
+      v.add(front.multiplyScalar(-a * deltaTime));
 
-  // v = v + v * damping;
-  v.addScaledVector(v, damping);
-  const deltaPosition = v.clone().multiplyScalar(deltaTime);
-  if(player && mixer) {
+      if (v.length() < 5) {
+        v.add(front.multiplyScalar(-a * deltaTime));
+      }
+    }
+    // v = v + v * damping;
+    v.addScaledVector(v, damping);
+    const deltaPosition = v.clone().multiplyScalar(deltaTime);
     player.position.add(deltaPosition);
     mixer.update(deltaTime);
     player.add(camera);
-    
   }
 
   requestAnimationFrame(render);
