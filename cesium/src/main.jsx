242d6c9c4ba566b42abc0aaf8cf2f740c7fad812
dezhizhang <1541609448@qq.com>
2024-04-20 05:58:15 +0800

    ftr:skyBox天地图

--- cesium/src/main.jsx
+++ cesium/src/main.jsx
@@ -5,10 +5,9 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-27 16:29:41
  * :last editor: 张德志
- * :date last edited: 2024-04-20 05:43:30
+ * :date last edited: 2024-04-20 05:57:46
  */
 import * as Cesium from 'cesium';
-
 
 // const atLayer = new Cesium.UrlTemplateImageryProvider({
 //     url: "https://webst02.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}",
@@ -16,34 +15,37 @@ import * as Cesium from 'cesium';
 //     maximumLevel: 18
 // })
 
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NWVlNTI2MC00YTViLTQzZjYtOGMxNy1lYTAxMDVkMTMwNTQiLCJpZCI6MTA3NjIxLCJpYXQiOjE2NjI3OTY2ODR9.9Amu-saGmeaPMMt9LE5MjF0FQcoC3toDrxCo_J4ItAg';
+Cesium.Ion.defaultAccessToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NWVlNTI2MC00YTViLTQzZjYtOGMxNy1lYTAxMDVkMTMwNTQiLCJpZCI6MTA3NjIxLCJpYXQiOjE2NjI3OTY2ODR9.9Amu-saGmeaPMMt9LE5MjF0FQcoC3toDrxCo_J4ItAg';
 // An example of using a b3dm tileset to classify another b3dm tileset.
-const viewer = new Cesium.Viewer("root",{
+const viewer = new Cesium.Viewer('root', {
   // 是否显示信息窗口
-  infoBox:false,
+  infoBox: false,
   // 是否显示查询按钮
-  geocoder:false,
+  geocoder: false,
   // 不显示home按钮
-  homeButton:false,
+  homeButton: false,
   // 控制查看器显示模式
-  sceneModePicker:false,
+  sceneModePicker: false,
   // 是否显示图层按钮
-  baseLayerPicker:false,
+  baseLayerPicker: false,
   // 是否显不帮助
-  navigationHelpButton:false,
+  navigationHelpButton: false,
   // 是否显示动画
-  animation:false,
+  animation: false,
   // 是否显示全屏按钮
-  timeline:false,
+  timeline: false,
+  skyBox: new Cesium.SkyBox({
+    sources: {
+      positiveX: '/texture/sky/px.jpg',
+      negativeX: '/texture/sky/nx.jpg',
+      positiveY: '/texture/sky/ny.jpg',
+      negativeY: '/texture/sky/py.jpg',
+      positiveZ: '/texture/sky/pz.jpg',
+      negativeZ: '/texture/sky/nz.jpg',
+    },
+  }),
 });
 
-Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
-  89.5,
-  20.4,
-  110.4,
-  61.2,
-)
+Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(89.5, 20.4, 110.4, 61.2);
 
-
-
-console.log(Cesium);
