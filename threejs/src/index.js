c0a2e254668e5013a8363f6b6785191b5f7a24d1
dezhizhang <1541609448@qq.com>
2024-04-22 07:28:13 +0800

    feat: 添加模型

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-04-07 14:25:08
  * :last editor: 张德志
- * :date last edited: 2024-04-22 06:46:49
+ * :date last edited: 2024-04-22 07:27:42
  */
 
 import dat from 'dat.gui';
@@ -24,8 +24,10 @@ import {
   PiecewiseBezier,
   ColorOverLife,
   ColorRange,
+  RandomQuatGenerator,
 } from 'three.quarks';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 //创建场影
 const scene = new THREE.Scene();
@@ -41,12 +43,12 @@ scene.add(camera);
 let batchRenderer = new BatchedParticleRenderer();
 const texture = new THREE.TextureLoader().load('/texture/particle_default.png');
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial({
-  roughness: 0.5,
-  metalness: 0.5,
-  color: 0x00ffff,
-});
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshStandardMaterial({
+//   roughness: 0.5,
+//   metalness: 0.5,
+//   color: 0x00ffff,
+// });
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
@@ -55,36 +57,41 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(10, 10, 10);
 scene.add(directionalLight);
 
-const particles = new ParticleSystem({
-  instancingGeometry: geometry,
-  duration: 5,
-  looping: true,
-  startLife: new IntervalValue(0, 1),
-  // 粒子开始的速度
-  startSpeed: new IntervalValue(0, 10),
-  // 粒子开始的大小
-  startSize: new IntervalValue(0.1, 0.1),
-  worldSpace: true,
-  maxPartices: 1000,
-  emissionOverTime: new ConstantValue(1000),
-  material: material,
-  renderMode: RenderMode.Mesh,
+const gltfLoader = new GLTFLoader();
+gltfLoader.load('/leave.glb', (gltf) => {
+
+  const geometry = gltf.scene.children[0].geometry;
+  const material = gltf.scene.children[0].material;
+  const particles = new ParticleSystem({
+    instancingGeometry: geometry,
+    duration: 5,
+    looping: true,
+    startLife: new IntervalValue(0, 3),
+    // 粒子开始的速度
+    startSpeed: new IntervalValue(0, 2),
+    // 粒子开始的大小
+    startSize: new IntervalValue(0.1, 0.1),
+    startColor: new RandomColor(
+      new THREE.Vector4(1, 0.91, 0.51, 1),
+      new THREE.Vector4(1, 0.44, 0.16, 1),
+    ),
+    startRotation: new RandomQuatGenerator(),
+    worldSpace: true,
+    maxPartices: 100,
+    emissionOverTime: new ConstantValue(1000),
+    material: material,
+    renderMode: RenderMode.Mesh,
+  });
+  // particles.addBehavior(
+  //   new ColorOverLife(
+  //     new ColorRange(new THREE.Vector4(1.0, 0.9, 0.1, 1.0), new THREE.Vector4(1, 0.1, 0.0, 1)),
+  //   ),
+  // );
+
+  particles.emitter.name = 'particles';
+  scene.add(particles.emitter);
+  batchRenderer.addSystem(particles);
 });
-
-particles.addBehavior(
-  new ColorOverLife(
-    new ColorRange(
-      new THREE.Vector4(1.0,0.9,0.1,1.0),
-      new THREE.Vector4(1,0.1,0.0,1)
-    )
-  )
-);
-
-
-
-particles.emitter.name = 'particles';
-scene.add(particles.emitter);
-batchRenderer.addSystem(particles);
 
 const gui = new dat.GUI();
 
