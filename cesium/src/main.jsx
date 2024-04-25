28f94656d0abce1c94b82d7190038fae5a775b87
dezhizhang <1541609448@qq.com>
2024-04-26 06:04:30 +0800

    feat:update

--- cesium/src/main.jsx
+++ cesium/src/main.jsx
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-04-22 20:18:01
  * :last editor: 张德志
- * :date last edited: 2024-04-25 05:42:33
+ * :date last edited: 2024-04-25 07:02:37
  */
 /*
  * :file description:
@@ -51,19 +51,19 @@ Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
 // An example of using a b3dm tileset to classify another b3dm tileset.
 const viewer = new Cesium.Viewer('root', {
   // 是否显示信息窗口
-  infoBox: false,
-  // 是否显示查询按钮
-  geocoder: false,
-  // 不显示home按钮
-  homeButton: false,
-  // 控制查看器显示模式
-  sceneModePicker: false,
-  // 是否显示图层按钮
-  baseLayerPicker: false,
-  // 是否显不帮助
-  navigationHelpButton: false,
-  // 是否显示动画
-  animation: false,
+  // infoBox: false,
+  // // 是否显示查询按钮
+  // geocoder: false,
+  // // 不显示home按钮
+  // homeButton: false,
+  // // 控制查看器显示模式
+  // sceneModePicker: false,
+  // // 是否显示图层按钮
+  // baseLayerPicker: false,
+  // // 是否显不帮助
+  // navigationHelpButton: false,
+  // // 是否显示动画
+  // animation: false,
   skyBox: new Cesium.SkyBox({
     sources: {
       positiveX: '/texture/sky/px.jpg',
