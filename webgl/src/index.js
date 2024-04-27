e17dcf251dbc78bab4ae1d310ab0ac12fd1e0996
dezhizhang <1541609448@qq.com>
2024-04-28 07:35:01 +0800

    feat: 基本图元

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -5,38 +5,35 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2024-04-28 06:36:53
+ * :date last edited: 2024-04-28 07:34:31
  */
-
-
+// const canvas = document.getElementById('canvas');
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const gl = canvas.getContext('webgl');
 
+
 const vertexShaderSource = `
-    precision mediump float;
-    attribute vec2 a_Position;
-    uniform float u_Size;
+    attribute vec4 a_Position;
     void main() {
-        gl_Position = vec4(a_Position,0,1);
-        gl_PointSize = u_Size;
+        gl_Position = a_Position;
+        gl_PointSize = 10.0;
     }
+
 `;
 
-
 const fragShaderSource = `
-    precision mediump float;
-    uniform vec3 u_color;
     void main() {
-        gl_FragColor = vec4(u_color,1);
+        gl_FragColor = vec4(1,0,0,1);
     }
 `;
 
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+
 
 gl.shaderSource(vertexShader,vertexShaderSource);
 gl.shaderSource(fragShader,fragShaderSource);
@@ -45,28 +42,38 @@ gl.shaderSource(fragShader,fragShaderSource);
 gl.compileShader(vertexShader);
 gl.compileShader(fragShader);
 
+// 创建对像
 const program = gl.createProgram();
 gl.attachShader(program,vertexShader);
 gl.attachShader(program,fragShader);
 
+// 链接几何体
 gl.linkProgram(program);
 gl.useProgram(program);
 
+// 创建点
+const dataVertices = new Float32Array([
+    -0.5,0.0,
+    0.5,0.0,
+    0.5,0.5,
+]);
 
-const a_Position = gl.getAttribLocation(program,'a_position');
-gl.vertexAttrib1fv(a_Position,new Float32Array([0.5,0.0]));
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
 
-const u_Size = gl.getUniformLocation(program,'u_Size');
-gl.uniform1f(u_Size,40.0);
+gl.bufferData(gl.ARRAY_BUFFER,dataVertices,gl.STATIC_DRAW);
 
-const u_color = gl.getUniformLocation(program,'u_color');
-gl.uniform3f(u_color,0.0,0.0,1.0);
-
+const a_Position = gl.getAttribLocation(program,'a_pos');
+gl.vertexAttribPointer(a_Position,2,gl.FLOAT,false,0,0);
+gl.enableVertexAttribArray(a_Position);
 
 
 gl.clearColor(0,0,0,1);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-gl.drawArrays(gl.POINTS,0,1);
+gl.drawArrays(gl.TRIANGLES,0,3);
+
 
 document.body.appendChild(canvas);
+
+
