7de4d3a2cbfb0d9386537d77d27e9c3c21f22e62
dezhizhang <1541609448@qq.com>
2024-04-27 19:46:07 +0800

    feat: 粒子系统

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,75 +1,60 @@
 /*
- * :file description:
+ * :file description: 
  * :name: /threejs/src/index.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-04-26 17:43:22
+ * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-04-26 19:43:04
+ * :date last edited: 2024-04-27 19:44:18
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(200, 200, 200);
-camera.lookAt(scene.position);
+const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,1,1000);
+camera.position.set(200,200,200);
+
+
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-
 document.body.appendChild(renderer.domElement);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
-directionalLight.position.set(400, 200, 300);
-scene.add(directionalLight);
-
-const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1.0);
-directionalLight2.position.set(-400, -200, -300);
-scene.add(directionalLight2);
-
-const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera,renderer.domElement);
 
 scene.add(new THREE.AxesHelper(100));
-scene.add(new THREE.AmbientLight(0xffffff, 1));
 
-const pointLight = new THREE.PointLight(0xffffff, 1);
-pointLight.position.set(400, 200, 300);
-scene.add(pointLight);
+scene.add(new THREE.AmbientLight(0xffffff));
+
+
+const geometry = new THREE.BoxGeometry(60,100,60);
+const material = new THREE.MeshLambertMaterial({
+  color:0x009999,
+});
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
 
 const textureLoader = new THREE.TextureLoader();
-const colorTexture = textureLoader.load('/heart/color.png');
-const normalTexture = textureLoader.load('/heart/normal.png');
-const specularTexture = textureLoader.load('/heart/specular.png');
+const texture = textureLoader.load('/红豆.png');
 
-const textureCube = new THREE.CubeTextureLoader()
-  .setPath('/environment/')
-  .load(['px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg']);
-
-const objLoader = new OBJLoader();
-objLoader.load('/heart/model.obj', (obj) => {
-  const mesh = obj.children[0];
-  mesh.scale.set(5, 5, 5);
-
-  const material = new THREE.MeshPhongMaterial({
-    map: colorTexture,
-    normalMap: normalTexture,
-    specularMap: specularTexture,
-    envMap: textureCube,
-    shininess: 100,
-    normalScale: new THREE.Vector2(1, 1),
-  });
-  mesh.material = material;
-
-  scene.add(obj);
+const spriteMaterial = new THREE.SpriteMaterial({
+  map:texture
 });
+
+const sprite = new THREE.Sprite(spriteMaterial);
+sprite.scale.set(40,40,1);
+sprite.position.copy(mesh.position);
+sprite.position.y += 80;
+scene.add(sprite);
+
 
 function render() {
   requestAnimationFrame(render);
-  renderer.render(scene, camera);
+  renderer.render(scene,camera);
 }
 
 render();
+
