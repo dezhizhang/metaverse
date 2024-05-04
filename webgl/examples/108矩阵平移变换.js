7147d9e1690ad027890150806ff643b26fb53b45
dezhizhang <1541609448@qq.com>
2024-05-04 11:51:00 +0800

    feat:108矩阵平移变换

--- webgl/examples/108矩阵平移变换.js
+++ webgl/examples/108矩阵平移变换.js
@@ -1,11 +1,11 @@
 /*
  * :file description:
- * :name: /webgl/src/index.js
+ * :name: /webgl/examples/108矩阵平移变换.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2024-05-04 11:50:05
+ * :date last edited: 2024-05-04 11:50:33
  */
 
 const width = 400;
