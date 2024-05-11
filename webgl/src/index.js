fd22d66ddd45b6c696d77ba96f4b6f5b1288c20a
dezhizhang <1541609448@qq.com>
2024-05-11 09:14:07 +0800

    feat:update

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -3,13 +3,19 @@
  * :name: /webgl/src/index.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
+ * :date created: 2024-05-09 05:56:23
+ * :last editor: 张德志
+ * :date last edited: 2024-05-10 21:04:41
+ */
+/*
+ * :file description:
+ * :name: /webgl/src/index.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
  * :date created: 2024-04-29 05:25:20
  * :last editor: 张德志
- * :date last edited: 2024-05-09 06:56:41
+ * :date last edited: 2024-05-09 05:54:38
  */
-
-import { glMatrix, mat4 } from 'gl-matrix';
-
 // const canvas = document.getElementById('canvas');
 const canvas = document.createElement('canvas');
 canvas.width = 400;
@@ -18,20 +24,32 @@ canvas.height = 400;
 const gl = canvas.getContext('webgl');
 
 const vertexShaderSource = `
+  precision mediump float;
   attribute vec2 a_position;
-  uniform mat4 u_projectionMatrix;
+
   void main() {
-    gl_Position = u_projectionMatrix * vec4(a_position,0.0,1.0);
+    gl_Position = vec4(a_position,0.0,1.0);
     gl_PointSize = 10.0;
   }
 `;
 
 const fragShaderSource = `
   precision mediump float;
-  void main() {
-    gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+  uniform float u_canvasSize;
+
+  uniform float u_stamp;
+
+  float rand(vec2 fragCoord) {
+    vec2 v = (fragCoord - u_canvasSize / 2.0);
+    return fract(atan(v.x,v.y) * 500.0);
   }
-`
+
+  void main() {
+    float f = rand(gl_FragCoord.xy);
+    gl_FragColor = vec4(f,f,f,1);
+  
+  }
+`;
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -49,33 +67,30 @@ gl.attachShader(program, fragShader);
 gl.linkProgram(program);
 gl.useProgram(program);
 
-const dataVertices = new Float32Array([
-  -0.5,0.5,
-  -0.5,-0.6,
-  0.5,0.5,
-]);
+const dataVertices = new Float32Array([-1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, -1.0]);
 const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
-gl.bufferData(gl.ARRAY_BUFFER,dataVertices,gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, dataVertices, gl.STATIC_DRAW);
 
-const a_position = gl.getAttribLocation(program,'a_position');
-gl.vertexAttribPointer(a_position,2,gl.FLOAT,false,0,0);
+const a_position = gl.getAttribLocation(program, 'a_position');
+gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(a_position);
 
-const tMatrix = mat4.create();
-
-const u_projectionMatrix = gl.getUniformLocation(program,'u_projectionMatrix');
-gl.uniformMatrix4fv(u_projectionMatrix,false,tMatrix);
+const u_canvasSize = gl.getUniformLocation(program, 'u_canvasSize');
+gl.uniform2fv(u_canvasSize, [400, 400]);
 
 
+const u_stamp = gl.getUniformLocation(program,'u_stamp');
 
+!(function tick(stamp) {
+  gl.uniform1f(u_stamp,stamp);
 
-
-
+  requestAnimationFrame(tick);
+})();
 
 gl.clearColor(0, 0, 0, 1);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
+gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
 document.body.appendChild(canvas);
