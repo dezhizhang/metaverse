13d7faaded14e5d23cde0af75328a092d303b4b8
dezhizhang <1541609448@qq.com>
2024-05-12 22:35:41 +0800

    feat:平行光阴影计算

--- threejs/examples/平行光阴影计算.js
+++ threejs/examples/平行光阴影计算.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/平行光阴影计算.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-12 22:35:17
  * :last editor: 张德志
- * :date last edited: 2024-05-12 22:34:08
+ * :date last edited: 2024-05-12 22:35:23
  */
 import dat from 'dat.gui';
 import * as THREE from 'three';
