2a6d92bccaaf60e10a9f6002381e5ae2df905393
dezhizhang <1541609448@qq.com>
2024-05-12 20:00:04 +0800

    feat:漫游动画

--- threejs/project/漫游动画.js
+++ threejs/project/漫游动画.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/project/漫游动画.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-12 19:59:30
  * :last editor: 张德志
- * :date last edited: 2024-05-12 19:57:34
+ * :date last edited: 2024-05-12 19:59:44
  */
 
 import * as THREE from 'three';
