6b5c8cb124aaccdf12aaf30063896d4cb2e78ce6
dezhizhang <1541609448@qq.com>
2024-05-12 14:18:19 +0800

    feat:perspectiveCamera

--- threejs/examples/perspectiveCamera.js
+++ threejs/examples/perspectiveCamera.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/perspectiveCamera.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-12 14:17:42
  * :last editor: 张德志
- * :date last edited: 2024-05-12 14:16:42
+ * :date last edited: 2024-05-12 14:17:43
  */
 
 import * as THREE from 'three';
