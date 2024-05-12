feea69d2434fea90a40d0587596fab544f56d67d
dezhizhang <1541609448@qq.com>
2024-05-12 21:10:24 +0800

    feat:controls控制

--- threejs/examples/controls控制.js
+++ threejs/examples/controls控制.js
@@ -1,12 +1,13 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/controls控制.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-12 21:09:52
  * :last editor: 张德志
- * :date last edited: 2024-05-12 21:09:06
+ * :date last edited: 2024-05-12 21:09:59
  */
+
 import dat from 'dat.gui';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
