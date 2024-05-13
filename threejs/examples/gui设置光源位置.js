08fcda37adf8919937cd6694370e8bcdfb4a58be
dezhizhang <1541609448@qq.com>
2024-05-14 05:46:46 +0800

    feat:gui设置光源位置

--- threejs/examples/gui设置光源位置.js
+++ threejs/examples/gui设置光源位置.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/gui设置光源位置.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-14 05:46:20
  * :last editor: 张德志
- * :date last edited: 2024-05-14 05:44:17
+ * :date last edited: 2024-05-14 05:46:28
  */
 import dat from 'dat.gui';
 import * as THREE from 'three';
