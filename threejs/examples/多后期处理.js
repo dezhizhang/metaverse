9452d87c1222f0e6d39874d750e0b81a701c3ced
dezhizhang <1541609448@qq.com>
2024-05-17 06:06:33 +0800

    ftr: 模型后期处理

--- threejs/examples/多后期处理.js
+++ threejs/examples/多后期处理.js
@@ -1,15 +1,15 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/多后期处理.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-17 05:30:15
  * :last editor: 张德志
- * :date last edited: 2024-05-17 06:05:23
+ * :date last edited: 2024-05-17 05:30:22
  */
+
 import dat from 'dat.gui';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
@@ -42,7 +42,20 @@ scene.add(directionalLightHelper);
 const ambient = new THREE.AmbientLight(0xffffff, 0.4);
 scene.add(ambient);
 
+const geometry = new THREE.BoxGeometry(50, 50, 50);
+const material = new THREE.MeshLambertMaterial({
+    color: 0x009999,
+});
+const mesh = new THREE.Mesh(geometry, material);
 
+const mesh2 = mesh.clone();
+mesh2.position.y = 100;
+const mesh3 = mesh.clone();
+mesh3.position.x = 100;
+
+
+// 三个网格模型用于高亮发光描边测试
+scene.add(mesh,mesh2,mesh3);
 
 // 初始化渲染器
 const renderer = new THREE.WebGLRenderer({
@@ -52,48 +65,27 @@ renderer.shadowMap.enabled = true;
 // 设置渲染器大小
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-let model = null;
-
-const gltfLoader = new GLTFLoader();
-gltfLoader.load('/工厂.glb',(gltf) => {
-  // console.log(gltf);
-  model = gltf.scene;
-  scene.add(gltf.scene);
-});
-
-
-
-
 
 
 // 后处理
-
 const composer = new EffectComposer(renderer);
 const renderPass = new RenderPass(scene,camera);
 composer.addPass(renderPass);
 
+
 const v2 = new THREE.Vector3(window.innerWidth,window.innerHeight);
+
+
 const outlinePass = new OutlinePass(v2,scene,camera);
+outlinePass.selectedObjects = [mesh];
 outlinePass.edgeThickness = 4;
 outlinePass.edgeStrength = 6;
-
-const xDom = document.getElementById('x');
-xDom.addEventListener('click',() => {
-  const A = model.getObjectByName('大货车2');
-  outlinePass.selectedObjects = [A];
-});
-
-const yDom = document.getElementById('y');
-yDom.addEventListener('click',() => {
-  const A = model.getObjectByName('大货车1');
-  outlinePass.selectedObjects = [A];
-});
-
-
-outlinePass.visibleEdgeColor.set(0x0000ff);
+outlinePass.visibleEdgeColor.set(0x00ff00);
 composer.addPass(outlinePass);
 
 
+const glitchPass = new GlitchPass(v2,scene,camera);
+composer.addPass(glitchPass);
 
 
 const controls = new OrbitControls(camera,renderer.domElement);
