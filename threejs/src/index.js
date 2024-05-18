851d71c551a277859787491c7a9e4b230a0e6d38
dezhizhang <1541609448@qq.com>
2024-05-18 21:12:20 +0800

    ftr: 工厂动画

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,17 +5,11 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2024-05-17 23:01:30
+ * :date last edited: 2024-05-18 21:11:06
  */
-import dat from 'dat.gui';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
-import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
-import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader.js';
 
 //创建场影
 const scene = new THREE.Scene();
@@ -31,11 +25,6 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(100, 60, 50);
 scene.add(directionalLight);
 
-// scene.add(new THREE.CameraHelper(directionalLight.shadow.camera));
-
-// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
-
-// scene.add(directionalLightHelper);
 
 const ambient = new THREE.AmbientLight(0xffffff, 0.4);
 scene.add(ambient);
@@ -48,62 +37,16 @@ renderer.shadowMap.enabled = true;
 // 设置渲染器大小
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const geometry = new THREE.BoxGeometry(15,15,15);
-const material = new THREE.MeshLambertMaterial({
-  color:0xff0000
-});
-const mesh = new THREE.Mesh(geometry,material);
+let mixer = null;
 
-mesh.name = 'Box';
-scene.add(mesh);
-
-
-const times = [0,3,6];
-
-const values = [0,0,0,100,0,0,0,0,100];
-
-const posKF = new THREE.KeyframeTrack('Box.position',times,values);
-
-const colorKF = new THREE.KeyframeTrack('Box.material.color',[2,5],[1,0,0,0,0,1]);
-
-const clip = new THREE.AnimationClip('test',6,[posKF,colorKF]);
-
-const mixer = new THREE.AnimationMixer(mesh);
-
-const clipAction = mixer.clipAction(clip);
-
-clipAction.loop = THREE.LoopOnce;
-clipAction.clampWhenFinished = true;
-
-
-const xDom = document.getElementById('x');
-xDom.addEventListener('click',() => {
-  if(!clipAction.paused) clipAction.paused = true;
-  if(clipAction.paused) clipAction.paused = false;
-
+const gltfLoader = new GLTFLoader();
+gltfLoader.load('/工厂.glb',(gltf) => {
+  console.log(gltf);
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  const clipAction = mixer.clipAction(gltf.animations[0]);
   clipAction.play();
+  scene.add(gltf.scene);
 });
-
-
-const yDom = document.getElementById('y');
-yDom.addEventListener('click',() => {
-  clipAction.paused = true;
-});
-
-
-const gui = new dat.GUI();
-gui.domElement.position = 'absolute';
-gui.domElement.style.top = '20px';
-gui.domElement.style.right = '20px';
-
-document.body.appendChild(gui.domElement);
-
-
-gui.add(clipAction,'timeScale').onChange((value) => {
-  clipAction.timeScale = value;
-});
-
-
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -126,7 +69,9 @@ function render() {
   requestAnimationFrame(render);
   // composer.render();
   renderer.render(scene, camera);
-  mixer.update(frameT);
+  if(mixer) {
+    mixer.update(frameT);
+  }
 
 }
 
