95360fe09f0869f7ff7f6af15bed35181f70f6d4
dezhizhang <1541609448@qq.com>
2024-05-19 20:46:48 +0800

    feat: 玩家角色运动

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,101 +1,82 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /threejs/src/index.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-05-19 15:48:43
+ * :date last edited: 2024-05-19 20:45:03
  */
-import * as dat from 'dat.gui';
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-//骨骼动画
-//创建场影
+const keyStates = {
+  W: false,
+  A: false,
+  S: false,
+  D: false,
+};
+
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'KeyW') keyStates.W = true;
+  if (event.code === 'KeyA') keyStates.A = true;
+  if (event.code === 'KeyS') keyStates.S = true;
+  if (event.code === 'KeyD') keyStates.D = true;
+});
+
+document.addEventListener('keyup', (event) => {
+  if (event.code === 'KeyW') keyStates.W = false;
+  if (event.code === 'KeyA') keyStates.A = false;
+  if (event.code === 'KeyS') keyStates.S = false;
+  if (event.code === 'KeyD') keyStates.D = false;
+});
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(200, 200, 200);
-camera.lookAt(0, 0, 0);
+camera.position.set(20, 20, 20);
+camera.lookAt(scene.position);
 
-const axesHelper = new THREE.AxesHelper(100);
-scene.add(axesHelper);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(100, 60, 50);
-scene.add(directionalLight);
-
-const ambient = new THREE.AmbientLight(0xffffff, 0.4);
-scene.add(ambient);
-
-// 初始化渲染器
-const renderer = new THREE.WebGLRenderer({
-  antialias: true,
-});
-renderer.shadowMap.enabled = true;
-// 设置渲染器大小
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(40, 40, 40);
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ffff,
-});
+//ligut 
+const ambientLight = new THREE.AmbientLight(0xffffff,1.0);
+scene.add(ambientLight);
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-
-const tag = document.createElement('div');
-tag.style.background = '#ccc';
-tag.style.color = '#fff';
-tag.style.position = 'absolute';
-tag.style.borderRadius = '4px';
-tag.style.padding = '4px';
-tag.innerHTML = '标签';
-
-const objMesh = new CSS2DObject(tag);
-scene.add(objMesh);
-
-const css2dRenderer = new CSS2DRenderer();
-css2dRenderer.domElement.style.position = 'absolute';
-css2dRenderer.domElement.style.left = '0px';
-css2dRenderer.domElement.style.top = '0px';
-css2dRenderer.domElement.style.pointerEvents = 'none';
-css2dRenderer.setSize(window.innerWidth,window.innerHeight);
-
-document.body.appendChild(css2dRenderer.domElement);
+const gridHelper = new THREE.GridHelper(30,25,0x004444,0x004444);
+scene.add(gridHelper);
 
 
 
+//gltf
+const gltfLoader = new GLTFLoader();
+const gltf = await gltfLoader.loadAsync('/机器人.glb');
+const player = gltf.scene;
 
-
+scene.add(player);
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-window.addEventListener('resize', () => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+scene.add(new THREE.AxesHelper(100));
 
-  renderer.setSize(width, height);
-  css2dRenderer.setSize(width,height);
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
-});
-
-document.body.append(renderer.domElement);
+const v3 = new THREE.Vector3(0,0,3);
 
 const clock = new THREE.Clock();
 
 function render() {
-  // const frameT = clock.getDelta();
+  const deltaTime = clock.getDelta();
+  if(keyStates.W) {
+    const deltaPos = v3.clone().multiplyScalar(deltaTime);
+    player.position.add(deltaPos);
+  }
   requestAnimationFrame(render);
-  // composer.render();
   renderer.render(scene, camera);
-  css2dRenderer.render(scene,camera);
-
 }
 
 render();
