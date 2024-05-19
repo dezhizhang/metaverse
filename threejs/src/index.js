c8f32213f8e7b79f651ff768f75d6172867da9d4
dezhizhang <1541609448@qq.com>
2024-05-19 21:18:22 +0800

    feat:添加AnimationMixer

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-03-13 22:44:48
  * :last editor: 张德志
- * :date last edited: 2024-05-19 20:54:09
+ * :date last edited: 2024-05-19 21:17:56
  */
 
 import * as THREE from 'three';
@@ -44,60 +44,71 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-//ligut 
-const ambientLight = new THREE.AmbientLight(0xffffff,1.0);
+//ligut
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
 scene.add(ambientLight);
 
-const gridHelper = new THREE.GridHelper(30,25,0x004444,0x004444);
+const gridHelper = new THREE.GridHelper(30, 25, 0x004444, 0x004444);
 scene.add(gridHelper);
 
-
-document.addEventListener('resize',() => {
-  renderer.setSize(window.innerWidth,window.innerHeight);
+document.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
 
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 });
 
-
-
-
 //gltf
 const gltfLoader = new GLTFLoader();
 const gltf = await gltfLoader.loadAsync('/机器人.glb');
 const player = gltf.scene;
 
-scene.add(player);
+const mixer = new THREE.AnimationMixer(player);
+const action = mixer.clipAction(gltf.animations[12]);
+action.play();
 
+
+
+scene.add(player);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 scene.add(new THREE.AxesHelper(100));
 
-const v3 = new THREE.Vector3(0,0,3);
+const v3 = new THREE.Vector3(0, 0, 0);
 
 const clock = new THREE.Clock();
 
 const a = 12;
 const vMax = 5; // 最大速度
-
-
+const damping = -0.04;
 
 function render() {
   const deltaTime = clock.getDelta();
   // z方向运动
-  if(keyStates.W) {
-    const front = new THREE.Vector3(0,0,1);
+  if (keyStates.W) {
+    const front = new THREE.Vector3(0, 0, 1);
 
-    if(v3.length() < vMax) {
+    if (v3.length() < vMax) {
       v3.add(front.multiplyScalar(a * deltaTime));
     }
-    const deltaPos = v3.clone().multiplyScalar(deltaTime);
-    player.position.add(deltaPos);
+  } else if (keyStates.S) {
+    const front = new THREE.Vector3(0, 0, -1);
+    if (v3.length() < vMax) {
+      v3.add(front.multiplyScalar(a * deltaTime));
+    }
   }
+
+  v3.addScaledVector(v3, damping);
+  const deltaPos = v3.clone().multiplyScalar(deltaTime);
+  player.position.add(deltaPos);
+
   requestAnimationFrame(render);
   renderer.render(scene, camera);
+  if(mixer) {
+    mixer.update(deltaTime);
+  }
 }
 
 render();
