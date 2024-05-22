fa119b6a6c6bc9563ae35c636291120c3a04febe
dezhizhang <1541609448@qq.com>
2024-05-23 07:23:36 +0800

    feat:生成点阵

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-05-22 22:36:48
  * :last editor: 张德志
- * :date last edited: 2024-05-23 05:48:07
+ * :date last edited: 2024-05-23 07:22:20
  */
 
 import * as THREE from 'three';
@@ -20,47 +20,107 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 // 设置相机位置
 camera.position.set(103, 45, 200);
-camera.lookAt(103,45,0)
+camera.lookAt(103, 45, 0);
 
 const pointArr = [];
 
 data.forEach((elem) => {
-  pointArr.push(elem[0],elem[1],0);
+  pointArr.push(elem[0], elem[1], 0);
 });
-
-
 scene.add(polygon(pointArr));
+scene.add(pointPolygon(data));
 
 function polygon(pointArr) {
-  
   const group = new THREE.Group();
 
   const geometry = new THREE.BufferGeometry();
   const vertices = new Float32Array(pointArr);
 
-  const attribute = new THREE.BufferAttribute(vertices,3);
+  const attribute = new THREE.BufferAttribute(vertices, 3);
   geometry.attributes.position = attribute;
 
   const material = new THREE.MeshBasicMaterial({
-    color:0x006666
+    color: 0x006666,
   });
-  const line = new THREE.LineLoop(geometry,material);
-
+  const line = new THREE.LineLoop(geometry, material);
 
   const pointMaterial = new THREE.PointsMaterial({
-    color:0xffffff,
-    size:0.3
+    color: 0xffffff,
+    size: 0.3,
   });
 
-  const points = new THREE.Points(geometry,pointMaterial);
+  const points = new THREE.Points(geometry, pointMaterial);
 
-  group.add(line,points);
+  group.add(line, points);
 
   return group;
 }
 
 
+function pointPolygon(polygon) {
+  const lonArr = [];
+  const latArr = [];
 
+  polygon.forEach((elem) => {
+    lonArr.push(elem[0]);
+    latArr.push(elem[1]);
+  });
+
+  const [lonMin,lonMax] = minMax(lonArr);
+  const [latMin,latMax] = minMax(latArr);
+
+
+  const step = 1;
+  const row = Math.ceil((lonMax - lonMin) / step);
+  const column = Math.ceil((latMax - latMin) / step);
+
+
+
+  const rectPontsArr = [];
+  for(let i=0;i < row + 1;i++) {
+    for(let j=0;j < column + 1;j++) {
+      rectPontsArr.push([lonMin + i * step,latMax + j * step]);
+    }
+  }
+
+  const pointArr = [];
+  rectPontsArr.forEach((elem) => {
+    pointArr.push(elem[0],elem[1],0);
+  });
+
+  const geometry = new THREE.BufferGeometry();
+  const attributes = new Float32Array(pointArr);
+  geometry.attributes.position = new THREE.BufferAttribute(attributes,3)
+
+
+  const material = new THREE.PointsMaterial({
+    color:0xff0000,
+    size:0.1
+  });
+
+  const points = new THREE.Points(geometry,material);
+  return points;
+
+
+
+}
+
+
+
+function minMax(arr) {
+  arr.sort(compareNum);
+  return [Math.floor(arr[0]), Math.ceil(arr[arr.length - 1])];
+}
+
+function compareNum(num1, num2) {
+  if (num1 < num2) {
+    return -1;
+  } else if (num1 > num2) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
 // 初始化渲染器
 const renderer = new THREE.WebGLRenderer();
@@ -69,12 +129,10 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(103,45,0);
+controls.target.set(103, 45, 0);
 controls.update();
 
-
 document.body.append(renderer.domElement);
-
 
 function render() {
   requestAnimationFrame(render);
