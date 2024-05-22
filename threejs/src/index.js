1fa4bd28fb80ecc0804b9ddc42dd781d1a02d432
dezhizhang <1541609448@qq.com>
2024-05-23 05:49:24 +0800

    feat: 解析数据

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,11 +5,11 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-05-22 22:36:48
  * :last editor: 张德志
- * :date last edited: 2024-05-23 05:03:11
+ * :date last edited: 2024-05-23 05:48:07
  */
 
 import * as THREE from 'three';
-import { OSS_URL } from '../config'
+import data from './polygonData';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 //创建场影
@@ -19,39 +19,64 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // 设置相机位置
-camera.position.set(200, 200, 200);
-scene.add(camera);
+camera.position.set(103, 45, 200);
+camera.lookAt(103,45,0)
 
-const textureLoader = new THREE.TextureLoader();
+const pointArr = [];
 
-const geometry = new THREE.PlaneGeometry(60, 60);
-const material = new THREE.MeshBasicMaterial({
-  map: textureLoader.load(`${OSS_URL}/sungent/wave-aperture.png`),
-  color: 0x00ffff,
-  transparent: true,
-  side: THREE.DoubleSide,
+data.forEach((elem) => {
+  pointArr.push(elem[0],elem[1],0);
 });
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+
+scene.add(polygon(pointArr));
+
+function polygon(pointArr) {
+  
+  const group = new THREE.Group();
+
+  const geometry = new THREE.BufferGeometry();
+  const vertices = new Float32Array(pointArr);
+
+  const attribute = new THREE.BufferAttribute(vertices,3);
+  geometry.attributes.position = attribute;
+
+  const material = new THREE.MeshBasicMaterial({
+    color:0x006666
+  });
+  const line = new THREE.LineLoop(geometry,material);
+
+
+  const pointMaterial = new THREE.PointsMaterial({
+    color:0xffffff,
+    size:0.3
+  });
+
+  const points = new THREE.Points(geometry,pointMaterial);
+
+  group.add(line,points);
+
+  return group;
+}
+
+
+
 
 // 初始化渲染器
-const renderer = new THREE.WebGL1Renderer();
+const renderer = new THREE.WebGLRenderer();
 
 // 设置渲染器大小
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(103,45,0);
+controls.update();
+
 
 document.body.append(renderer.domElement);
 
-let s = 1.0;
 
 function render() {
-  s += 0.01;
-  mesh.scale.set(s, s, s);
-  mesh.material.opacity = 1 - (s - 1) / 1.5;
-  if (s > 2.5) s = 1.0;
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
