d8a019c98f8297ba3ff3fa04815bfc06051657f8
dezhizhang <1541609448@qq.com>
2024-05-23 05:06:36 +0800

    feat: 波动光圈

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,10 +5,11 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-05-22 22:36:48
  * :last editor: 张德志
- * :date last edited: 2024-05-22 22:49:29
+ * :date last edited: 2024-05-23 05:03:11
  */
 
 import * as THREE from 'three';
+import { OSS_URL } from '../config'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 //创建场影
@@ -21,11 +22,12 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.set(200, 200, 200);
 scene.add(camera);
 
-const geometry = new THREE.PlaneGeometry(20, 160);
 const textureLoader = new THREE.TextureLoader();
+
+const geometry = new THREE.PlaneGeometry(60, 60);
 const material = new THREE.MeshBasicMaterial({
-  map: textureLoader.load('https://tugua.oss-cn-hangzhou.aliyuncs.com/sungent/light-injection.png'),
-  color: 0x44ffaa,
+  map: textureLoader.load(`${OSS_URL}/sungent/wave-aperture.png`),
+  color: 0x00ffff,
   transparent: true,
   side: THREE.DoubleSide,
 });
@@ -43,7 +45,13 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 document.body.append(renderer.domElement);
 
+let s = 1.0;
+
 function render() {
+  s += 0.01;
+  mesh.scale.set(s, s, s);
+  mesh.material.opacity = 1 - (s - 1) / 1.5;
+  if (s > 2.5) s = 1.0;
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
