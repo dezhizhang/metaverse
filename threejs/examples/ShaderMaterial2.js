099ba755fe330621c3f4f87b84aafba542b6d0e7
dezhizhang <1541609448@qq.com>
2024-05-26 21:51:49 +0800

    feat:ShaderMaterial2传值

--- threejs/examples/ShaderMaterial2.js
+++ threejs/examples/ShaderMaterial2.js
@@ -1,13 +1,12 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/ShaderMaterial2.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-26 21:48:56
  * :last editor: 张德志
- * :date last edited: 2024-05-26 21:48:30
+ * :date last edited: 2024-05-26 21:50:49
  */
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
