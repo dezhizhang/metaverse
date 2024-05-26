330f77f93cc236cb7f6a4e9181b887dcade45529
dezhizhang <1541609448@qq.com>
2024-05-26 14:22:05 +0800

    feat: 颜色差值

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2024-05-26 10:10:20
+ * :date last edited: 2024-05-26 14:16:35
  */
 
 import * as THREE from 'three';
@@ -26,19 +26,35 @@ const renderer = new THREE.WebGLRenderer();
 
 
 const geometry = new THREE.BufferGeometry();
-const arc = new THREE.ArcCurve(0,0,50,0,2 * Math.PI,false);
+const curve = new THREE.CatmullRomCurve3([
+  new THREE.Vector3(100,0,-100),
+  new THREE.Vector3(0,80,0),
+  new THREE.Vector3(-100,0,100),
+]);
 
-const points = arc.getSpacedPoints(50);
+const points = curve.getSpacedPoints(100);
 geometry.setFromPoints(points);
 
 const material = new THREE.LineBasicMaterial({
-  color:0x00ffff
+  color:0x006666,
 });
 
 const line = new THREE.Line(geometry,material);
 scene.add(line);
 
+const index = 20;
+const num = 10;
 
+const points2 = points.slice(index,index + num);
+const geometry2 = new THREE.BufferGeometry();
+geometry2.setFromPoints(points2);
+
+const material2 = new THREE.LineBasicMaterial({
+  color:0xffff00,
+});
+
+const line2 = new THREE.Line(geometry2,material2);
+scene.add(line2);
 
 
 // 设置渲染器大小
