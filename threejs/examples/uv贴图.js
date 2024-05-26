5a3d127daa2dedd88cafa9029b1adf29f0bf8356
dezhizhang <1541609448@qq.com>
2024-05-27 06:17:15 +0800

    feat:uv贴图

--- threejs/examples/uv贴图.js
+++ threejs/examples/uv贴图.js
@@ -1,13 +1,12 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/uv贴图.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-27 06:16:44
  * :last editor: 张德志
- * :date last edited: 2024-05-27 06:14:06
+ * :date last edited: 2024-05-27 06:16:50
  */
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
