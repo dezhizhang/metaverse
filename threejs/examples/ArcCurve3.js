e04cc9c37f0ae4b809f15bf459032ae8fde42303
dezhizhang <1541609448@qq.com>
2024-05-26 10:11:44 +0800

    feat:ArcCurve3

--- threejs/examples/ArcCurve3.js
+++ threejs/examples/ArcCurve3.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/ArcCurve3.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-26 10:11:05
  * :last editor: 张德志
- * :date last edited: 2024-05-26 10:10:20
+ * :date last edited: 2024-05-26 10:11:07
  */
 
 import * as THREE from 'three';
