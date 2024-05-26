3643c3351cb529bd54c8dcc8f4d8e2b07c54b607
dezhizhang <1541609448@qq.com>
2024-05-26 14:40:19 +0800

    feat: 飞线

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2023-03-13 05:58:33
  * :last editor: 张德志
- * :date last edited: 2024-05-26 14:16:35
+ * :date last edited: 2024-05-26 14:37:57
  */
 
 import * as THREE from 'three';
@@ -42,6 +42,7 @@ const material = new THREE.LineBasicMaterial({
 const line = new THREE.Line(geometry,material);
 scene.add(line);
 
+
 const index = 20;
 const num = 10;
 
@@ -49,9 +50,30 @@ const points2 = points.slice(index,index + num);
 const geometry2 = new THREE.BufferGeometry();
 geometry2.setFromPoints(points2);
 
+const posNum = points.length - 2;
+const colorArr = [];
+
+for(let i=0;i < points2.length;i++) {
+  var color1 = new THREE.Color(0x006666); //轨迹线颜色 青色
+  var color2 = new THREE.Color(0xffff00); //黄色
+
+  let color = null;
+
+  if(i < posNum) {
+    color = color1.lerp(color2,i / posNum);
+  }else {
+    color = color2.lerp(color1, (i - posNum) / (points2.length - posNum));
+  }
+  colorArr.push(color.r,color.g,color.b);
+
+}
+
+geometry2.attributes.color = new THREE.BufferAttribute(new Float32Array(colorArr),3);
+
 const material2 = new THREE.LineBasicMaterial({
-  color:0xffff00,
-});
+  vertexColors: true //使用顶点颜色，不用设置color
+})
+
 
 const line2 = new THREE.Line(geometry2,material2);
 scene.add(line2);
