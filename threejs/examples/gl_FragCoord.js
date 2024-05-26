f1ed770ab9f68003f1211492d82dbcb6250ec23c
dezhizhang <1541609448@qq.com>
2024-05-26 22:22:03 +0800

    feat:gl_FragCoord

--- threejs/examples/gl_FragCoord.js
+++ threejs/examples/gl_FragCoord.js
@@ -1,13 +1,12 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/gl_FragCoord.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :date created: 2024-05-26 22:21:25
  * :last editor: 张德志
- * :date last edited: 2024-05-26 22:20:58
+ * :date last edited: 2024-05-26 22:21:26
  */
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
