ec4bd057dd338f44de510dee0a4cbe09cc5c0a15
dezhizhang <1541609448@qq.com>
2024-06-12 05:33:27 +0800

    feat: 光照效果

--- cesium/README.md
+++ cesium/README.md
@@ -118,3 +118,90 @@ dataSource.then((data) => {
   viewer.zoomTo(data);
 })
 ```
+### 计算包围盒
+```ts
+const line = turf.lineString([
+  [108, 34],
+  [108, 34.2],
+  [108.2, 34.1],
+]);
+
+const polygon:any = turf.polygon([
+  [
+    [108,34],
+    [108,34.2],
+    [108.2,34.1],
+    [108,34],
+
+  ]
+]);
+
+const box = turf.bbox(polygon);
+console.log('box',box)
+    
+
+const collection = turf.featureCollection([line,polygon]);
+
+const datasource = Cesium.GeoJsonDataSource.load(collection,{
+  stroke: Cesium.Color.BLUE,
+  fill: Cesium.Color.RED,
+  strokeWidth: 3,
+});
+
+datasource.then((data) => {
+  viewer.dataSources.add(data);
+  viewer.zoomTo(data);
+})
+
+```
+
+### 设置tilset样式
+```ts
+const tilset = await new Cesium.Cesium3DTileset({
+  url: '/tileset.json',
+}).readyPromise;
+
+viewer.scene.primitives.add(tilset);
+
+viewer.zoomTo(tilset);
+
+
+const styleArr = [
+      {
+        color:{
+          conditions:[
+            ["${building_name} === 'build0","color(purple)"],
+            ["${building_name} === 'build1","color(red)"],
+            ["${building_name} === 'build1","color(orange)"],
+            ["true","color(orange)"],
+          ]
+        }
+      }
+    ];
+
+tilset.style = new Cesium.Cesium3DTileStyle(styleArr[0]);
+```
+
+###  
+```ts
+  // 光照效果
+    viewer.scene.globe.enableLighting = true;
+
+    // 雾
+    // viewer.scene.fog.enabled = true;
+    // viewer.scene.fog.minimumBrightness = 0.1;
+    // viewer.scene.fog.density = 0.03;
+
+
+    // viewer.scene.globe.showGroundAtmosphere = true;
+    // viewer.scene.globe.lightingFadeInDistance = 10;
+    
+    
+    // 天空大气效果
+    // viewer.scene.skyAtmosphere.show = true;
+    // viewer.scene.skyAtmosphere.brightnessShift = 20;
+
+    // HDR效果
+    viewer.scene.highDynamicRange = true;
+
+```
--- cesium/src/pages/index.tsx
+++ cesium/src/pages/index.tsx
@@ -5,10 +5,10 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-06-02 21:15:48
  * :last editor: 张德志
- * :date last edited: 2024-06-06 22:37:30
+ * :date last edited: 2024-06-12 05:30:59
  */
 import * as Cesium from 'cesium';
-import * as turf from '@turf/turf'
+import * as turf from '@turf/turf';
 import '/public/Widgets/widgets.css';
 import { useEffect } from 'react';
 import './index.less';
@@ -20,7 +20,7 @@ export const ACCESS_TOKEN =
 (window as any).CESIUM_BASE_URL = '/';
 
 export default function IndexPage() {
-  useEffect(() => {
+  const load3dModel = async () => {
     Cesium.Ion.defaultAccessToken = ACCESS_TOKEN;
     Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
       89.5,
@@ -46,86 +46,55 @@ export default function IndexPage() {
       timeline: false,
       // 是否显示全屏按钮
       fullscreenButton: false,
-      imageryProvider: new Cesium.UrlTemplateImageryProvider({
-        url:
-          'http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=18&x={x}&y={y}&z={z}',
-        // layer: "tdtVecBasicLayer",
-        // style: "default",
-        // format: "image/png",
-        // tileMatrixSetID: "GoogleMapsCompatible",
-      }),
+      // imageryProvider: new Cesium.UrlTemplateImageryProvider({
+      //   url:
+      //     'http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=18&x={x}&y={y}&z={z}',
+      //   // layer: "tdtVecBasicLayer",
+      //   // style: "default",
+      //   // format: "image/png",
+      //   // tileMatrixSetID: "GoogleMapsCompatible",
+      // }),
       // 添加天空盒子
       // terrainProvider:new Cesium.CesiumTerrainProvider({
       //   url:'',
       // })
     });
 
-    // const tiles3d = Cesium.createOsmBuildings();
-    // const osmBuildings = viewer.scene.primitives.add(tiles3d);
+    // 光照效果
+    viewer.scene.globe.enableLighting = true;
 
-    // viewer.camera.setView({
-    //   destination: Cesium.Cartesian3.fromDegrees(113.3919, 23.109, 1000),
-    //   orientation: {
-    //     // heading:Cesium.Cartesian3.fromRadians(-30)
-    //   },
-    // });
+    // 雾
+    // viewer.scene.fog.enabled = true;
+    // viewer.scene.fog.minimumBrightness = 0.1;
+    // viewer.scene.fog.density = 0.03;
 
 
-    // viewer.camera.setView({
-    //   destination:Cesium.Cartesian3.fromDegrees(109,34,10000)
-    // })
-    // var polygons = turf.randomPolygon(25, {bbox: [-180, -90, 180, 90]})
+    // viewer.scene.globe.showGroundAtmosphere = true;
+    // viewer.scene.globe.lightingFadeInDistance = 10;
+    
+    
+    // 天空大气效果
+    // viewer.scene.skyAtmosphere.show = true;
+    // viewer.scene.skyAtmosphere.brightnessShift = 20;
 
+    // HDR效果
+    viewer.scene.highDynamicRange = true;
 
-
-
-    // const polygon = turf.polygon(
-    //   [
-    //     [
-    //       [108,34],
-    //       [108,34.5],
-    //       [109,34.5],
-    //       [109,34],
-    //       [108,34],
-    //     ]
-    //   ],
-    //   {
-    //     name:'polygon'
-    //   }
-    // );
-
-    // const point:any = turf.point([109,34],{name:'point'});
-
-    // const collection = turf.featureCollection([
-    //   point,
-    //   polygon
-    // ]);
-
-  
-
-    const center = [108.5,34.5];
-    const radis = 5;
-
-    const options = {steps:100,unit:'kilometers'};
-    const circle = turf.circle(center,radis,options);
-
-
-    const collection = turf.featureCollection([
-      circle
-    ]);
-
-    const dataSource = Cesium.GeoJsonDataSource.load(collection);
-
-    dataSource.then((data) => {
-      viewer.dataSources.add(data);
-      viewer.zoomTo(data);
-    })
+    
 
 
     
 
 
 
+    
+
+
+  };
+  useEffect(() => {
+    load3dModel();
+
+   
   }, []);
 
   return <div id="container" />;
