223186d76dab30bab9f62e4193e5f456cfb4d63a
dezhizhang <1541609448@qq.com>
2024-08-12 22:52:06 +0800

    feat:深度缓冲区

--- webgpu/src/main.js
+++ webgpu/src/main.js
@@ -7,82 +7,87 @@ async function init() {
   canvas.height = 500;
   document.body.append(canvas);
 
+
   const adapter = await navigator.gpu.requestAdapter();
   const device = await adapter.requestDevice();
   const format = await navigator.gpu.getPreferredCanvasFormat();
 
-  const ctx = canvas.getContext("webgpu");
+  const ctx = canvas.getContext('webgpu');
 
   const vertexArray = new Float32Array([
-    0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+    0.0, 1.0, 0.0,
   ]);
 
   const modelMatrix = glMatrix.mat4.create();
-  glMatrix.mat4.rotateZ(modelMatrix, modelMatrix, Math.PI / 6);
-
+  glMatrix.mat4.rotateZ(modelMatrix,modelMatrix,Math.PI / 6);
   const modelMatrixBuffer = device.createBuffer({
-    size: modelMatrix.byteLength,
-    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    size:modelMatrix.byteLength,
+    usage:GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
   });
 
-  device.queue.writeBuffer(modelMatrixBuffer, 0, modelMatrix);
+  device.queue.writeBuffer(modelMatrixBuffer,0,modelMatrix);
 
   const vertexBuffer = device.createBuffer({
-    // 绘冲区长度
-    size: vertexArray.byteLength,
-    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    size:vertexArray.byteLength,
+    usage:GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
   });
-  device.queue.writeBuffer(vertexBuffer, 0, vertexArray);
+
+  device.queue.writeBuffer(vertexBuffer,0,vertexArray);
 
   ctx.configure({
     device,
-    format,
+    format
   });
-
-  // 创建渲染管理
+  
+  // 创建渲染管线
   const pipeline = device.createRenderPipeline({
-    layout: "auto",
-    vertex: {
-      module: device.createShaderModule({ code: vertex }),
-      entryPoint: "main",
-      buffers: [
+    layout:'auto',
+    vertex:{
+      module:device.createShaderModule({code:vertex}),
+      entryPoint:"main",
+      buffers:[
         {
-          arrayStride: 3 * 4,
-          attributes: [
+          arrayStride:3 * 4,
+          attributes:[
             {
-              shaderLocation: 0,
-              format: "float32x3",
-              offset: 0,
-            },
-          ],
-        },
-      ],
+              shaderLocation:0,
+              format:'float32x3',
+              offset:0,
+            }
+          ]
+        }
+      ]
     },
-    fragment: {
-      module: device.createShaderModule({ code: fragment }),
-      targets: [
+    fragment:{
+      module:device.createShaderModule({
+        code:fragment,
+      }),
+      targets:[
         {
-          format,
-        },
-      ],
+          format
+        }
+      ]
     },
-    primitive: {
-      topology: "triangle-list",
-    },
-  });
+    primitive:{
+      topology:'triangle-list'
+    }
+  })
 
-  // 设置uniform数据的绑定组
+
+  // 设置uniform数据邦定组
   const bindGroup = device.createBindGroup({
     layout: pipeline.getBindGroupLayout(0),
     entries: [
       {
         binding: 0,
-        resource: { buffer: modelMatrixBuffer },
+        resource: {
+          buffer: modelMatrixBuffer,
+        },
       },
     ],
   });
-
-
 
   let angle = 0;
 
@@ -91,34 +96,33 @@ async function init() {
 
     const modelMatrix = glMatrix.mat4.create();
 
-    glMatrix.mat4.rotateZ(modelMatrix,modelMatrix,angle);
+    glMatrix.mat4.rotateZ(modelMatrix, modelMatrix, angle);
 
-    device.queue.writeBuffer(modelMatrixBuffer,0,modelMatrix);
+    device.queue.writeBuffer(modelMatrixBuffer, 0, modelMatrix);
 
     // 创建命令编码器
     const commandEncoder = device.createCommandEncoder();
     // 创建渲染通道
     const renderPass = commandEncoder.beginRenderPass({
-      colorAttachments:[
+      colorAttachments: [
         {
-          view:ctx.getCurrentTexture().createView(),
-          storeOp:'store',
-          loadOp:'clear',
-          clearValue:{
-            r:0,
-            g:0,
-            b:0,
-            a:1
-          }
-        }
-      ]
+          view: ctx.getCurrentTexture().createView(),
+          storeOp: "store",
+          loadOp: "clear",
+          clearValue: {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 1,
+          },
+        },
+      ],
     });
 
     // 设置渲染管线
     renderPass.setPipeline(pipeline);
-    renderPass.setVertexBuffer(0,vertexBuffer);
-    renderPass.setBindGroup(0,bindGroup);
-
+    renderPass.setVertexBuffer(0, vertexBuffer);
+    renderPass.setBindGroup(0, bindGroup);
 
     // 绘制
     renderPass.draw(3);
@@ -129,10 +133,8 @@ async function init() {
     device.queue.submit([commandBuffer]);
 
     requestAnimationFrame(render);
-
   }
 
-  
   render();
 }
 
--- webgpu/src/shader.js
+++ webgpu/src/shader.js
@@ -14,13 +14,14 @@ fn main(@location(0) pos:vec3<f32>) -> @builtin(position) vec4<f32> {
 }
 `
 
-// 片元着色器
-const fragment =  /*wgsl*/ `
+const fragment = /*wgsl*/`
 @fragment
-fn main() ->@location(0) vec4<f32>{
-    return vec4<f32>(1.0,0.0,0.0,1.0);
+fn main(@builtin(position) fragCoord:vec4<f32>) ->@location(0) vec4<f32>{
+    var x = fragCoord.x;
+    var y = fragCoord.y;
+    return vec4<f32>(x,y,0.0,1.0);
 }
-`;
+`
 
 
 export { vertex, fragment };
