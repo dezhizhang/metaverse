c9b3debeedf4560021a7adea9835a47050a4da96
dezhizhang <1541609448@qq.com>
2024-11-27 05:48:21 +0800

    feat:物理材质

--- physics/webpack.config.js
+++ physics/webpack.config.js
@@ -1,33 +1,39 @@
 /*
- * :file description: 
- * :name: /metaverse/physics/webpack.config.js
+ * :file description:
+ * :name: /physics/webpack.config.js
  * :author:张德志
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-11-26 05:55:02
  * :last editor: 张德志
- * :date last edited: 2024-11-26 05:55:03
+ * :date last edited: 2024-11-27 05:36:06
  */
-const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'build.js'
-    },
-    module: {
-        rules: [{
-            test: /\.(png|svg|jpg|gif|jpeg)$/,
-            use: ['file-loader']
-        }]
-    },
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "build.js",
+  },
 
-    plugins: [
-        new htmlWebpackPlugin({
-            template: './public/index.html',
-            inject: 'body'
-        })
-    ]
-}
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./public/index.html",
+      inject: "body",
+    }),
+  ],
+  devServer: {
+    port: 8000,
+  },
+};
