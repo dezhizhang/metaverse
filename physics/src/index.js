63b790992431d079ffa8ad96faef5b68e5372dbc
dezhizhang <1541609448@qq.com>
2024-12-02 07:08:56 +0800

    feat:物理材质

--- physics/src/index.js
+++ physics/src/index.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-11-26 05:55:59
  * :last editor: 张德志
- * :date last edited: 2024-12-02 06:51:26
+ * :date last edited: 2024-12-02 07:08:15
  */
 import * as THREE from "three";
 import * as CANNON from 'cannon-es';
@@ -29,20 +29,17 @@ renderer.setSize(window.innerWidth,window.innerHeight);
 
 //------------------------------------------------------
 
-// 创建物理世界
 const world = new CANNON.World();
 world.gravity.set(0,-9.82,0);
-
 
 // 创建物理世界的平面
 const planeShape = new CANNON.Plane();
 const planeBody = new CANNON.Body({
   mass:0,
   shape:planeShape,
-  position: new CANNON.Vec3(0,0,0)
+  position:new CANNON.Vec3(0,0,0)
 });
 planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI / 2);
-
 world.addBody(planeBody);
 
 const boxShape = new CANNON.Box(
@@ -60,7 +57,6 @@ world.addBody(boxBody);
 
 
 
-//------------------------------------------------------
 // 创建一个渲染立方体
 const boxGeometry = new THREE.BoxGeometry(1,1,1);
 const boxMaterial = new THREE.MeshBasicMaterial({
@@ -70,7 +66,6 @@ const boxMesh = new THREE.Mesh(boxGeometry,boxMaterial);
 scene.add(boxMesh);
 meshs.push(boxMesh);
 
-
 // 创建渲染平面
 const planeGeometry = new THREE.PlaneGeometry(10,10);
 const planeMaterial = new THREE.MeshBasicMaterial({
@@ -79,6 +74,7 @@ const planeMaterial = new THREE.MeshBasicMaterial({
 const planeMesh = new THREE.Mesh(planeGeometry,planeMaterial);
 planeMesh.rotation.x = -Math.PI / 2;
 scene.add(planeMesh);
+
 
 
 
