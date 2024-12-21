5dc9e8e932038a0f37f9896ff896dc7ef47bc987
dezhizhang <1541609448@qq.com>
2024-12-22 05:21:34 +0800

    feat:着色器

--- threejs/examples/自定义着色器.js
+++ threejs/examples/自定义着色器.js
@@ -1,13 +1,12 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
- * :author: 张德志
- * :copyright: (c) 2024, Tungee
- * :date created: 2023-03-13 05:58:33
+ * :file description: 
+ * :name: /threejs/examples/自定义着色器.js
+ * :author:张德志
+ * :copyright: (c) 2024, Xiaozhi
+ * :date created: 2024-12-22 05:20:48
  * :last editor: 张德志
- * :date last edited: 2024-12-22 05:18:56
+ * :date last edited: 2024-12-22 05:20:49
  */
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
@@ -16,8 +15,6 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,1,1000);
 camera.position.set(200,200,200);
 camera.lookAt(scene.position);
-
-
 
 
 const vertexShader = `
