6a1bdaf92edcf597e4553b308965794e078dd12d
zhangdezhi <dezhi.zhang@desaysv.com>
2025-01-16 07:23:24 +0800

    feat:提交

--- threejs/examples/ConeGeometry2.js
+++ threejs/examples/ConeGeometry2.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/ConeGeometry2.js
  * :author:张德志
  * :copyright: (c) 2025, Xiaozhi
- * :date created: 2024-07-27 12:32:40
+ * :date created: 2025-01-16 07:22:44
  * :last editor: 张德志
- * :date last edited: 2025-01-16 07:22:34
+ * :date last edited: 2025-01-16 07:22:45
  */
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
