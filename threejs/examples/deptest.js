cda82391c3a26bd9fc505a6d969fb7d2ab952396
dezhizhang <1541609448@qq.com>
2025-02-19 07:29:44 +0800

    feat: 深度测试

--- threejs/examples/deptest.js
+++ threejs/examples/deptest.js
@@ -1,13 +1,12 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/deptest.js
  * :author:张德志
  * :copyright: (c) 2025, Xiaozhi
- * :date created: 2024-07-27 12:32:40
+ * :date created: 2025-02-19 07:29:09
  * :last editor: 张德志
- * :date last edited: 2025-02-19 07:27:55
+ * :date last edited: 2025-02-19 07:29:10
  */
-
 import * as THREE from "three";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
