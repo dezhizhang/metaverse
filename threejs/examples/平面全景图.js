f1d57a8df6f1090149480ad06955c4e1268a6b9b
dezhizhang <1541609448@qq.com>
2025-02-22 15:48:16 +0800

    feat:平面全景图

--- threejs/examples/平面全景图.js
+++ threejs/examples/平面全景图.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/平面全景图.js
  * :author:张德志
  * :copyright: (c) 2025, Xiaozhi
- * :date created: 2024-07-27 12:32:40
+ * :date created: 2025-02-22 15:47:46
  * :last editor: 张德志
- * :date last edited: 2025-02-22 15:45:48
+ * :date last edited: 2025-02-22 15:47:47
  */
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
