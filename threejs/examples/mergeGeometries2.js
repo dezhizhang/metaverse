84a6ff34663c3490cf080be40a3f5d79d586f2f3
dezhizhang <1541609448@qq.com>
2025-02-22 17:48:48 +0800

    feat: 几何体合并

--- threejs/examples/mergeGeometries2.js
+++ threejs/examples/mergeGeometries2.js
@@ -1,11 +1,11 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/mergeGeometries2.js
  * :author:张德志
  * :copyright: (c) 2025, Xiaozhi
- * :date created: 2024-07-27 12:32:40
+ * :date created: 2025-02-22 17:48:08
  * :last editor: 张德志
- * :date last edited: 2025-02-22 17:47:47
+ * :date last edited: 2025-02-22 17:48:09
  */
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
