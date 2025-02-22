40ce2d6d5829f046bb0cd216bc1c4d9c48bdb678
dezhizhang <1541609448@qq.com>
2025-02-22 10:02:36 +0800

    feat: 地图网格

--- threejs/examples/gridHelper.js
+++ threejs/examples/gridHelper.js
@@ -1,15 +1,14 @@
 /*
- * :file description:
- * :name: /threejs/src/index.js
+ * :file description: 
+ * :name: /threejs/examples/gridHelper.js
  * :author:张德志
  * :copyright: (c) 2025, Xiaozhi
- * :date created: 2024-07-27 12:32:40
+ * :date created: 2025-02-22 09:59:40
  * :last editor: 张德志
- * :date last edited: 2025-02-22 09:58:21
+ * :date last edited: 2025-02-22 09:59:41
  */
 import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
 
 const scene = new THREE.Scene();
 
@@ -28,12 +27,8 @@ camera.lookAt(scene.position);
 const gridHelper = new THREE.GridHelper(3000,50,0x004444,0x004444);
 scene.add(gridHelper);
 
-
 new OrbitControls(camera,renderer.domElement);
-
 scene.add(new THREE.AxesHelper(1000));
-
-
 
 function render() {
   renderer.render(scene, camera);
