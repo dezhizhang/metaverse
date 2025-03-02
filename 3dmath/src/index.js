78d96c6672068bc32497a9647ff19c8d7931c26e
dezhizhang <1541609448@qq.com>
2025-03-02 10:33:59 +0800

    feat: 3d数学

---
+++ 3dmath/src/index.js
@@ -0,0 +1,9 @@
+/*
+ * :file description: 
+ * :name: /3dmath/src/index.js
+ * :author:张德志
+ * :copyright: (c) 2025, Xiaozhi
+ * :date created: 2025-03-02 10:32:29
+ * :last editor: 张德志
+ * :date last edited: 2025-03-02 10:32:30
+ */
