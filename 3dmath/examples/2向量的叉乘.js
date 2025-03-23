ce092df6099777097496402eb19aaa9dbbe8ee3d
dezhizhang <1541609448@qq.com>
2025-03-24 07:37:00 +0800

    feat: 2向量的叉乘

--- 3dmath/examples/2向量的叉乘.js
+++ 3dmath/examples/2向量的叉乘.js
@@ -1,19 +1,12 @@
 /*
- * :file description:
- * :name: /3dmath/src/index.js
+ * :file description: 
+ * :name: /3dmath/examples/2向量的叉乘.js
  * :author:张德志
  * :copyright: (c) 2025, Xiaozhi
- * :date created: 2025-03-02 10:32:29
+ * :date created: 2025-03-24 07:33:01
  * :last editor: 张德志
- * :date last edited: 2025-03-24 07:36:17
+ * :date last edited: 2025-03-24 07:36:33
  */
-
-// https://www.shadertoy.com/view/WtG3RD
-
-import * as THREE from "three";
-
-
-
 // 创建两个向量
 let v1 = new THREE.Vector3(1, 0, 0);
 let v2 = new THREE.Vector3(0, 1, 0);
@@ -23,4 +16,3 @@ let result = new THREE.Vector3();
 result.crossVectors(v1, v2);
 
 console.log(result); // 结果是 (0, 0, 1)
-
