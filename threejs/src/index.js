4b61ac07dc05ac5ffd7a927576969757a2778b51
dezhizhang <1541609448@qq.com>
2025-04-22 07:10:00 +0800

    feat: 发光后期处理

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -5,15 +5,20 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-05-17 05:30:15
  * :last editor: 张德志
- * :date last edited: 2025-04-22 06:20:33
+ * :date last edited: 2025-04-22 07:09:22
  */
 
 import * as THREE from 'three';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass.js';
+import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+
 // 场景
 const scene = new THREE.Scene();
 // 摄像机
@@ -21,10 +26,19 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.set(200,200,200);
 camera.lookAt(0,0,0);
 
-const geometry = new THREE.BoxGeometry(20, 20, 20);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+scene.add(new THREE.AmbientLight(0xffffff,1));
+
+
+
+const group = new THREE.Group();
+
+
+const loader = new GLTFLoader();
+loader.load('/工厂.glb', function(gltf) {
+    const model = gltf.scene;
+    group.add(model);
+    scene.add(model);
+})
 
 
 
@@ -47,17 +61,23 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 
-
 const composer = new EffectComposer(renderer);
 const renderPass = new RenderPass(scene,camera);
 composer.addPass(renderPass);
 
 
+
+
 const v2 = new THREE.Vector2(window.innerWidth,window.innerHeight);
+
 const bloomPass = new UnrealBloomPass(v2);
-bloomPass.strength = 1.5;
-bloomPass.selectedObjects = [cube];
 composer.addPass(bloomPass);
+bloomPass.selectedObjects = [group];
+
+
+const gammaPass = new ShaderPass(GammaCorrectionShader);
+composer.addPass(gammaPass);
+
 
 
 
@@ -66,8 +86,6 @@ function animate() {
     requestAnimationFrame(animate);
 
     composer.render();
-    // 渲染场景
-    // renderer.render(scene, camera);
 }
 
 
